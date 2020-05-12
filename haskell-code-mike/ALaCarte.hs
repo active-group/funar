184ab8a35df165eac3b237f4e2746036b662b8e0
Mike Sperber <sperber@deinprogramm.de>
2020-05-12 17:25:08 +0200

    Update.

--- haskell-code-mike/ALaCarte.hs
+++ haskell-code-mike/ALaCarte.hs
@@ -2,10 +2,16 @@
 
 module ALaCarte where
 
+data SimpleArith e =
+    SVal Int | SAdd e e
+
 data Expr f = In (f (Expr f))
 
-data Val e = Val Int
-data Add e = Add e e
+data Val e = Val Int -- Val :: * -> *
+
+data Add e = Add e e -- Add :: * -> *
+
+data Mul e = Mul e e
 
 -- Produkt: zusammgesetzte Daten
 -- kartesische Produkt - Tupel
@@ -13,8 +19,24 @@ data Add e = Add e e
 -- Int x String, F# int * string
 type X = (Int, String)
 
--- Coprodukt: gemischte Daten - |
+-- Coprodukt: gemischte Daten, "Summe" - |
+-- data Either a b = Left a | Right b
 
-data (f :+: g) e = Inl (f e) | Inr (g e)
+data (f :+: g) e = -- f :: * -> *, g :: * -> *
+    Inl (f e) -- "inject left"
+  | Inr (g e) -- "inject right"
 
 
+addExample :: Expr (Val :+: Add)
+-- "118 + 1219"
+addExample = In (Inr (Add (In (Inl (Val 118))) (In (Inl (Val 1219)))))
+
+instance Functor Val where
+    fmap f (Val x) = Val x 
+
+instance Functor Add where 
+    fmap f (Add e1 e2) = Add (f e1) (f e2)
+
+instance (Functor f, Functor g) => Functor (f :+: g) where
+    fmap f (Inl e1) = Inl (fmap f e1)
+    fmap f (Inr e1) = Inr (fmap f e2)
