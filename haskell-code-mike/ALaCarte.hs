989117c2f453b1ec6d65a69739899b4873b4329c
Mike Sperber <sperber@deinprogramm.de>
2020-05-12 17:13:51 +0200

    Update.

--- haskell-code-mike/ALaCarte.hs
+++ haskell-code-mike/ALaCarte.hs
@@ -1,2 +1,20 @@
+{-# LANGUAGE TypeOperators #-}
+
 module ALaCarte where
+
+data Expr f = In (f (Expr f))
+
+data Val e = Val Int
+data Add e = Add e e
+
+-- Produkt: zusammgesetzte Daten
+-- kartesische Produkt - Tupel
+-- X ist das Produkt von Int und String
+-- Int x String, F# int * string
+type X = (Int, String)
+
+-- Coprodukt: gemischte Daten - |
+
+data (f :+: g) e = Inl (f e) | Inr (g e)
+
 
