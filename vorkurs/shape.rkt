f93dbe85d53408c216cc34688a8a1e6fcfb90856
Mike Sperber <sperber@deinprogramm.de>
2020-06-22 12:03:30 +0200

    Update.

--- vorkurs/shape.rkt
+++ vorkurs/shape.rkt
@@ -1,8 +1,6 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
-#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname shape) (read-case-sensitive #f) (teachpacks ((lib "image.rkt" "teachpack" "deinprogramm" "sdp"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image.rkt" "teachpack" "deinprogramm" "sdp")))))
-
-
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname shape) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
 ; Zusammengesetzte Daten
 
 ; Ein Kreis hat folgende Eigenschaften:
@@ -29,3 +27,58 @@
 (define point1 (make-point 5 10)) ; Punkt, X=5, Y=10
 (define point2 (make-point 3 17)) ; Punkt, X=3, Y=17
 
+(define circle1 (make-circle point1 3)) ; Kreis mit Mittelpunkt X=5, Y=10, Radius 3
+(define circle2 (make-circle point2 5)) ; Kreis mit Mittelpunkt point2, Radius 5
+
+; Ist Punkt in Kreis?
+(: in-circle? (point circle -> boolean))
+
+(check-expect (in-circle? point1 circle1) #t)
+(check-expect (in-circle? point2 circle2) #t)
+(check-expect (in-circle? (make-point 20 20) circle1) #f)
+
+; Schablone
+#;(define in-circle?
+  (lambda (point circle)
+    (define center (circle-center circle))
+    (point-x center)
+    (point y center)
+    (point-x point)
+    (point-y point)
+    (circle-center circle)
+    (circle-radius circle)
+    ...))
+
+(define in-circle?
+  (lambda (point circle)
+    (<= (distance (circle-center circle) point)
+        (circle-radius circle))))
+
+(define non-negative?
+  (lambda (x)
+    (>= x 0)))
+
+(define non-negative-number
+  (signature (predicate
+              non-negative?)))
+
+; Abstand zwischen zwei Punkten berechnen
+(: distance (point point -> (combined real
+                                      non-negative-number)))
+
+(check-expect (distance (make-point 0 0) (make-point 5 0))
+              5)
+(check-expect (distance (make-point 0 0) (make-point 0 5))
+              5)
+(check-expect (distance (make-point 0 0) (make-point 0 -5))
+              5)
+
+(define distance
+  (lambda (point1 point2)
+    (define dx (- (point-x point1)
+                  (point-x point2)))
+
+    (define dy (- (point-y point1)
+                  (point-y point2)))
+
+    (sqrt (+ (* dx dx) (* dy dy)))))
