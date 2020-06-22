0ac684a37f5a44237e9937f7786ef084cefe0239
Mike Sperber <sperber@deinprogramm.de>
2020-06-22 16:44:37 +0200

    Update.

--- vorkurs/rev.rkt
+++ vorkurs/rev.rkt
@@ -1,3 +1,26 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "vanilla-reader.rkt" "deinprogramm" "sdp")((modname rev) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Liste umdrehen
+(: rev ((list-of %a) -> (list-of %a)))
+
+(check-expect (rev (list 1 2 3))
+              (list 3 2 1))
+
+(define rev
+  (lambda (list)
+    (cond
+      ((empty? list) empty)
+      ((cons? list)
+       (add-to-end
+        (rev (rest list)) ; 3 2
+        (first list)))))) ; 1
+
+(define add-to-end
+  (lambda (list element)
+    (cond
+      ((empty? list) (cons element empty))
+      ((cons? list)
+       (cons(first list)
+            (add-to-end (rest list) element))))))
+       
