6fb4df17f77991c47cec215a01792758396ea874
Mike Sperber <sperber@deinprogramm.de>
2020-09-22 14:57:31 +0200

    Update

--- haskell-code-mike/Intro.hs
+++ haskell-code-mike/Intro.hs
@@ -226,7 +226,7 @@ instance Semigroup Additive where
   combine (Additive n1) (Additive n2) = Additive (n1 + n2)
 
 class Functor f where
-  -- universalMap id f = id
+  -- universalMap id f == f
   universalMap :: (a -> b) -> f a -> f b
 
 instance Functor Optional where
