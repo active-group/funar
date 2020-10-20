6abf766b2992d0dfbbdd43b4d5942902a04116af
Mike Sperber <sperber@deinprogramm.de>
2020-10-20 09:48:30 +0200

    Update.

--- haskell-code-mike/Intro.hs
+++ haskell-code-mike/Intro.hs
@@ -1,2 +1,24 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 module Intro where
+
+x :: Integer
+x = 15 + 22
+
+-- Ein Haustier ist eins der folgenden:
+-- - Hund
+-- - Katze
+-- - Schlange
+-- neuer Datentyp
+data Pet = Dog | Cat | Snake
+  deriving Show
+
+pet1 :: Pet
+pet1 = Dog
+
+-- Ist Haustier niedlich?
+isCute :: Pet -> Bool
+isCute Dog = True
+isCute Cat = True
+-- isCute Snake = False
+
 
