198ec78c3f1bb0c3643d0c6e5997a301bc379f39
Mike Sperber <sperber@deinprogramm.de>
2020-10-22 12:06:08 +0200

    Executable.

--- hearts-mike/docker-shell.sh
+++ hearts-mike/docker-shell.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -p 8080:8080 -p 8001:8001 -p 8002:8002 -p 8003:8003 -p 8004:8004 -v `pwd`:/hearts -w /hearts -i -t ghcide:latest bash
+docker run --rm -p 8080:8080 -p 8001:8001 -p 8002:8002 -p 8003:8003 -p 8004:8004 -v `pwd`:/hearts -w /hearts -i -t haskell:8.8 bash
