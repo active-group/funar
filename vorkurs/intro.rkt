686d833f335267b48955f0781fbae0dfe8b27bf3
Mike Sperber <sperber@deinprogramm.de>
2020-11-30 13:20:44 +0100

    Update.

--- vorkurs/intro.rkt
+++ vorkurs/intro.rkt
@@ -155,7 +155,6 @@ class Dillo {
 (define animal
   (signature (mixed dillo parrot)))
 
-#|
 ; Tier überfahren
 (: run-over-animal (animal -> animal))
 
@@ -167,7 +166,6 @@ class Dillo {
 (define run-over-animal
   (lambda (animal)
     (cond
-      (... ...)
-      (... ...))))
+      ((dillo? animal) (run-over-dillo animal))
+      ((parrot? animal) (run-over-parrot animal)))))
      
-|#
