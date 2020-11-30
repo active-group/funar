c4f8d295035a85d8988304728b20f72c3a846436
Mike Sperber <sperber@deinprogramm.de>
2020-11-30 09:52:43 +0100

    Update.

--- vorkurs/images.rkt
+++ vorkurs/images.rkt
@@ -2,3 +2,5 @@
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "vanilla-reader.rkt" "deinprogramm" "sdp")((modname images) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
 ; Vorkurs
+; Menü: Language/Sprache -> Choose Language
+; Schreibe Dein Programm! - Anfänger
