75e0eb4e91142e0e89a9a06fdb92e77acf7c07a9
Mike Sperber <sperber@deinprogramm.de>
2020-11-30 09:50:33 +0100

    Update.

---
+++ vorkurs/images.rkt
@@ -0,0 +1,4 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "vanilla-reader.rkt" "deinprogramm" "sdp")((modname images) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Vorkurs
