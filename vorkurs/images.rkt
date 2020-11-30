5e0269bd412d21dfdd01ab1e951b9053b291014d
Mike Sperber <sperber@deinprogramm.de>
2020-11-30 09:57:07 +0100

    Update.

--- vorkurs/images.rkt
+++ vorkurs/images.rkt
@@ -1,6 +1,6 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
-#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname images) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname images) (read-case-sensitive #f) (teachpacks ((lib "image.rkt" "teachpack" "deinprogramm" "sdp"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image.rkt" "teachpack" "deinprogramm" "sdp")))))
 ; Vorkurs
 ; Menü: Language/Sprache -> Choose Language
 ; Schreibe Dein Programm! - Anfänger
