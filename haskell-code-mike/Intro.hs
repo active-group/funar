16ae2f86dad4a72bb69e9258cc4cdd45e3a31ad7
Mike Sperber <sperber@deinprogramm.de>
2020-12-01 09:32:05 +0100

    Update.

--- haskell-code-mike/Intro.hs
+++ haskell-code-mike/Intro.hs
@@ -14,3 +14,11 @@ isCute :: Pet -> Bool
 isCute Dog = True
 isCute Cat = True
 isCute Snake = False
+
+-- Typen und Werte haben unterschiedliche Namensräume
+
+-- Ein Gürteltier hat folgende Eigenschaften:
+-- - lebendig oder tot
+-- - Gewicht
+data Dillo = Dillo { dilloAlive :: Bool,
+                     dilloWeight :: Double }
