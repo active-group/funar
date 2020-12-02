7a5037a9ec82b42cccd0d9f6e930e1760dc0749a
Mike Sperber <sperber@deinprogramm.de>
2020-12-02 10:21:45 +0100

    Update.

--- hearts-mike/src/GameEvent.hs
+++ hearts-mike/src/GameEvent.hs
@@ -1,1 +1,20 @@
 module GameEvent where
+
+import Cards
+
+-- Event: etwas, das in der Vergangenheit passiert ist
+-- Die Events beschreiben, alles, was passiert ist.
+-- Sie sollten aus der Domäne kommen.
+-- Sie dürfen redundant sein.
+data GameEvent =
+      PlayerArrived Player
+    | CardsDealt
+    | GameStarted [Player]
+    | CardPlayed Player Card
+    | TrickDone Trick Player
+    | InvalidMove Player Card
+    | GameOver Player -- Gewinner
+
+
+-- Command: Beschreibung von etwas, das passieren soll.
+-- != Events
