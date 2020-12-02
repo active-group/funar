ee78e023f00e1b24a877e8e2c773acc64b6eccdc
Mike Sperber <sperber@deinprogramm.de>
2020-12-02 10:58:49 +0100

    Update.

--- hearts-mike/src/GameEvent.hs
+++ hearts-mike/src/GameEvent.hs
@@ -2,6 +2,20 @@ module GameEvent where
 
 import Cards
 
+data GameEvent =
+    HandDealt Player Hand
+  | PlayerTurnChanged Player
+  | LegalCardPlayed Player Card
+  | TrickTaken Player Trick
+  | IllegalCardPlayed Player Card
+  | GameEnded Player
+  deriving Show
+
+data GameCommand =
+    DealHands PlayerHands
+  | PlayCard Player Card
+  deriving Show
+
 {-
 -- Event: etwas, das in der Vergangenheit passiert ist
 -- Die Events beschreiben, alles, was passiert ist.
