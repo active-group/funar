e472af055cb15080964c1720032fb0db5d22b88c
Mike Sperber <sperber@deinprogramm.de>
2020-12-02 13:15:16 +0100

    Update.

--- hearts-mike/src/Table.hs
+++ hearts-mike/src/Table.hs
@@ -141,3 +141,25 @@ addToStack playerStacks player cards =
 
 -- Verbindung zwischen TableState und GameEvent / GameCommand
 tableProcessEvent :: GameEvent -> TableState -> TableState
+tableProcessEvent (HandDealt player hand) state =
+  state {
+    tableStateHands = Map.insert player hand (tableStateHands state),
+    tableStateTrick = emptyTrick
+  }
+tableProcessEvent (PlayerTurnChanged player) state =
+  state {
+    tableStatePlayers  = rotateTo player (tableStatePlayers state)
+  }
+tableProcessEvent (LegalCardPlayed player card) state =
+  state {
+    tableStateHands = takeCard (tableStateHands state) player card,
+    tableStateTrick = addToTrick player card (tableStateTrick state)
+  }
+tableProcessEvent (TrickTaken player trick) state =
+  state {
+    tableStateStacks =
+      addToStack (tableStateStacks state) player (cardsOfTrick trick),
+    tableStateTrick = emptyTrick
+  }
+tableProcessEvent (IllegalCardPlayed player card) state = state
+tableProcessEvent (GameEnded player) state = state
