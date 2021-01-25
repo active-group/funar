99e03ebf63a014c24f1b262e96882b1dec872871
Mike Sperber <sperber@deinprogramm.de>
2021-01-25 16:09:31 +0100

    Update.

--- haskell-code-mike/Validation.hs
+++ haskell-code-mike/Validation.hs
@@ -1,2 +1,34 @@
 module Validation where
 
+data Person = Person {
+    personName :: String,
+    personEmail :: String,
+    personAge :: Int
+}
+
+p1 = Person "Mike" "sperber@deinprogramm.de" 49
+p2 = Person "Fritz" "fritz/-/(&/(" 500
+
+
+-- Typsystem nicht stark genug, um illegale Daten zu verbieten
+
+-- Funktionale Programmierung: "make illegal states unrepresentable"
+-- schwache Version: "don't create illegal states"
+
+-- "parse, don't validate"
+
+{-
+-- Fehlschlag 0:
+validateAge age = if age <= 120
+                  then Just age
+                  else Nothing -- keine Fehlermeldung
+
+-- Fehlschlag 1: (kein Parsing)
+validateAge' age = if age <= 120
+                   then Nothing -- kein Parsing
+                   else Just "too old"
+-}
+
+data Validation error result =
+    Success result
+  | Failure error
