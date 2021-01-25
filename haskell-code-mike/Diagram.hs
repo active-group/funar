3a78a57ed28e9b19a773dc0627776910c7549c5b
Mike Sperber <sperber@deinprogramm.de>
2021-01-25 15:11:26 +0100

    Update.

--- haskell-code-mike/Diagram.hs
+++ haskell-code-mike/Diagram.hs
@@ -6,6 +6,12 @@ import Data.Semigroup
 -- combine heißt jetzt <>
 -- neutral heißt jetzt 
 
+-- Größe mit inbegriffen + Position
 data Prim = Square | Circle | Triangle | Smiley
 
+-- Idee: primitive Bilder übereinanderlegen
+-- oberstes Bild kommt zuerst
 type Diagram = [Prim]
+
+-- Brent Yorgey: Bilder müssen Monoid bilden
+
