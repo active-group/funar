72771f98c2053d0cdd4bfc2771e9767a172bb72f
Mike Sperber <sperber@deinprogramm.de>
2021-01-25 15:15:29 +0100

    Update.

--- haskell-code-mike/Diagram.hs
+++ haskell-code-mike/Diagram.hs
@@ -10,10 +10,12 @@ import Data.Semigroup
 data Prim = Square | Circle | Triangle | Smiley
 
 -- Idee: primitive Bilder übereinanderlegen
--- oberstes Bild kommt zuerst
+-- oberstes Bild kommt zuletzt
 type Diagram = [Prim]
 
 -- Brent Yorgey: Bilder müssen Monoid bilden
 -- Listen bilden einen Monoiden
+-- hätte gern einen Monoiden, beim dem das oberste Bild zuerst kommt
 
 -- => Monoid Diagram
+-- [p1, p2, p3] <> [p4, p5] = [p1, p2, p3, p4, p5]
