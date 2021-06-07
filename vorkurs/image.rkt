eb6ae856e2a94c200027b7272692fdd322ac5608
Mike Sperber <sperber@deinprogramm.de>
2021-06-07 08:58:57 +0200

    Update.

--- vorkurs/image.rkt
+++ vorkurs/image.rkt
@@ -2,4 +2,3 @@
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname image) (read-case-sensitive #f) (teachpacks ((lib "image.rkt" "teachpack" "deinprogramm" "sdp"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image.rkt" "teachpack" "deinprogramm" "sdp")))))
 ; Vorkurs Funktionale Softwarearchitektur
-; Rechnen mit Bildern
