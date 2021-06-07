c3999fa739f39dfe8ca65565c53ab9c8088271e1
Mike Sperber <sperber@deinprogramm.de>
2021-06-07 11:07:24 +0200

    Update.

--- vorkurs/intro.rkt
+++ vorkurs/intro.rkt
@@ -1,4 +1,16 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname intro) (read-case-sensitive #f) (teachpacks ((lib "image.rkt" "teachpack" "deinprogramm" "sdp"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image.rkt" "teachpack" "deinprogramm" "sdp")))))
-; Intro
+; Datenanalyse
+; Datendefinition:
+; Ein Haustier ist eins der folgenden:
+; - Hund ODER
+; - Katze ODER
+; - Schlange
+; Fallunterscheidung
+; hier Spezialfall: Aufzählung
+; in Code:
+(define pet
+  (signature
+   (enum "dog" "cat" "snake")))
+
