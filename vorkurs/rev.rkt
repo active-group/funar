e5a77b56bfe08604f67c8173d06449383bb2c742
Mike Sperber <sperber@deinprogramm.de>
2021-06-07 16:55:01 +0200

    Update.

--- vorkurs/rev.rkt
+++ vorkurs/rev.rkt
@@ -4,7 +4,7 @@
 ; Liste umdrehen
 (: rev ((list-of %a) -> (list-of %a)))
 
-(check-expect (rev (list 1 2 3)) (list 3 2 1))
+;(check-expect (rev (list 1 2 3)) (list 3 2 1))
 
 (define rev
   (lambda (list)
@@ -16,7 +16,7 @@
 
 (: adjoin ((list-of %a) %a -> (list-of %a)))
 
-(check-expect (adjoin (list 3 2) 1) (list 3 2 1))
+;(check-expect (adjoin (list 3 2) 1) (list 3 2 1))
 
 (define adjoin
   (lambda (list element)
@@ -25,4 +25,21 @@
       ((cons? list)
        (cons (first list)
              (adjoin (rest list) element))))))
-    
+
+; Berechnungsschritte: n + (n-1) + (n-2) + ... + 2 + 1
+; n/2 * (n+1) = n^2 + ... = O(n^2)
+
+; Programmieren mit Akkumulator
+
+(: rev1 ((list-of %a) (list-of %a) -> (list-of %a)))
+
+(check-expect (rev1 (list 1 2 3) empty) (list 3 2 1))
+
+(define rev1
+  (lambda (list acc)
+    (cond
+      ((empty? list) acc)
+      ((cons? list)
+       (rev1 (rest list) (cons (first list) acc))))))
+       
+
