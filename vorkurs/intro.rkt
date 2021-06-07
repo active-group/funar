18370c3df34d509eb77d7bc9433dc91ed4712d3e
Mike Sperber <sperber@deinprogramm.de>
2021-06-07 13:31:03 +0200

    Update.

--- vorkurs/intro.rkt
+++ vorkurs/intro.rkt
@@ -46,19 +46,21 @@
 
 ;(cute? "parakeet")
 
+(define hour (signature (integer-from-to 0 23)))
+(define minute (signature (integer-from-to 0 59)))
+
 ; Uhrzeit besteht aus: / hat folgende Eigenschaften:
 ; - Stunde UND
 ; - Minute
 ; zusammengesetzte Daten
 (define-record time ; Signatur
   make-time ; Konstruktor, "make-" Konvention
-  (time-hour natural) ; Selektoren / "Getter-Funktion"
-  (time-minute natural))
+  (time-hour hour) ; Selektoren / "Getter-Funktion"
+  (time-minute minute))
 
-(: make-time (natural natural -> time))
-(: time-hour (time -> natural))
-(: time-minute (time -> natural))
-
+(: make-time (hour minute -> time))
+(: time-hour (time -> hour))
+(: time-minute (time -> minute))
 
 (define time1 (make-time 12 24)) ; 12 Uhr 24
 (define time2 (make-time 0 0)) ; Mitternacht
@@ -74,4 +76,50 @@
     (+ (* 60 (time-hour time))
        (time-minute time))))
 
-              
+; Andersrum
+(: msm->time (natural -> time))
+
+(check-expect (msm->time 744) time1)
+(check-expect (msm->time (msm time1)) time1)
+
+(check-property
+ (for-all ((a number)
+           (b number))
+   (= (+ a b) (+ b a))))
+
+(check-property
+ (for-all ((t time))
+   (expect (msm->time (msm t))
+           t)))
+
+(define msm->time
+  (lambda (minutes)
+    (make-time (quotient minutes 60)
+               (remainder minutes 60))))
+
+; Ein Gürteltier hat folgende Eigenschaften:
+; - tot oder lebendig
+; - Gewicht
+; Zustand eines Gürteltiers zu einem bestimmten Zeitpunkt
+(define-record dillo
+  make-dillo
+  (dillo-alive? boolean)
+  (dillo-weight number))
+
+(: make-dillo (boolean number -> dillo))
+
+(define dillo1 (make-dillo #t 10)) ; Gürteltier, 10kg, lebendig
+(define dillo2 (make-dillo #f 12)) ; totes Gürteltier, 12kg
+
+; Gürteltier überfahren
+(: run-over-dillo (dillo -> dillo))
+
+(check-expect (run-over-dillo dillo1)
+              (make-dillo #f 10))
+(check-expect (run-over-dillo dillo2)
+              dillo2)
+
+(define run-over-dillo
+  (lambda (dillo)
+    (make-dillo #f (dillo-weight dillo))))
+
