01ad97e288244de635d01540a9640b7d3269a780
Mike Sperber <sperber@deinprogramm.de>
2021-06-08 16:43:25 +0200

    Update.

--- haskell-code-mike/Intro.hs
+++ haskell-code-mike/Intro.hs
@@ -273,3 +273,5 @@ instance Monoid [e] where
 instance (Semigroup a, Semigroup b) => Semigroup (a, b) where
     combine (a1, b1) (a2, b2) = (a1 `combine` a2, b1 `combine` b2)
 
+instance Semigroup (Map key value) where
+    
