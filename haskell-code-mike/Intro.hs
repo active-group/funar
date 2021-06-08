30e7158fd0cddeb16cd3b250c645c9e769637cad
Mike Sperber <sperber@deinprogramm.de>
2021-06-08 09:31:47 +0200

    Update.

--- haskell-code-mike/Intro.hs
+++ haskell-code-mike/Intro.hs
@@ -49,3 +49,7 @@ type Weight = Double
 
 -- 2 Dinge, die Dillo heißen: Typ, Konstruktor
 data Dillo = Dillo { dilloLiveness :: Liveness, dilloWeight :: Weight }
+
+dillo1 :: Dillo
+dillo1 = Dillo { dilloLiveness = Alive, dilloWeight = 10 } -- lebendiges Gürteltier, 10kg
+dillo2 = Dillo Dead 12 -- totes Gürteltier, 12kg
