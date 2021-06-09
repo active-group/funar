3fd86ae44dec2f49ac70436c1a39af008fbcefe9
Mike Sperber <sperber@deinprogramm.de>
2021-06-09 09:29:15 +0200

    Update.

--- haskell-code-mike/Cards.hs
+++ haskell-code-mike/Cards.hs
@@ -10,5 +10,12 @@ data Maybe a = Nothing | Just a
 -}
 
 {-
-class
+class Eq a => Ord a where
+  compare :: a -> a -> Ordering
+  (<) :: a -> a -> Bool
+  (<=) :: a -> a -> Bool
+  (>) :: a -> a -> Bool
+  (>=) :: a -> a -> Bool
+  max :: a -> a -> a
+  min :: a -> a -> a
 -}
