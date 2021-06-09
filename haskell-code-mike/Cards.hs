d0e5acb01693f749e039e51e72c3c3ec62a7a8a4
Mike Sperber <sperber@deinprogramm.de>
2021-06-09 09:27:11 +0200

    Update.

---
+++ haskell-code-mike/Cards.hs
@@ -0,0 +1,10 @@
+module Cards where
+
+-- Repräsentation (Typ) für Karte
+-- kompletter Satz Karten
+-- Funktion, die sagt, ob eine Karte höherwertig ist als eine andere
+-- (geht nur für gleiche Farbe)
+
+{-
+data Maybe a = Nothing | Just a
+-}
