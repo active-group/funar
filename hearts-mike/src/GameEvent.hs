852a7b41e4eb9e2bbf935a88fe610218f1814abb
Mike Sperber <sperber@deinprogramm.de>
2021-06-09 11:39:12 +0200

    Update.

--- hearts-mike/src/GameEvent.hs
+++ hearts-mike/src/GameEvent.hs
@@ -24,3 +24,17 @@ data Command =
 
    -}
 
+data GameEvent
+  = HandDealt Player Hand
+  | PlayerTurnChanged Player
+  | LegalCardPlayed Player Card
+  | TrickTaken Player Trick
+  | IllegalCardPlayed Player Card
+  | GameEnded Player
+  deriving (Eq, Show)
+
+data GameCommand
+  = DealHands PlayerHands
+  | PlayCard Player Card
+  deriving (Eq, Show)
+
