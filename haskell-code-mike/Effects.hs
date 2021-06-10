5e83c858f5730c7ab63f4bec38daa047e07f83d5
Mike Sperber <sperber@deinprogramm.de>
2021-06-10 09:21:52 +0200

    Update.

---
+++ haskell-code-mike/Effects.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs, FlexibleContexts, TypeOperators, DataKinds, PolyKinds #-}
+{-# LANGUAGE LambdaCase, BlockArguments, TypeApplications, ScopedTypeVariables, ConstraintKinds #-}
+{-# OPTIONS_GHC -fplugin=Polysemy.Plugin #-}
+
+module Effects where
