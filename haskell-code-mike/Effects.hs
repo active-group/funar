d2c60f88deea51c4ffbb1d61ddd5ea8784825e46
Mike Sperber <sperber@deinprogramm.de>
2021-06-10 09:30:33 +0200

    Update.

--- haskell-code-mike/Effects.hs
+++ haskell-code-mike/Effects.hs
@@ -18,9 +18,17 @@ data DB' self
   | Put' String Integer (() -> self)
 
 Get' :: String -> (Integer -> self) -> DB' self
+Put' :: String -> Integer -> (() -> self) -> DB' self
 -}
 
 
 -- GADTs: Generalized Algebraic Datatypes
 
 data DB monad a where -- nicht =, GADT
+  Get :: String -> DB monad Integer  -- bei jedem Konstruktor wird a anders instanziert
+  Put :: String -> Integer -> DB monad ()
+
+-- Sem ist die freie Monade in Polysemy
+
+get :: Member DB effects => String -> Sem effects Integer
+get key = send (DB key)
