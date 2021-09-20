e50201f0c37fd9d2495053a90597a97cb1f6df51
Mike Sperber <sperber@deinprogramm.de>
2021-09-20 10:47:45 +0200

    Update.

--- vorkurs/animals.rkt
+++ vorkurs/animals.rkt
@@ -1,4 +1,18 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname animals) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
-;
+; Tiere auf dem texanischen Highway
+
+; Datenanalyse
+
+; Datendefinition
+; Ein Gürteltier hat folgende Eigenschaften:
+; Alternative: "besteht aus"
+; - tot oder lebendig - UND -
+; - Gewicht
+; zusammengesetzte Daten
+(define-record dillo
+  make-dillo
+  (dillo-liveness liveness)
+  (dillo-weight number))
+
