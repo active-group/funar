ac03b421c7d02dccdb564ae9596edfa09d4e039a
Mike Sperber <sperber@deinprogramm.de>
2021-09-20 09:47:53 +0200

    Update.

--- vorkurs/intro.rkt
+++ vorkurs/intro.rkt
@@ -9,3 +9,9 @@
 (define circle1 (circle 50 "solid" "red"))
 (define square1 (square 100 "outline" "blue"))
 (define star1 (star 50 "solid" "green"))
+(define overlay1 (overlay star1 square1))
+(define overlay2 (overlay overlay1 circle1))
+
+(above
+ (beside circle1 star1)
+ (beside star1 circle1))
