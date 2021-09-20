0919aebac9632a44fb079a5d26036b982be1af33
Mike Sperber <sperber@deinprogramm.de>
2021-09-20 13:49:09 +0200

    Update.

--- vorkurs/river.rkt
+++ vorkurs/river.rkt
@@ -1,3 +1,10 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname river) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Ein Fluss:
+; - kommt aus Quelle - ODER -
+; - fließt aus 2 Flüssen zusammen
+
+; Ein Fluss ist eins der folgenden:
+; - ein Bach aus einer Quelle
+; - ein Zusammenfluss von Haupt- und Nebenfluss
