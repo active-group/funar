377673755aefec0504bdcc519f1b02dce2a43654
Mike Sperber <sperber@deinprogramm.de>
2021-09-20 13:46:12 +0200

    Update.

---
+++ vorkurs/river.rkt
@@ -0,0 +1,3 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname river) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
