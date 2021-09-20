a5a4a9400e929790d0622bd582061405750fcb84
Mike Sperber <sperber@deinprogramm.de>
2021-09-20 09:41:36 +0200

    Update.

--- vorkurs/intro.rkt
+++ vorkurs/intro.rkt
@@ -1,4 +1,8 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname intro) (read-case-sensitive #f) (teachpacks ((lib "image.rkt" "teachpack" "deinprogramm" "sdp"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image.rkt" "teachpack" "deinprogramm" "sdp")))))
-(define x (+ 12 (* 23 42)))
+(define x
+  (+ 12
+     (* 23
+        42)))
+
