9095dd7e338da5edd99c1217d81966e09eece951
Mike Sperber <sperber@deinprogramm.de>
2021-09-21 15:10:30 +0200

    Update.

--- scala-code-mike/src/main/scala/funar/Cards.scala
+++ scala-code-mike/src/main/scala/funar/Cards.scala
@@ -16,6 +16,18 @@ sealed trait Rank {
 
 object Rank {
   case object Two extends Rank { override val value = 2 }
+  case object Three extends Rank { override val value = 3 }
+  case object Four extends Rank { override val value = 4 }
+  case object Five extends Rank { override val value = 5 }
+  case object Six extends Rank { override val value = 6 }
+  case object Seven extends Rank { override val value = 7 }
+  case object Eight extends Rank { override val value = 8 }
+  case object Nine extends Rank { override val value = 9 }
+  case object Ten extends Rank { override val value = 10 }
+  case object Jack extends Rank { override val value = 11 }
+  case object Queen extends Rank { override val value = 12 }
+  case object King extends Rank { override val value = 13 }
+  case object Ace extends Rank { override val value = 14 }
 }
 
 
