da2b768b82785bfbb84663a4f4af27fcbcc671e9
Mike Sperber <sperber@deinprogramm.de>
2021-09-21 15:04:51 +0200

    Update.

---
+++ scala-code-mike/src/main/scala/funar/Cards.scala
@@ -0,0 +1,12 @@
+package funar
+
+sealed trait Suit
+object Suit {
+  case object Diamonds extends Suit
+  case object Clubs extends Suit
+  case object Spades extends Suit
+  case object Hearts extends Suit
+
+  val all = Seq(Diamonds, Clubs, Spades, Hearts)
+}
+
