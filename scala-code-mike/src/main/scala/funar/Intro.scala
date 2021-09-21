8ccda03c967129a07ceaa0f45a3bfdac3afb1303
Mike Sperber <sperber@deinprogramm.de>
2021-09-21 11:03:03 +0200

    Update.

--- scala-code-mike/src/main/scala/funar/Intro.scala
+++ scala-code-mike/src/main/scala/funar/Intro.scala
@@ -2,7 +2,7 @@ package funar
 
 object Intro {
 
-  sealed trait Liveness
-  case object Alive extends Liveness
+  sealed trait Liveness // trait - think "interface"
+  case object Alive extends Liveness // case: automatically define constructors, equality, etc.
   case object Dead extends Liveness
 }
