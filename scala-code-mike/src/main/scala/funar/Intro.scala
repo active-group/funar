a97b3445269237d2965bd26ecb4cd902bd512d28
Mike Sperber <sperber@deinprogramm.de>
2021-09-21 14:13:04 +0200

    Update.

--- scala-code-mike/src/main/scala/funar/Intro.scala
+++ scala-code-mike/src/main/scala/funar/Intro.scala
@@ -127,5 +127,7 @@ object Intro {
   def curry[A, B, C](f: (A, B) => C): A => (B => C) =
     { a => { b => f(a, b) }}
 
+  def uncurry[A, B, C](f: A => (B => C)): (A, B) => C =
+    { (a, b) => f(a)(b) }
 
 }
