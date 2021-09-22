16b4b9a3fe945ce053c4f09ac0b43a1f00b97524
Mike Sperber <sperber@deinprogramm.de>
2021-09-22 13:40:33 +0200

    Update.

--- scala-code-mike/src/main/scala/funar/Monads.scala
+++ scala-code-mike/src/main/scala/funar/Monads.scala
@@ -1,6 +1,6 @@
 package funar
 
-trait Functor[F[_]] {
+trait Functor[F[_]] { // [_]: F is a type constructor
   def map[A, B](f: A => B, x: F[A]): F[B]
 }
 
@@ -12,4 +12,9 @@ object Functor {
       case None => None
       case Some(x) => Some(f(x))
     }
+
+  def listFunctor = new Functor[List] {
+    def map[A, B](f: A => B, x: List[A]): List[B] =
+      x.map(f)
+  }
 }
