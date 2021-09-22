adbc49cc64a4081196c0206de7685828d3433695
Mike Sperber <sperber@deinprogramm.de>
2021-09-22 16:39:33 +0200

    Update.

--- hearts-scala-mike/src/main/scala/funar/hearts/Player.scala
+++ hearts-scala-mike/src/main/scala/funar/hearts/Player.scala
@@ -11,5 +11,37 @@ case class Player(id: String, name: String) {
 }
 
 object Player {
-  
+  import GameEvent._
+
+  type Pile = Set[Card]
+  case class PlayerState[PlayerId](hand: Hand, trick: Trick, pile: Pile)
+
+  def emptyPlayerState[PlayerId] = PlayerState[PlayerId](Hand.empty, Trick.empty, Set.empty)
+
+  def playerProcessEvent[PlayerId](player: Player, event: GameEvent, state: PlayerState[PlayerId]): PlayerState[PlayerId] =
+    event match {
+      case HandDealt(player1, hand) =>
+        if (player == player1)
+          PlayerState(hand = hand, trick = Trick.empty, pile = Set.empty)
+        else
+          state
+      case PlayerTurnChanged(player1) => state
+      case LegalCardPlayed(player1, card) =>
+        if (player == player1)
+          state.copy(hand = state.hand - card,
+                     trick = Trick.add(state.trick, player1, card))
+        else
+          state.copy(trick = Trick.add(state.trick, player1, card))
+      case IllegalCardPlayed(player1, card) => state
+      case TrickTaken(player1, trick) =>
+        if (player1 == player)
+          state.copy(trick = Trick.empty,
+                     pile = state.pile ++ Trick.cards(trick))
+        else
+          state.copy(trick = Trick.empty)
+      case GameEnded(winner) => state
+    }
+
+    type EventProcessor[Event, Command] = Event => Seq[Command]
+
 }
