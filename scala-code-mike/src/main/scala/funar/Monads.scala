5b7cbc54fb6e40ada030d02c91259d069ee41ca3
Mike Sperber <sperber@deinprogramm.de>
2021-09-22 14:06:50 +0200

    Update.

--- scala-code-mike/src/main/scala/funar/Monads.scala
+++ scala-code-mike/src/main/scala/funar/Monads.scala
@@ -1,5 +1,23 @@
 package funar
 
+/*
+DSL for database access / dependency injection
+key-value store: String |-> Int
+
+put("Mike", 15)
+x = get("Mike")
+put("Mike", x+1)
+y = get("Mike")
+return (x + y)
+*/
+
+sealed trait DBCommand
+case class Put(key: String, value: Int) extends DBCommand
+case class Get(key: String) extends DBCommand
+case class Return(result: Int) extends DBCommand
+
+type DBProgram = List[DBCommand]
+
 trait Functor[F[_]] { // [_]: F is a type constructor
   def map[A, B](f: A => B, x: F[A]): F[B]
 }
