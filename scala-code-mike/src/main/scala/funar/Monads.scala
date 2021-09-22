6d50146251c1ffa57247c055b520d73c8bef08c5
Mike Sperber <sperber@deinprogramm.de>
2021-09-22 15:31:55 +0200

    Update.

--- scala-code-mike/src/main/scala/funar/Monads.scala
+++ scala-code-mike/src/main/scala/funar/Monads.scala
@@ -27,7 +27,8 @@ object DB {
   // trick: continuation
   // continuation-passing style / CPS
   sealed trait DB[A] {
-    
+    def flatMap[B](next: A => DB[B]): DB[B] = splice(this)(next)
+
   }
   case class Get[A](key: String, callback: Int => DB[A]) extends DB[A]
   // Unit only has one single member ()
