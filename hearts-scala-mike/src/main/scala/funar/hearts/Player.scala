f5e6ab0994861a2febee3a0dacf6ca575d2215af
Mike Sperber <sperber@deinprogramm.de>
2021-09-22 16:40:37 +0200

    Update.

--- hearts-scala-mike/src/main/scala/funar/hearts/Player.scala
+++ hearts-scala-mike/src/main/scala/funar/hearts/Player.scala
@@ -1,5 +1,10 @@
 package funar.hearts
 
+import cats.data._
+import org.atnos.eff._
+import org.atnos.eff.all._
+import org.atnos.eff.syntax.all._
+
 case class Player(id: String, name: String) {
   override def equals(other: Any): Boolean =
     other match {
