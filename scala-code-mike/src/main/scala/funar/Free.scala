aba99afa43c2642121f22ac6b4f6e7b5c2ef7fd8
Mike Sperber <sperber@deinprogramm.de>
2021-09-22 15:57:34 +0200

    Update.

--- scala-code-mike/src/main/scala/funar/Free.scala
+++ scala-code-mike/src/main/scala/funar/Free.scala
@@ -1,5 +1,14 @@
 package funar
 
 object Free {
-  
+/*
+  sealed trait Reader[Env, A]
+  case class Ask[Env, A](callback: Env => Reader[Env, A]) extends Reader[Env, A]
+  case class ReturnR[Env, A](result: A) extends Reader[Env, A]
+*/
+
+case class Ask
+
+
+
 }
