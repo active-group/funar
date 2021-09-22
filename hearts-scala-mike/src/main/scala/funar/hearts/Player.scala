953e7c68554576a1ab2d67f2cb9b1249ec4b1614
Mike Sperber <sperber@deinprogramm.de>
2021-09-22 16:49:46 +0200

    Update.

--- hearts-scala-mike/src/main/scala/funar/hearts/Player.scala
+++ hearts-scala-mike/src/main/scala/funar/hearts/Player.scala
@@ -47,9 +47,102 @@ object Player {
       case GameEnded(winner) => state
     }
 
-    // Eff: free monad from the eff library
-    type EventProcessor[Effects, Event, Command] = Event => Eff[Effects, Seq[Command]]
+  // Eff: free monad from the eff library
+  type EventProcessor[Effects, Event, Command] = Event => Eff[Effects, Seq[Command]]
 
-    type Strategy[Effects] = Player => EventProcessor[Effects, GameEvent, GameCommand]
+  type Strategy[Effects] = Player => EventProcessor[Effects, GameEvent, GameCommand]
+
+  type Chooser[PlayerId, Effects] = (Player, PlayerState[PlayerId]) => Eff[Effects, Card]
+
+  def chooserStrategy[PlayerId, Effects](chooser: Chooser[PlayerId, Effects])
+          (implicit member: State[PlayerState[PlayerId], *] |= Effects): Strategy[Effects] = {
+    import GameCommand._
+    (player: Player) => 
+      (event: GameEvent) =>
+        for {
+          _ <- playerProcessEventM(player, event)
+          playerState <- get[Effects, PlayerState[PlayerId]]
+          commands <- event match {
+            case HandDealt(player1, hand) =>
+              if ((player == player1) && hand.contains(Card(Suit.Clubs, Rank.Two)))
+                Eff.pure[Effects, Seq[GameCommand]](Seq(PlayCard(player, Card(Suit.Clubs, Rank.Two))))
+              else
+                Eff.pure[Effects, Seq[GameCommand]](Seq.empty)
+            case PlayerTurnChanged(player1) =>
+              if (player == player1)
+                for { card <- chooser(player, playerState) } yield Seq(PlayCard(player, card))
+              else
+                Eff.pure[Effects, Seq[GameCommand]](Seq.empty)
+            case LegalCardPlayed(player1, card) => Eff.pure[Effects, Seq[GameCommand]](Seq.empty)
+            case IllegalCardPlayed(player1, card) => Eff.pure[Effects, Seq[GameCommand]](Seq.empty)
+            case TrickTaken(player1, trick) => Eff.pure[Effects, Seq[GameCommand]](Seq.empty)
+            case GameEnded(winner) => Eff.pure[Effects, Seq[GameCommand]](Seq.empty)
+          }
+
+        } yield commands
+  }
+
+  def chooseAlong[PlayerId, Effects]: Chooser[PlayerId, Effects] = {
+    (_, playerState) =>
+    if (Trick.isEmpty(playerState.trick))
+      Eff.pure[Effects, Card](playerState.hand.minBy(_.rank))  
+    else {
+      val firstCard = Trick.leadingCard(playerState.trick)
+      val followingCardsOnHand = playerState.hand.filter { _.suit == firstCard.suit }
+      followingCardsOnHand.minByOption(_.rank) match {
+        case None => Eff.pure[Effects, Card](playerState.hand.maxBy(_.rank))
+        case Some(card) => Eff.pure[Effects, Card](card)
+      }
+    } 
+  }
+
+  def alongStrategy[PlayerId, Effects](implicit member: State[PlayerState[PlayerId], *] |= Effects): Strategy[Effects] =
+    chooserStrategy(chooseAlong[PlayerId, Effects])
+
+  def chooseInteractive[Effects, PlayerId](implicit member: Teletype[*] |= Effects): Chooser[PlayerId, Effects] = {
+    (player, playerState) =>
+      for {
+        _ <- Teletype.writeTTY("Your turn, player " ++ player.name)
+        handList = playerState.hand.toList
+        _ <- if (Trick.isEmpty(playerState.trick))
+                Teletype.writeTTY("You lead the next trick.")
+             else
+                Teletype.writeTTY("Trick: " ++ Trick.pretty(playerState.trick))
+        _ <- Teletype.writeTTY("Your hand:\n" ++ prettyCards(handList))
+        ncards = playerState.hand.size
+        _ <- Teletype.writeTTY("Pick a card (1-"  ++ ncards.toString ++ ")")
+        selected <- getNumber(1, ncards)
+      } yield handList(selected-1)
+  }
+
+  def getNumber[Effects](lo: Int, hi: Int)(implicit member: Teletype |= Effects): Eff[Effects, Int] =
+    for {
+      s <- Teletype.readTTY
+      input = s.toInt
+      n <- if ((lo <= input) && (input <= hi))
+             Eff.pure[Effects, Int](input)
+           else
+            for {
+              _ <- Teletype.writeTTY("Input must be between " ++ lo.toString ++ " and " ++ hi.toString ++ ". Try again.")
+              n <- getNumber(lo, hi)
+            } yield n
+    } yield n
+
+  def prettyCards(cards: List[Card]): String = {
+    def prettyOne(n: Integer, card: Card): String = "(" ++ n.toString ++ ")" ++ Trick.prettyCard(card)
+    def loop(n: Integer, cards: List[Card]): String =
+      cards match {
+        case Nil => ""
+        case List(card) => prettyOne(n, card)
+        case card::cards => prettyOne(n, card) ++ "\n" ++ loop(n+1, cards)
+      }
+    loop(1, cards)
+  }
+
+  def interactiveStrategy[PlayerId, Effects]
+    (implicit ttyState: Teletype |= Effects,
+              memberState: State[PlayerState[PlayerId], *] |= Effects): Strategy[Effects] =
+    chooserStrategy(chooseInteractive)
+
 
 }
