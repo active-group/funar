7ba52821f3f32e73a6b5397f19bf95e21631efb8
Mike Sperber <sperber@deinprogramm.de>
2021-09-23 15:03:29 +0200

    Update.

--- scala-code-mike/src/main/scala/funar/Contracts.scala
+++ scala-code-mike/src/main/scala/funar/Contracts.scala
@@ -11,4 +11,9 @@ case object CHF extends Currency
 case object EUR extends Currency
 case object GBP extends Currency
 
+case class Date(desc: String) extends Ordered[Date] {
+  def compare(that: Date): Int =
+    this.desc.compare(that.desc)
+}
+
 case class ZeroCouponBond(amount: Double, currency: Currency, date: Date)
