99c84a6b4f58e933ecfc9ea269e6f5049cc628bd
Mike Sperber <sperber@deinprogramm.de>
2021-09-23 11:24:38 +0200

    Update.

--- hearts-scala-mike/src/main/scala/funar/json/Decoder.scala
+++ hearts-scala-mike/src/main/scala/funar/json/Decoder.scala
@@ -96,7 +96,7 @@ object Decode {
     }
 
   // parsing & validation
-  def map2[A, B, C](a: Decoder[A], b: Decoder[B])(f: (A, B) => C)
+  def map2[A, B, C](a: Decoder[A], b: Decoder[B])(f: (A, B) => C): Decoder[C] = ???
 
   implicit val decoderMonad: Monad[Decoder] = new Monad[Decoder] {
     def flatMap[A, B](decoder: Decoder[A])(f: A => Decoder[B]): Decoder[B] = { Json =>
