229ddba1b546eef82a0c1d75df8fe0e355db06dc
Mike Sperber <sperber@deinprogramm.de>
2021-12-14 09:21:10 +0100

    Update.

--- vorkurs/reverse.rkt
+++ vorkurs/reverse.rkt
@@ -1,3 +1,31 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
-#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname reverse) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+#reader(lib "vanilla-reader.rkt" "deinprogramm" "sdp")((modname reverse) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Reihenfolge der Elemente einer Liste umdrehen
+(: rev ((list-of %a) -> (list-of %a)))
+
+(check-expect (rev (list 1 2 3))
+              (list 3 2 1))
+
+(define rev
+  (lambda (list)
+    (cond
+      ((empty? list) empty)
+      ((cons? list)
+       (append-element
+        (rev (rest list))
+        (first list))))))
+
+(: append-element ((list-of %a) %a -> (list-of %a)))
+
+(check-expect (append-element (list 3 2) 1)
+              (list 3 2 1))
+
+(define append-element
+  (lambda (list element)
+    (cond
+      ((empty? list) (cons element empty))
+      ((cons? list)
+       (cons
+        (first list) ; 3
+        (append-element (rest list) element))))))
