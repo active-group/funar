a1a9904620cb065279e015918b1523a1bda9a548
Mike Sperber <sperber@deinprogramm.de>
2021-12-14 09:33:37 +0100

    Update.

--- vorkurs/reverse.rkt
+++ vorkurs/reverse.rkt
@@ -4,7 +4,7 @@
 ; Reihenfolge der Elemente einer Liste umdrehen
 (: rev ((list-of %a) -> (list-of %a)))
 
-(check-expect (rev (list 1 2 3))
+#;(check-expect (rev (list 1 2 3))
               (list 3 2 1))
 
 (define rev
@@ -18,7 +18,7 @@
 
 (: append-element ((list-of %a) %a -> (list-of %a)))
 
-(check-expect (append-element (list 3 2) 1)
+#;(check-expect (append-element (list 3 2) 1)
               (list 3 2 1))
 
 (define append-element
@@ -29,3 +29,22 @@
        (cons
         (first list) ; 3
         (append-element (rest list) element))))))
+
+; 1 + 2 + 3 + ... + (n-1) + n
+; (n + 1) * n/2 = n^2 + ... = O(n^2)
+
+; rev, aber mit Zwischenergebnis
+(: rev1 ((list-of %a) (list-of %a) -> (list-of %a)))
+
+(check-expect (rev1 (list 1 2 3) empty)
+              (list 3 2 1))
+
+; "acc ist die Liste der schon gesehenen Elemente, umgedreht."
+
+(define rev1
+  (lambda (list acc)
+    (cond
+      ((empty? list) acc)
+      ((cons? list)
+       (rev1 (rest list) (cons (first list) acc))))))
+                    
