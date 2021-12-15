493f4ae60cd64d2fe4a719a59f47e1dfd7f14532
Mike Sperber <sperber@deinprogramm.de>
2021-12-15 11:24:03 +0100

    Update.

--- haskell-code-mike/DB.hs
+++ haskell-code-mike/DB.hs
@@ -1,2 +1,23 @@
 module DB where
 
+{-
+put "Mike" 50
+x = get "Mike"
+put "Mike" (x + 1)
+y = get "Mike"
+return (x + y)
+-}
+{-
+data DBCommand =
+    Put String Integer
+  | Get String 
+
+type DBProgram = [DBCommand]
+
+p1 :: [DBCommand]
+p1 = [Put "Mike" 50, Get "Mike" ] -- können Ergebnis keinen Namen geben: fail
+-}
+
+data DB =
+    Get String (Integer -> DB)
+  | Put String Integer (() -> DB)
