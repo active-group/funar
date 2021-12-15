9e575e26429390f7dfc87916a711cb2d40a27794
Mike Sperber <sperber@deinprogramm.de>
2021-12-15 13:42:23 +0100

    Hearts

---
+++ hearts/src/Cards.hs
@@ -0,0 +1,105 @@
+-- Das französische Blatt
+module Cards where
+
+import qualified Data.Set as Set
+import Data.Set (Set)
+
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map, (!))
+
+data Suit = Diamonds | Clubs | Spades | Hearts
+  deriving (Show, Eq, Ord)
+
+-- |Liste aller Farben
+allSuits :: [Suit]
+allSuits = [Spades, Hearts, Diamonds, Clubs]
+
+data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten
+         | Jack | Queen | King | Ace
+  deriving (Show, Eq, Ord)
+
+-- |Liste aller Werte
+allRanks :: [Rank]
+allRanks = [Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, 
+            Jack, Queen, King, Ace]
+
+-- |Spielkarte
+data Card = Card { suit :: Suit, rank :: Rank }
+  deriving (Show, Eq, Ord)
+
+-- |Eine Karte kann nur eine andere Karte gleicher Farbe nach Wert schlagen
+cardBeats :: Card -> Card -> Maybe Bool
+cardBeats c1 c2 = 
+  if suit c1 == suit c2
+  then Just (rank c1 > rank c2)
+  else Nothing
+
+cartesianProduct :: [a] -> [b] -> [(a, b)]
+cartesianProduct list1 list2 =
+  concat (map (\ el1 -> map (\ el2 -> (el1, el2)) list2) list1)
+
+-- |Das ganze Kartenspiel
+deck :: [Card]
+deck = map (uncurry Card) (cartesianProduct allSuits allRanks)
+
+-- |Karten, die jemand auf der Hand hält
+type Hand = Set Card
+
+-- |Ist die Hand leer?
+isHandEmpty :: Hand -> Bool
+isHandEmpty hand = Set.null hand
+
+-- |Enthält die Hand eine spezifische Karte?
+containsCard :: Card -> Hand -> Bool
+containsCard card hand = Set.member card hand
+
+-- |Karte aus der Hand entfernen
+removeCard :: Card -> Hand -> Hand
+removeCard card hand = Set.delete card hand
+
+-- |Leere Hand
+emptyHand :: Hand
+emptyHand = Set.empty
+
+-- Spieler
+
+data Player = Player { playerId :: String, playerName :: String }
+  deriving Show
+
+instance Eq Player where
+  Player id1 _ == Player id2 _ = id1 == id2
+
+instance Ord Player where
+  compare (Player id1 _) (Player id2 _) = compare id1 id2
+
+-- * Stich
+
+-- Letzte Karte liegt oben
+type Trick = [(Player, Card)]
+
+-- leeren Stich herstellen
+emptyTrick :: Trick
+emptyTrick = []
+
+-- ist Stich leer
+trickEmpty :: Trick -> Bool
+trickEmpty trick = null trick
+
+-- wie groß ist der Stich?
+trickSize :: Trick -> Int
+trickSize trick = length trick
+
+-- alle Karten desStich
+cardsOfTrick :: Trick -> [Card]
+cardsOfTrick trick = map snd trick
+
+-- Karte auf den Stich legen
+addToTrick :: Player -> Card -> Trick -> Trick
+addToTrick player card trick = (player, card) : trick
+
+-- die Karte des Stich, die bedient werden muß
+leadingCardOfTrick :: Trick -> Card
+leadingCardOfTrick trick = snd (last trick)
+
+-- Hände der Spieler
+type PlayerHands  = Map Player Hand
---
+++ hearts/test/HeartsJsonSpec.hs
@@ -0,0 +1,146 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+module HeartsJsonSpec (spec) where
+
+import Cards
+import qualified Data.Aeson as Json
+import Data.ByteString.Lazy (ByteString)
+import Data.Either.Combinators (mapLeft)
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import GameEvent
+import HeartsJson
+import Json.Decode (DecodeError (..), Decoder (..))
+import qualified Json.Decode as Decode
+import Test.Hspec
+import Test.QuickCheck
+
+instance Arbitrary Suit where
+  arbitrary = oneof $ map pure allSuits
+
+instance Arbitrary Rank where
+  arbitrary = oneof $ map pure allRanks
+
+instance Arbitrary Card where
+  arbitrary = Card <$> arbitrary <*> arbitrary
+
+instance Arbitrary Player where
+  arbitrary = Player <$> arbitrary <*> arbitrary
+
+instance Arbitrary GameEvent where
+  arbitrary =
+    oneof
+      [ HandDealt <$> arbitrary <*> arbitrary,
+        PlayerTurnChanged <$> arbitrary,
+        LegalCardPlayed <$> arbitrary <*> arbitrary,
+        TrickTaken <$> arbitrary <*> arbitrary,
+        IllegalCardPlayed <$> arbitrary <*> arbitrary,
+        GameEnded <$> arbitrary
+      ]
+
+instance Arbitrary GameCommand where
+  arbitrary =
+    oneof
+      [ DealHands <$> arbitrary,
+        PlayCard <$> arbitrary <*> arbitrary
+      ]
+
+decode :: Decoder a -> ByteString -> Either (Either String DecodeError) a
+decode decoder bs =
+  case Json.eitherDecode bs of
+    Left err -> Left (Left err)
+    Right json -> mapLeft Right (runDecoder decoder json)
+
+jsonRoundtrip :: Eq a => Decoder a -> (a -> Json.Value) -> a -> Bool
+jsonRoundtrip decoder encode v =
+  runDecoder decoder (encode v) == Right v
+
+spec :: Spec
+spec = do
+  mapDecoderSpec
+  dataDecoderSpec
+  dealHandsDecoderSpec
+  gameCommandDecoderSpec
+  describe "decode . encode ~= identity" $ do
+    it "Card" $
+      property (jsonRoundtrip cardDecoder encodeCard)
+    it "Player" $
+      property (jsonRoundtrip playerDecoder encodePlayer)
+    it "Hand" $
+      property (jsonRoundtrip handDecoder encodeHand)
+    it "Trick" $
+      property (jsonRoundtrip trickDecoder encodeTrick)
+    -- this does not fulfill the property due to the special "bonzo" case:
+    -- it "GameCommand" $
+    --   property (jsonRoundtrip gameCommandDecoder encodeGameCommand)
+    it "GameEvent" $
+      property (jsonRoundtrip gameEventDecoder encodeGameEvent)
+
+mapDecoderSpec :: Spec
+mapDecoderSpec =
+  describe "mapDecoder" $ do
+    it "correctly decodes a single sample value" $ do
+      let s =
+            "[[{\"playerName\" : \"bonzo\", \"playerId\" : \"1\"},\
+            \  [{\"suit\" : \"Diamonds\", \"rank\" : \"Eight\"},\
+            \   {\"suit\" : \"Clubs\", \"rank\" : \"Eight\"}]]]"
+          d = mapDecoder playerDecoder (Decode.list cardDecoder)
+          expected =
+            Map.fromList
+              [ ( Player "1" "bonzo",
+                  [Card Diamonds Eight, Card Clubs Eight]
+                )
+              ]
+      decode d s `shouldBe` Right expected
+
+dataDecoderSpec :: Spec
+dataDecoderSpec =
+  describe "dataDecoder" $ do
+    it "correctly decodes a single sample value" $ do
+      let d =
+            dataDecoder
+              ( Map.fromList
+                  [ ("i", show <$> Decode.int),
+                    ("s", Decode.string)
+                  ]
+              )
+              (\tag -> Decode.faild ("Unknown tag: " ++ tag))
+      decode d "{\"tag\" : \"s\", \"contents\" : \"foo\"}" `shouldBe` Right "foo"
+
+dealHandsDecoderSpec :: Spec
+dealHandsDecoderSpec =
+  describe "dealHandsDecoder" $ do
+    it "correctly decodes a single sample value" $ do
+      let s =
+            "[[{\"playerName\" : \"bonzo\", \"playerId\" : \"1\"},\
+            \  [{\"suit\" : \"Diamonds\", \"rank\" : \"Eight\"},\
+            \   {\"suit\" : \"Clubs\", \"rank\" : \"Eight\"}]]]"
+          expected =
+            DealHands
+              ( Map.fromList
+                  [ ( Player "1" "bonzo",
+                      Set.fromList [Card Diamonds Eight, Card Clubs Eight]
+                    )
+                  ]
+              )
+      decode dealHandsDecoder s `shouldBe` Right expected
+
+gameCommandDecoderSpec :: Spec
+gameCommandDecoderSpec =
+  describe "gameCommandDecoderSpec" $ do
+    it "correctly identifies and decodes a single DealHands value" $ do
+      let s =
+            "{\"tag\" : \"DealHands\",\
+            \ \"contents\" : [[{\"playerName\" : \"bonzo\", \"playerId\" : \"1\"},\
+            \                  [{\"suit\" : \"Diamonds\", \"rank\" : \"Eight\"},\
+            \                   {\"suit\" : \"Clubs\", \"rank\" : \"Eight\"}]]]}"
+          expected =
+            DealHands
+              ( Map.fromList
+                  [ ( Player "1" "bonzo",
+                      Set.fromList [Card Diamonds Eight, Card Clubs Eight]
+                    )
+                  ]
+              )
+      decode gameCommandDecoder s `shouldBe` Right expected
