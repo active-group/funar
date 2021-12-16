4a8e1cf4b33b48e6d6c1564f2bac29e736b26f65
Mike Sperber <sperber@deinprogramm.de>
2021-12-16 14:03:41 +0100

    Update.

--- haskell-code-mike/Contract.hs
+++ haskell-code-mike/Contract.hs
@@ -1,2 +1,22 @@
 module Contract where
 
+{-
+Einfaches Beispiel:
+"Ich bekomme am 24.12.2021 100EUR."
+Zero-Coupon Bond
+
+
+-}
+
+type Amount = Double 
+
+data Currency = EUR | USD | GBP | YEN
+  deriving Show
+
+data Date = Date String 
+  deriving (Show, Eq, Ord)
+
+data Contract =
+    ZeroCouponBond Date Amount Currency
+
+zcb1 = ZeroCouponBond (Date "2021-12-24") 100 EUR
