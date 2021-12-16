e71f946d713167f123aff2be18717dc41f3ebca4
Mike Sperber <sperber@deinprogramm.de>
2021-12-16 14:16:46 +0100

    Update.

--- haskell-code-mike/Contract.hs
+++ haskell-code-mike/Contract.hs
@@ -31,4 +31,12 @@ zcb1 = ZeroCouponBond (Date "2021-12-24") 100 EUR
 data Contract =
     One Currency
   | Multiple Amount Contract
+  | Later Date Contract
   deriving Show
+
+zcb1 = Later (Date "2021-12-24") (Multiple 100 (One EUR))
+
+zeroCouponBond date amount currency =
+    Later date (Multiple amount (One currency))
+
+zcb1' = zeroCouponBond (Date "2021-12-24") 100 EUR
