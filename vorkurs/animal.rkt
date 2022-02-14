2adfd6cbc84b68ed36857a9acba41a72bcb8f255
Mike Sperber <sperber@deinprogramm.de>
2022-02-14 13:23:21 +0100

    Update.

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -4,6 +4,7 @@
 ; Gürteltier hat folgende Eigenschaften:
 ; - lebendig oder tot
 ; - Gewicht
+; ^^^ Zustand des Gürteltiers zu einem bestimmten Zeitpunkt
 ; zusammengesetzte Daten
 (define-record dillo
   make-dillo
@@ -14,5 +15,20 @@
 (: dillo-alive? (dillo -> boolean))
 (: dillo-weight (dillo -> number))
 
+; lebendiges Gürteltier, 10kg
+(define dillo1 (make-dillo #t 10))
+; totes Gürteltier, 8kg
+(define dillo2 (make-dillo #f 8))
+
 ; Gürteltier überfahren
 ; Java: void runOver() { this.alive = false; }
+(: run-over-dillo (dillo -> dillo))
+
+(check-expect (run-over-dillo dillo1)
+              (make-dillo #f 10))
+(check-expect (run-over-dillo dillo2) dillo2)
+
+(define run-over-dillo
+  (lambda (dillo)
+    (make-dillo #f (dillo-weight dillo))))
+    
