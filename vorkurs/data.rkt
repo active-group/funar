5284385dd83ee6da0a92717f412c2fbe72521fce
Mike Sperber <sperber@deinprogramm.de>
2022-02-14 11:05:06 +0100

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -1,1 +1,41 @@
 #lang deinprogramm/sdp/beginner
+; Datenanalyse
+; Ein Haustier ist eins der folgenden:
+; - Hund - ODER -
+; - Katze - ODER -
+; - Schlange
+; Fallunterscheidung
+; Spezialfall: Aufzählung
+; Datendefinition ---> Code
+(define pet
+  (signature (enum "dog" "cat" "snake")))
+
+
+; Ist ein Haustier niedlich?
+(: cute? (pet -> boolean))
+
+(check-expect (cute? "dog") #t)
+(check-expect (cute? "cat") #t)
+(check-expect (cute? "snake") #f)
+
+; Gerüst
+#;(define cute?
+  (lambda (pet)
+    ...))
+
+; Schablone
+#;(define cute?
+  (lambda (pet)
+    (cond ; Verzweigung
+      ; Zweig (<Bedingung> <Antwort>)
+      ((string=? pet "dog") ...)
+      ((string=? pet "cat") ...)
+      ((string=? pet "snake") ...))))
+
+(define cute?
+  (lambda (pet)
+    (cond ; Verzweigung
+      ; Zweig (<Bedingung> <Antwort>)
+      ((string=? pet "dog") #t)
+      ((string=? pet "cat") #t)
+      ((string=? pet "snake") #f))))
