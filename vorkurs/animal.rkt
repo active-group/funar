c195eeef107d37af5f87d2dda3250bd03eeb6428
Mike Sperber <sperber@deinprogramm.de>
2022-02-14 13:16:28 +0100

    Update.

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -1,1 +1,18 @@
-#lang racket
+#lang deinprogramm/sdp/beginner
+; Tiere auf dem texanischen Highway
+
+; Gürteltier hat folgende Eigenschaften:
+; - lebendig oder tot
+; - Gewicht
+; zusammengesetzte Daten
+(define-record dillo
+  make-dillo
+  (dillo-alive? boolean)
+  (dillo-weight number))
+
+(: make-dillo (boolean number -> dillo))
+(: dillo-alive? (dillo -> boolean))
+(: dillo-weight (dillo -> number))
+
+; Gürteltier überfahren
+; Java: void runOver() { this.alive = false; }
