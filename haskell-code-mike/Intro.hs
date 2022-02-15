c1c2abcc154922147a5aa4c4e20d259d3059dee5
Mike Sperber <sperber@deinprogramm.de>
2022-02-15 11:52:29 +0100

    Update.

--- haskell-code-mike/Intro.hs
+++ haskell-code-mike/Intro.hs
@@ -14,7 +14,17 @@ data Pet = Dog | Cat | Snake
   deriving Show
 
 -- Ist Haustier niedlich?
+-- >>> isCute Dog
+-- True
+-- >>> isCute Snake
+-- False
 isCute :: Pet -> Bool
 isCute Dog = True
 isCute Cat = True
 isCute Snake = False
+
+-- Gürteltier hat folgende Eigenschaften:
+-- - lebendig oder tot
+-- - Gewicht
+data Liveness = Dead | Alive
+  deriving Show
