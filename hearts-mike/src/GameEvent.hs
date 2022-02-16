c5cbf3b9d23db4214e60a8b4cf19a0ed9fdab561
Mike Sperber <sperber@deinprogramm.de>
2022-02-16 12:16:22 +0100

    Update.

--- hearts-mike/src/GameEvent.hs
+++ hearts-mike/src/GameEvent.hs
@@ -18,7 +18,34 @@ import Cards
 -- "Wunsch, daß etwas in der Zukunft passiert"
 -- != Events
 
+{-
+data GameEvent =
+    CardPlayed Player Card
+  | GameStarted [Player]
+  | PlayerArrived Player 
+  | CardDealt Card Player 
+  | GameFinished (Map Player Integer)
+  | TrickTaken Player Trick
+  | CardsRotated Player Player [Card] -- "from" / "to"
+  | PlayerLeft Player 
+-}
+
 data GameEvent
+  = HandDealt Player Hand
+  | PlayerTurnChanged Player
+  | LegalCardPlayed Player Card
+  | TrickTaken Player Trick
+  | IllegalCardPlayed Player Card
+  | GameEnded Player
+  deriving (Eq, Show)
 
+
+{-
+data GameCommand =
+    PlayCard Player Card 
+  | StartGame PlayerHands
+-}
 data GameCommand
-
+  = DealHands PlayerHands
+  | PlayCard Player Card
+  deriving (Eq, Show)
