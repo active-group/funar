c21cfacd87b23d7f0ba3a6361d17f8b6b485b427
Mike Sperber <sperber@deinprogramm.de>
2022-03-24 10:42:45 +0100

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -3,3 +3,12 @@
 ; Konstruktionsanleitungen / design recipes
 
 ; Datenanalyse
+
+; Datendefinition
+
+; Ein Haustier ist eins der folgenden:
+; - Hund - ODER -
+; - Katze - ODER -
+; - Schlange
+; Fallunterscheidung
+; hier speziell: Aufzählung
