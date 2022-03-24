0900a456d2fbcd54e648a4de27aebe689185fad7
Mike Sperber <sperber@deinprogramm.de>
2022-03-24 11:09:07 +0100

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -61,9 +61,33 @@
 ; zusammengesetzte Daten
 (define-record time ; Signatur
   make-time ; Konstruktor
-  (time-hour natural)
+  (time-hour natural) ; Selektor
   (time-minute natural))
 
 ; natural: natürliche Zahlen, 0,1,2,3,4,5,6,...
 
 (: make-time (natural natural -> time))
+(: time-hour (time -> natural))
+(: time-minute (time -> natural))
+
+; 11 Uhr 2 Minuten
+(define time1 (make-time 11 2))
+; 14:05 Uhr
+(define time2 (make-time 14 5))
+
+; Minuten seit Mitternacht
+(: msm (time -> natural))
+
+(check-expect (msm time1) 662)
+(check-expect (msm time2) (+ (* 14 60) 5))
+
+; Schablone:
+#;(define msm
+  (lambda (time)
+    ... (time-hour time) ... (time-minute time) ...))
+
+(define msm
+  (lambda (time)
+    (+ (* (time-hour time) 60)
+       (time-minute time))))
+
