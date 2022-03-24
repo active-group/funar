aab95d4ae7fbd8fd6a1196decae0d6fb907a6979
Mike Sperber <sperber@deinprogramm.de>
2022-03-24 09:57:06 +0100

    Update.

--- vorkurs/image.rkt
+++ vorkurs/image.rkt
@@ -6,6 +6,23 @@
      (* 42 3)))
 
 (define circle1 (circle 50 "solid" "red"))
-(define square1 (square 50 "outline" "blue"))
+(define square1 (square 100 "outline" "blue"))
 (define star1 (star 50 "solid" "gold"))
 
+(define overlay1 (overlay star1 circle1))
+
+(above
+ (beside circle1 star1)
+ (beside star1 circle1))
+
+(above
+ (beside square1 circle1)
+ (beside circle1 square1))
+
+; Abstraktion
+
+(define tile
+  (lambda (image1 image2)
+    (above
+     (beside image1 image2)
+     (beside image2 image1))))
