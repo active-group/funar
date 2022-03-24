ea2dd6c4a2011468378c207761b2aed36c8a811d
Mike Sperber <sperber@deinprogramm.de>
2022-03-24 10:52:42 +0100

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -18,3 +18,34 @@
   (signature (enum "dog" "cat" "snake")))
 
 ; Ist ein Haustier niedlich?
+(: cute? (pet -> boolean))
+
+(check-expect (cute? "dog") #t)
+(check-expect (cute? "cat") #t)
+(check-expect (cute? "snake") #f)
+
+; Gerüst
+#;(define cute?
+  (lambda (pet)
+    ...))
+
+; Schablone: entsteht aus der Signatur und den Daten
+#;(define cute?
+  (lambda (pet)
+    ; Eingabe: pet, Fallunterscheidung
+    ; brauchen: Verzweigung, 1 Zweig pro Fall
+    (cond
+      ; jeder Zweig: (<Bedingung> <Antwort>)
+      ((string=? pet "dog") ...)
+      ((string=? pet "cat") ...)
+      ((string=? pet "snake") ...))))
+
+(define cute?
+  (lambda (pet)
+    ; Eingabe: pet, Fallunterscheidung
+    ; brauchen: Verzweigung, 1 Zweig pro Fall
+    (cond
+      ; jeder Zweig: (<Bedingung> <Antwort>)
+      ((string=? pet "dog") #t)
+      ((string=? pet "cat") #t)
+      ((string=? pet "snake") #f))))
