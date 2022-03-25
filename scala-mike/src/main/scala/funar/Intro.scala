6c75ecc591948c5643b2ede148299ffd1ee4b3ab
Mike Sperber <sperber@deinprogramm.de>
2022-03-25 10:23:55 +0100

    Update.

--- scala-mike/src/main/scala/funar/Intro.scala
+++ scala-mike/src/main/scala/funar/Intro.scala
@@ -71,8 +71,8 @@ type Weight = Double
 
 case class Dillo(liveness: Liveness, weight: Weight)
 
-val dillo1 = Dillo(Alive, 10)
-val dillo2 = Dillo(Dead, 8)
+val dillo1 = Dillo(Liveness.Alive, 10)
+val dillo2 = Dillo(Liveness.Dead, 8)
 
 // run over an armadillo
 def runOverDillo(dillo: Dillo): Dillo =
