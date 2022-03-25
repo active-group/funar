23894590d27999cc76472b11062b4cf8b40c03e4
Mike Sperber <sperber@deinprogramm.de>
2022-03-25 14:00:14 +0100

    Update.

--- scala-mike/src/main/scala/funar/Intro.scala
+++ scala-mike/src/main/scala/funar/Intro.scala
@@ -277,7 +277,7 @@ def rev[A](list: List[A], acc: List[A]): List[A] =
 
 def revLoop[A](list0: List[A]): List[A] = {
   var list = list0
-  val acc = Nil
+  val acc: List[A] = Nil
   while (list != Nil) {
     list = list.tail
     acc = list.head :: acc
