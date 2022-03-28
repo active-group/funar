4e1b0ced8bee3bc4e5cf0c451ff60501983f31d8
Mike Sperber <sperber@deinprogramm.de>
2022-03-28 16:33:21 +0200

    Update.

--- scala-mike/src/main/scala/funar/DB.scala
+++ scala-mike/src/main/scala/funar/DB.scala
@@ -1,3 +1,19 @@
 package funar
 
+// Datenbank-Zugriff funktional
 
+// put("Mike", 50)
+// x = get("Mike")
+// put("Mike", x+1)
+// y = get("Mike")
+// return (x + y)
+
+// 1. Idee: weg von "Funktion macht was"
+// hin zu: Funktion generiert eine Beschreibung von dem,
+// was sie machen will
+
+enum DBCommand[A] {
+  case Put(key: String, value: Int)
+  case Get(key: String)
+  case Return(result: A)
+}
