fb2a5b388b56ce60b00e390907f95b53f68380df
Mike Sperber <sperber@deinprogramm.de>
2022-03-28 16:38:25 +0200

    Update.

--- scala-mike/src/main/scala/funar/DB.scala
+++ scala-mike/src/main/scala/funar/DB.scala
@@ -6,14 +6,25 @@ package funar
 // x = get("Mike")
 // put("Mike", x+1)
 // y = get("Mike")
-// return (x + y)
+// return ((x + y).toString)
 
 // 1. Idee: weg von "Funktion macht was"
 // hin zu: Funktion generiert eine Beschreibung von dem,
 // was sie machen will
 
+/*
 enum DBCommand[A] {
   case Put(key: String, value: Int)
   case Get(key: String)
   case Return(result: A)
+}
+
+type DBProgram[A] = List[DBCommand[A]]
+
+val p1 = List(Put("Mike", 50),
+              Get("Mike"),
+              // hier geht's nicht mehr funktioniert
+*/
+enum DB[A] {
+  case Get(key: String, callback: Int => DB[A])
 }
