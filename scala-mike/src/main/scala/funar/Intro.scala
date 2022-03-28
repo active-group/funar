eb0f42034fb6e578477bc25d700c84f1cf31e75b
Mike Sperber <sperber@deinprogramm.de>
2022-03-28 16:18:58 +0200

    Update.

--- scala-mike/src/main/scala/funar/Intro.scala
+++ scala-mike/src/main/scala/funar/Intro.scala
@@ -260,7 +260,7 @@ def optionMap[A, B]        (f: A => B, option: Option[A]): Option[B] =
 
 // F ist sowas wie List, Option
 trait Functor[F[_]] {
-  extension (c: F[A])
+  extension [A] (c: F[A])
     def fmap(f: A => B): F[B]
 }
 
