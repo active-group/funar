29626a266f9d2426d480d0f335ce4037f709b208
Mike Sperber <sperber@deinprogramm.de>
2022-03-29 15:20:20 +0200

    Update.

---
+++ scala-mike/src/main/scala/funar/Contract.scala
@@ -0,0 +1,53 @@
+package funar
+
+import javax.swing.plaf.BorderUIResource.EmptyBorderUIResource
+
+/*
+Finanzderivate:
+
+Zins-Swap
+in der Zukunft:
+bekomme festen Zinssatz
+zahle variablen Zinssatz
+
+2 Vertragspartner:innen
+
+1. finde ein einfaches Beispiel
+
+Rollover-Geschäft: heute was bekommen, morgen was mit Zinsen zahlen
+
+- heute was bekommen
+- morgen was mit Zinsen zahlen
+- morgen was (ohne Zinsen) zahlen
+- morgen was (ohne Zinsen) bekommen
+
+"Am 24.12. bekomme ich 100€": Zero-Bond, Zero-Coupon Bond
+
+3 Ideen:
+
+- € "Währung"
+- Vielfaches
+- Später
+*/
+
+case class Date(iso: String)
+
+type Amount = Double
+
+enum Currency {
+  case EUR 
+  case GBP
+  case USD
+}
+
+/*
+enum Contract {
+  case ZeroCouponBond(date: Date, amount: Amount, currency: Currency)
+}
+*/
+
+object Contract {
+  import Currency._
+  import Contract._
+  val zcb1 = ZeroCouponBond(Date("2022-12-24"), 100, EUR)
+}
