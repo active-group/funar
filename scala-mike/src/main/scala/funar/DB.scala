a3ec521a409c62b4417a28b888a5df892b2b3978
Mike Sperber <sperber@deinprogramm.de>
2022-03-29 10:33:32 +0200

    Update.

--- scala-mike/src/main/scala/funar/DB.scala
+++ scala-mike/src/main/scala/funar/DB.scala
@@ -33,7 +33,7 @@ enum DB[A] {
   case Return(result: A)
 
   def map[B](f: A => B): DB[B] =
-    this.flatMap { x => Return(x) }
+    this.flatMap { x => Return(f(x)) }
 
   // magischer Name
   // cont ~ "continuation"
