41d52cc2802bf33e1b8cfdb85657ec6e6d6f1a64
Mike Sperber <sperber@deinprogramm.de>
2022-09-19 13:44:48 +0200

    Update.

--- vorkurs/river.rkt
+++ vorkurs/river.rkt
@@ -1,3 +1,27 @@
 #lang deinprogramm/sdp/beginner
+; Ein Fluss ist eins der folgenden:
+; - Bach (kommt aus Quelle)
+; - Zusammentreffen (aus zwei Flüssen)
+(define river
+  (signature (mixed creek confluence)))
 
+; Ein Bach hat folgende Eigenschaften:
+; - Ursprungsort
+(define-record creek
+  make-creek creek?
+  (creek-origin string))
+
+; Ein Zusammentreffen hat folgende Eigenschaften:
+; - Ort
+; - Haupt*fluss*  <--- Selbstbezug
+; - Nebenfluss    <--- auch
+(define-record confluence
+  make-confluence confluence?
+  (confluence-location string)
+  (confluence-main-stem river)
+  (confluence-tributary river))
+
+(define eschach (make-creek "Heimliswald"))
+(define prim (make-creek "Dreifaltigkeitsberg"))
+(define neckar1 (make-confluence "Rottweil" eschach prim))
 
