c72487dffbdd1c585389cbd79c406f81d8739b5d
Mike Sperber <sperber@deinprogramm.de>
2022-09-19 09:59:20 +0200

    Update.

--- vorkurs/image.rkt
+++ vorkurs/image.rkt
@@ -9,3 +9,22 @@
 (define square1 (square 100 "outline" "green"))
 (define star1 (star 50 "solid" "blue"))
 (define overlay1 (overlay star1 circle1))
+
+#;(above
+ (beside circle1 star1)
+ (beside star1 circle1))
+
+#;(above
+ (beside square1 circle1)
+ (beside circle1 square1))
+
+; Konstruktionsanleitungen
+
+; Schritt 1: Kurzbeschreibung
+; quadratisches Kachelmuster erstellen
+
+(define tile
+  (lambda (image1 image2)
+    (above
+     (beside image1 image2)
+     (beside image2 image1))))
