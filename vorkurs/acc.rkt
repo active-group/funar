1a17d34cc247dd5ad8c1808ece66d22716720cce
Mike Sperber <sperber@deinprogramm.de>
2022-09-20 09:24:25 +0200

    Update.

--- vorkurs/acc.rkt
+++ vorkurs/acc.rkt
@@ -1,6 +1,6 @@
 #lang deinprogramm/sdp
 
-(define list-sum
+#;(define list-sum
   (lambda (list)
     (cond
       ((empty? list) 0)
@@ -10,13 +10,19 @@
 
 ; (list-sum (list 1 2 3 4))
 
-(define list-sum*
-  (lambda (list acc) ; acc ist das Zwischenergebnis / die Summe der "gesehenen" Elemente
-    (cond
-      ((empty? list) acc)
-      ((cons? list)
-       ; Aufruf ohne Kontext / tail call / endrekursiv
-       (list-sum* (rest list) (+ acc (first list)))))))
+(define list-sum
+  (lambda (list0)    
+    (define list-sum*
+      ; acc ist die Summe aller Elemente zwischen list0 und list
+      ; Schleifeninvariante
+      (lambda (list acc) ; acc ist das Zwischenergebnis / die Summe der "gesehenen" Elemente
+        (cond
+          ((empty? list) acc)
+          ((cons? list)
+           ; Aufruf ohne Kontext / tail call / endrekursiv
+           (list-sum* (rest list) (+ acc (first list)))))))
+    (list-sum* list0 0)))
+    
 
-(list-sum* (list 1 2 3 4) 0)
+(list-sum (list 1 2 3 4))
 
