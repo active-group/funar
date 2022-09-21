f75aef24bf84e7b60c15feeb313a0522ce5687cf
Mike Sperber <sperber@deinprogramm.de>
2022-09-21 09:22:36 +0200

    Update.

--- haskell-code-mike/DB.hs
+++ haskell-code-mike/DB.hs
@@ -7,6 +7,7 @@ y = get "Mike"
 return x + y
 -}
 
+{-
 data DBCommand a =
     Put String Integer
   | Get String
@@ -16,6 +17,9 @@ type DBProgram a = [DBCommand a]
 
 p1 :: DBProgram Integer
 p1 = [Put "Mike" 51, Get "Mike", Return 5 ] -- und nu? Wie dem Ergebnis einen Namen geben?
+-}
 
-
-
+type DB a =
+    Get String (Integer -> DB a)
+  | Put String Integer (() -> DB a)
+  | Return a
