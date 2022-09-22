3f87b8afbfcf58dd78082356de417ccb5b4175bc
Mike Sperber <sperber@deinprogramm.de>
2022-09-22 09:49:28 +0200

    Update.

--- haskell-code-mike/State.hs
+++ haskell-code-mike/State.hs
@@ -2,12 +2,12 @@ module State where
 
 import Prelude hiding (read)
 
-data State a =
-    Read (Integer -> State a)
-  | Write Integer (() -> State a)
+data State s a =
+    Read (s -> State s a)
+  | Write s (() -> State s a)
   | Return a
 
-instance Show a => Show (State a) where
+instance Show a => Show (State s a) where
     show (Read callback) = "Read"
     show (Write newState callback) =
         "Write " ++ (show newState)
@@ -15,17 +15,17 @@ instance Show a => Show (State a) where
         "Return " ++ (show result)
 
 
-read :: State Integer
+read :: State s s
 read = Read Return
 
-write :: Integer -> State ()
+write :: s -> State s ()
 write newValue = Write newValue Return
 
-instance Functor State where
+instance Functor (State s) where
 
-instance Applicative State where
+instance Applicative (State s) where
 
-instance Monad State where
+instance Monad (State s) where
     return = Return
     Read callback >>= next =
         Read (\value ->
@@ -36,7 +36,7 @@ instance Monad State where
     Return result >>= next = 
         next result
 
-p1 :: State String
+
 -- >>> p1
 -- Write 17
 p1 = do write 17
@@ -55,10 +55,12 @@ runState (Write newState callback) state =
 runState (Return result) state = result
 
 traceState :: State a -> Integer -> (a, [Integer])
+-- >>> traceState p1 0
+-- ("18",[17,18])
 traceState (Read callback) state =
     traceState (callback state) state
 traceState (Write newState callback) state =
     let (result, trace) = traceState (callback ()) newState
-    in (result, trace ++ [newState])
+    in (result, newState : trace)
 traceState (Return result) state = 
     (result, [])
