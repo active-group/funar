d371e97878536485106a91f8a690983c90907b80
Mike Sperber <sperber@deinprogramm.de>
2022-09-22 16:44:11 +0200

    Update.

--- haskell-code-mike/Contract.hs
+++ haskell-code-mike/Contract.hs
@@ -71,10 +71,41 @@ c5 = And c3 (Reverse c4)
 data Payment = Payment Direction Date Amount Currency
   deriving Show
 
+multiplyPayment :: Amount -> Payment -> Payment
+multiplyPayment factor (Payment dir date amount currency) =
+    Payment dir date (factor * amount) currency
+
+invertPayment :: Payment -> Payment
+invertPayment (Payment Long date amount currency) =
+    Payment Short date amount currency
+invertPayment (Payment Short date amount currency) =
+    Payment Long date amount currency
+
 -- im Paper:
 -- Later (Obs Amount) Contract
 -- Obs: (zeitabhängige) Beobachtung
 
 -- alle Zahlungen bis zum Datum
 semantics :: Contract -> Date -> ([Payment], Contract)
+semantics Zero now =
+    ([], Zero)
+semantics (One currency) now =
+    ([Payment Long now 1 currency], Zero)
+semantics (Multiple amount contract) now =
+    let (payments, contract') = semantics contract now
+    in (map (multiplyPayment amount) payments, Multiple amount contract')
+semantics (Reverse contract) now =
+    let (payments, contract') = semantics contract now
+    in (map invertPayment payments, Reverse contract')
+semantics (And contract1 contract2) now = 
+    let (payments1, contract1') = semantics contract1 now
+        (payments2, contract2') = semantics contract2 now
+    in (payments1 ++ payments2, And contract1' contract2')   
+semantics c@(Later date contract) now =
+    if now >= date
+    then -- Weihnachten
+      semantics contract now
+    else -- noch nicht Weihnachten
+      ([], c)
+
 
