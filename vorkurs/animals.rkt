92b1d346c90bd015e699e548402e280bbe6c0e34
Mike Sperber <sperber@deinprogramm.de>
2022-09-26 11:07:11 +0200

    Update.

--- vorkurs/animals.rkt
+++ vorkurs/animals.rkt
@@ -20,4 +20,13 @@
 ; totes Gürteltier, 8kg
 (define dillo2 (make-dillo #f 8))
 
+; Gürteltier überfahren
+(: run-over-dillo (dillo -> dillo))
+
+(check-expect (run-over-dillo dillo1)
+              (make-dillo #f 10))
+(check-expect (run-over-dillo dillo2)
+              dillo2)
+
+
 
