44d95c07c1151185b4bd0f02e87966fb21e925ce
Mike Sperber <sperber@deinprogramm.de>
2022-09-26 12:16:02 +0200

    Update.

--- vorkurs/river.rkt
+++ vorkurs/river.rkt
@@ -2,6 +2,8 @@
 ; Ein Fluss ist eins der folgenden:
 ; - Bach
 ; - Zusammenfluss
+(define river
+  (signature (mixed stream confluence)))
 
 ; Ein Bach hat folgende Eigenschaften:
 ; - Ursprungsort
@@ -13,4 +15,12 @@
 (define prim (make-stream "Dreifaltigkeitsberg"))
 (define schlichem (make-stream "Tieringen"))
 
-
+; Ein Zusammenfluss hat folgende Eigenschaften:
+; - Ort
+; - Hauptfluss
+; - Nebenfluss
+(define-record confluence
+  make-confluence confluence?
+  (confluence-location string)
+  (confluence-main-stem river)
+  (confluence-tributary river))
