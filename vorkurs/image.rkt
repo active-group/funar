a4ff1c5fef77fbcb3d74c5f41922461050e898d9
Mike Sperber <sperber@deinprogramm.de>
2022-09-26 09:44:27 +0200

    Update.

--- vorkurs/image.rkt
+++ vorkurs/image.rkt
@@ -1,6 +1,12 @@
 #lang deinprogramm/sdp/beginner
+(require deinprogramm/sdp/image)
+  
 (define x
   (+ 42
      (* 5
         23)))
 
+(define circle1 (circle 50 "solid" "red"))
+(define square1 (square 100 "outline" "blue"))
+(define star1 (star 50 "solid" "gold"))
+(define overlay1 (overlay star1 circle1))
