dcc7f41d867b322840825f1cc100de9ccdc6a01e
Mike Sperber <sperber@deinprogramm.de>
2022-09-26 11:02:04 +0200

    Update.

--- vorkurs/animals.rkt
+++ vorkurs/animals.rkt
@@ -1,2 +1,13 @@
 #lang deinprogramm/sdp/beginner
 
+; Tiere auf dem texanischen Highway
+
+; Gürteltier hat folgende Eigenschaften:
+; - lebendig oder tot - UND -
+; - Gewicht
+; zusammengesetzte Daten
+(define-record dillo
+  make-dillo
+  (dillo-alive? boolean)
+  (dillo-weight number))
+
