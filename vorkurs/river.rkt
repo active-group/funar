47a9d980343343d7bd0529baf60d7e628d5b1112
Mike Sperber <sperber@deinprogramm.de>
2022-09-26 12:14:22 +0200

    Update.

--- vorkurs/river.rkt
+++ vorkurs/river.rkt
@@ -1,1 +1,16 @@
-#lang racket
+#lang deinprogramm/sdp/beginner
+; Ein Fluss ist eins der folgenden:
+; - Bach
+; - Zusammenfluss
+
+; Ein Bach hat folgende Eigenschaften:
+; - Ursprungsort
+(define-record stream
+  make-stream stream?
+  (stream-origin string))
+
+(define eschach (make-stream "Heimliswald"))
+(define prim (make-stream "Dreifaltigkeitsberg"))
+(define schlichem (make-stream "Tieringen"))
+
+
