769b8a9df3ad249fbf6fa27cecfe3c5f09956573
Mike Sperber <sperber@deinprogramm.de>
2022-09-26 09:58:59 +0200

    Update.

--- vorkurs/image.rkt
+++ vorkurs/image.rkt
@@ -27,4 +27,20 @@
      (beside image1 image2)
      (beside image2 image1))))
 
-(tile star1 circle1)
+; (tile star1 circle1)
+
+
+#|
+class C {
+  static m(int x) {
+
+     ... x ...
+     x = x + 1;
+     ... x ...
+  }
+
+  ... C.m(17) ... => wird x durch 17 ersetzt?
+
+}
+
+|#
