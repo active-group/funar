f7a738661cd20730aea9201f056bc617bd127bab
Mike Sperber <sperber@deinprogramm.de>
2022-09-27 09:03:57 +0200

    Update.

--- vorkurs/acc.rkt
+++ vorkurs/acc.rkt
@@ -1,2 +1,12 @@
 #lang deinprogramm/sdp
 
+(define list-sum
+  (lambda (list)
+    (cond
+      ((empty? list) 0)
+      ((cons? list)
+       (+
+        (first list)
+        (list-sum (rest list)))))))
+
+(list-sum (list 1 2 3 4))
