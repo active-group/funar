009879528b5b2a0b9ac8c7be566a4452b992ec63
Mike Sperber <sperber@deinprogramm.de>
2022-09-27 09:14:56 +0200

    Update.

--- vorkurs/acc.rkt
+++ vorkurs/acc.rkt
@@ -1,6 +1,6 @@
 #lang deinprogramm/sdp
 
-(define list-sum
+#;(define list-sum
   (lambda (list)
     (cond
       ((empty? list) 0)
@@ -9,4 +9,33 @@
         (first list)
         (list-sum (rest list)))))))
 
-(list-sum (list 1 2 3 4))
+(check-expect (list-sum* (list 1 2 3 4) 0)
+              10)
+
+  #|
+  list = ...;
+  acc = 0;
+  while (list != empty) {
+    acc = (first list) + acc;
+    list = (rest list);
+  }
+  return acc;
+  |#
+
+(define list-sum
+  (lambda (list0)
+    (define list-sum*
+      ; acc: Summe der "gesehenen" Listenelemente
+      (lambda (list acc)
+        (cond
+          ((empty? list) acc)
+          ((cons? list)
+           (list-sum* (rest list)
+                      ; neues Zwischenergebnis aus dem alten und (first list) berechnen
+                      (+ (first list) acc))))))
+    (list-sum* list0 0)))
+    
+       
+    
+
+;(list-sum (list 1 2 3 4))
