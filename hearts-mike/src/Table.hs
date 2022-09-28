36d51b364976ad0e331d9d132c4283847b008083
Mike Sperber <sperber@deinprogramm.de>
2022-09-28 15:46:12 +0200

    Update.

--- hearts-mike/src/Table.hs
+++ hearts-mike/src/Table.hs
@@ -133,7 +133,7 @@ playCard :: PlayerHands -> Player -> Card -> PlayerHands
 playCard playerHands player card =
   Map.alter (fmap (removeCard card)) player playerHands
 
--- tableProcessEvent :: GameEvent -> TableState -> TableState
+
 
 -- Karten zum Stapel hinzufügen
 addToPile :: PlayerPiles -> Player -> [Card] -> PlayerPiles
@@ -141,3 +141,27 @@ addToPile playerPiles player cards =
   let playerPile = Map.findWithDefault Set.empty player playerPiles
   in Map.insert player (Set.union playerPile (Set.fromList cards)) playerPiles
 
+-- Ereignis in den Zustand einarbeiten
+tableProcessEvent :: GameEvent -> TableState -> TableState
+tableProcessEvent (HandDealt player hand) state =
+  state
+    { tableStateHands = Map.insert player hand (tableStateHands state),
+      tableStateTrick = emptyTrick
+    }
+tableProcessEvent (PlayerTurnChanged player) state =
+  state
+    { tableStatePlayers = rotateTo player (tableStatePlayers state)
+    }
+tableProcessEvent (LegalCardPlayed player card) state =
+  state
+    { tableStateHands = playCard (tableStateHands state) player card,
+      tableStateTrick = addToTrick player card (tableStateTrick state)
+    }
+tableProcessEvent (TrickTaken player trick) state =
+  state
+    { tableStatePiles =
+        addToPile (tableStatePiles state) player (cardsOfTrick trick),
+      tableStateTrick = emptyTrick
+    }
+tableProcessEvent (IllegalCardAttempted player card) state = state
+tableProcessEvent (GameEnded player) state = state
