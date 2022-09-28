2412c9347052d297511695e24230cac1af5be42f
Mike Sperber <sperber@deinprogramm.de>
2022-09-28 10:38:25 +0200

    Update.

--- haskell-code-mike/Cards.hs
+++ haskell-code-mike/Cards.hs
@@ -6,3 +6,20 @@ module Cards where
 -- Liste aller Spielkarten des französischen Blatts
 -- Denkt an die Listenfunktionen, die Ihr schon kennt!
 
+-- |Liste aller Farben
+allSuits :: [Suit]
+allSuits = [Spades, Hearts, Diamonds, Clubs]
+
+data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten
+         | Jack | Queen | King | Ace
+  deriving (Show, Eq, Ord)
+
+-- |Liste aller Werte
+allRanks :: [Rank]
+allRanks = [Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, 
+            Jack, Queen, King, Ace]
+
+-- |Spielkarte
+data Card = Card { suit :: Suit, rank :: Rank }
+  deriving (Show, Eq, Ord)
+
