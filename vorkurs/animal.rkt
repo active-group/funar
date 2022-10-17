9930c09f81f0f00750794b0de3e624e184cca184
Mike Sperber <sperber@deinprogramm.de>
2022-10-17 10:30:32 +0200

    Update.

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -1,1 +1,15 @@
-#lang racket
+#lang deinprogramm/sdp/beginner
+
+; Datenanalyse
+
+; Datendefinition (Kommentar)
+
+; Haustier ist eins der folgenden:
+; - Katze - ODER -
+; - Hund - ODER -
+; - Schlange
+; Fallunterscheidung
+; hier speziell: Aufzählung
+
+
+; Ist Haustier niedlich?
