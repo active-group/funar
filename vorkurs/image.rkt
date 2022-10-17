3b4fffcf796cf54ec99fbafa2e107547c3379dbf
Mike Sperber <sperber@deinprogramm.de>
2022-10-17 09:50:34 +0200

    Update.

--- vorkurs/image.rkt
+++ vorkurs/image.rkt
@@ -9,4 +9,20 @@
 (define circle1 (circle 50 "solid" "green"))
 (define square1 (square 100 "outline" "yellow"))
 (define star1 (star 50 "solid" "blue"))
-  
+(define overlay1 (overlay star1 circle1))
+
+(above
+ (beside star1 circle1)
+ (beside circle1 star1))
+
+(above
+ (beside square1 star1)
+ (beside star1 square1))
+
+; Abstraktion
+; 1. kopieren
+; 2. Unterschiede durch Namen ersetzen
+
+(above
+ (beside image1 image2)
+ (beside image2 image1))
