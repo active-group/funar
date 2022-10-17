b5da6bc456c59ef7a89901f4e4acf6df1d82fee2
Mike Sperber <sperber@deinprogramm.de>
2022-10-17 13:36:19 +0200

    Update.

--- vorkurs/river.rkt
+++ vorkurs/river.rkt
@@ -1,1 +1,26 @@
-#lang racket
+#lang deinprogramm/sdp/beginner
+
+; Ein Fluss ist eins der folgenden:
+; - ein Bach - ODER -
+; - Zusammenfluss
+(define river
+  (signature (mixed creek confluence)))
+
+; Ein Bach hat folgende Eigenschaft(en):
+; - Ursprungsort
+(define-record creek
+  make-creek creek?
+  (creek-origin string))
+
+; Ein Zusammenfluss hat folgende Eigenschaften:
+; - Ort
+; - Hauptfluss
+;        ^^^^^ Selbstbezug
+; - Nebenfluss
+(define-record confluence
+  make-confluence confluence?
+  (confluence-location string)
+  (confluence-main-stem river)
+  (confluence-tributary river))
+
+; Fließt Wasser aus einem Ort in einen Fluss?
