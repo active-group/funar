b7968fbffb088628d2e5b7e73bdc90cbfc0f0438
Mike Sperber <sperber@deinprogramm.de>
2022-10-17 12:04:55 +0200

    Update.

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -167,3 +167,12 @@
 ; Verabschiedungs-Papagei, 2kg
 (define parrot2 (make-parrot "Tschüss!" 2))
 
+; Papagei überfahren
+(: run-over-parrot (parrot -> parrot))
+
+(check-expect (run-over-parrot parrot1)
+              (make-parrot "" 1))
+
+(define run-over-parrot
+  (lambda (parrot)
+    (make-parrot "" (parrot-weight parrot))))
