e926bc100c115e53de3e86fd594c8faa97960700
Mike Sperber <sperber@deinprogramm.de>
2022-10-17 15:15:49 +0200

    Update.

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -325,5 +325,13 @@ Schön wäre: beides billig, "expression problem"
            (cons (first list) (extract-odds (rest list)))
            (extract-odds (rest list)))))))
 
+(define extract
+  (lambda (p? list)
+    (cond
+      ((empty? list) empty)
+      ((cons? list)
+       (if (p? (first list))
+           (cons (first list) (extract p? (rest list)))
+           (extract p? (rest list)))))))
 
 
