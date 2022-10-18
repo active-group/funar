75f185476ee21b85cc618d09c30d00bb9ea33ceb
Mike Sperber <sperber@deinprogramm.de>
2022-10-18 08:34:04 +0200

    Mike's Haskell code

---
+++ haskell-code-mike/shell.nix
@@ -0,0 +1,31 @@
+{ withVSCode ? false }:
+
+let
+  # Update the sources/pin with niv; see:
+  # nix-shell -p nix --run 'niv modify --help'
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { config.allowUnfree = withVSCode; };
+  funar-code =
+    pkgs.haskellPackages.callCabal2nix "funar-code" (pkgs.lib.cleanSource ./.)
+    { };
+  vscodeFunar = pkgs.vscode-with-extensions.override {
+    vscodeExtensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+      haskell.haskell
+      justusadam.language-haskell
+    ];
+  };
+in pkgs.haskellPackages.shellFor {
+  packages = _: [ funar-code ];
+  buildInputs = (with pkgs; [ cabal-install ghcid haskell-language-server ])
+    ++ pkgs.lib.optional withVSCode vscodeFunar;
+  shellHook = ''
+    export PS1="\n\[\033[1;32m\][nix-shell:\W \[\033[1;31m\]FUNAR\[\033[1;32m\]]\$\[\033[0m\] "
+    echo -e "\n\033[1;31m ♣ ♠ Welcome to FUNAR! ♥ ♦ \033[0m\n"
+    echo -e "   Use the following command to open VSCode in this directory:\n"
+    echo "       code ."
+  '' + (if withVSCode then ''
+    echo -e "\n   All required extensions should be pre-installed and ready."
+  '' else
+    "");
+}
