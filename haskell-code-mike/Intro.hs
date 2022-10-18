edc4b670d92f799eba9c6b8dcb9943ee823106c9
Mike Sperber <sperber@deinprogramm.de>
2022-10-18 10:06:18 +0200

    Update.

--- haskell-code-mike/Intro.hs
+++ haskell-code-mike/Intro.hs
@@ -13,6 +13,15 @@ f = \ n -> n * 2
 
 -- Abkürzung:
 f' :: Integer -> Integer
+-- >>> f' 12
+-- 24
 f' n = n * 2
 
+-- Ein Haustier ist eins der folgenden:
+-- - Katze - ODER -
+-- - Hund - ODER -
+-- - Schlange
+-- ==> neuer Typ
+data Pet = Cat | Dog | Snake
+ deriving Show
 
