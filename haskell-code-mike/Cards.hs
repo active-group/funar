264e6ddd78cd066273b1107d51275f1550d806e8
Mike Sperber <sperber@deinprogramm.de>
2022-10-19 13:34:36 +0200

    Update.

--- haskell-code-mike/Cards.hs
+++ haskell-code-mike/Cards.hs
@@ -3,3 +3,11 @@ module Cards where
 
 -- Datenanalyse für Spielkarten
 -- "Make illegal states unrepresentable."
+
+
+-- Zusatzaufgaben:
+-- - Liste aller Karten (ohne sie manuell aufzuzählen)
+--   (Gibt's da eine Abstraktion?)
+
+-- - Ist eine Karte höherwertiger als eine andere?
+--   (nur zulässig bei gleicher Farbe)
