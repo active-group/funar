99456c26fe92153797db4c9d064c6326312044ff
Mike Sperber <sperber@deinprogramm.de>
2022-10-20 09:56:47 +0200

    Update.

--- hearts-mike/src/Table.hs
+++ hearts-mike/src/Table.hs
@@ -204,4 +204,107 @@ tableProcessCommand (PlayCard player card) state =
 data Game a = -- brauchen Typparameter
   -- pro Operation einen Konstruktor
     PlayValid Player Card (Bool -> Game a)
-  
+   | RecordEvent GameEvent (() -> Game a)
+ | GetCommand (GameCommand -> Game a)
+ | TurnOverTrick (Maybe (Trick, Player) -> Game a)
+ | PlayerAfter Player (Player -> Game a)
+ | GameOver (Maybe Player -> Game a)
+ | Done a
+
+data GameStep a =
+   RecordedEvent GameEvent (() -> Game a)
+ | NeedsCommand (GameCommand -> Game a)
+ | GameDone a
+
+runGameStep :: Game a -> TableState -> (GameStep a, TableState)
+runGameStep (PlayValid player card cont) state =
+  runGameStep (cont (playValid state player card)) state
+runGameStep (TurnOverTrick cont) state =
+  runGameStep (cont (turnOverTrick state)) state
+runGameStep (PlayerAfter player cont) state =
+  runGameStep (cont (playerAfter state player)) state
+runGameStep (GameOver cont) state =
+  runGameStep (cont (gameOver state)) state
+runGameStep (RecordEvent event cont) state =
+  (RecordedEvent event cont, tableProcessEvent event state)
+runGameStep (GetCommand cont) state =
+  (NeedsCommand cont, state)
+runGameStep (Done result) state =
+  (GameDone result, state)
+
+playValidM :: Player -> Card -> Game Bool
+playValidM player card = PlayValid player card Done
+recordEventM :: GameEvent -> Game ()
+recordEventM event = RecordEvent event Done
+turnOverTrickM :: Game (Maybe (Trick, Player))
+turnOverTrickM = TurnOverTrick Done
+playerAfterM player = PlayerAfter player Done
+gameOverM = GameOver Done
+
+instance Functor Game where
+
+instance Applicative Game where
+
+instance Monad Game where
+  return = Done
+  (PlayValid player card cont) >>= next =
+    PlayValid player card (\valid ->
+      cont valid >>= next)
+  (RecordEvent event cont) >>= next =
+    RecordEvent event (\() ->
+      cont () >>= next)
+  (GetCommand cont) >>= next =
+    GetCommand (\command ->
+      cont command >>= next)
+  (TurnOverTrick cont) >>= next =
+    TurnOverTrick (\over ->
+      cont over >>= next)
+  (PlayerAfter player cont) >>= next =
+    PlayerAfter player (\player ->
+      cont player >>= next)
+  (GameOver cont) >>= next =
+    GameOver (\won ->
+      cont won >>= next)
+  (Done result) >>= next = next result
+
+
+-- Einen Spielschritt verarbeiten
+tableProcessCommandM :: GameCommand -> Game (Maybe Player)
+tableProcessCommandM (DealHands hands) =
+  do mapM_ (recordEventM . uncurry HandDealt) (Map.toList hands)
+     return Nothing
+tableProcessCommandM (PlayCard player card) =
+  do valid <- playValidM player card
+     if valid
+     then
+      do recordEventM (LegalCardPlayed player card)
+         turnOverTrick <- turnOverTrickM
+         case turnOverTrick of
+          Just (trick, trickTaker) ->
+            do recordEventM (TrickTaken trickTaker trick)
+               over <- gameOverM
+               case over of
+                 Just winner -> 
+                   do recordEventM (GameEnded winner)
+                      return (Just winner)
+                 Nothing -> 
+                   do recordEventM (PlayerTurnChanged trickTaker)
+                      return Nothing
+          Nothing -> 
+            do nextPlayer <- playerAfterM player
+               recordEventM (PlayerTurnChanged nextPlayer)
+               return Nothing
+     else
+      do recordEventM (IllegalCardAttempted player card)
+         return Nothing
+
+-- Gesamtes Spiel spielen 
+tableLoopM :: GameCommand -> Game (Maybe Player)
+tableLoopM command =
+  do maybeWinner <- tableProcessCommandM command
+     case maybeWinner of
+      Nothing -> 
+        GetCommand tableLoopM
+      Just winner ->
+        return maybeWinner
+
