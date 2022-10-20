164c320870e74e97199e3add7f9874af1635daf1
Mike Sperber <sperber@deinprogramm.de>
2022-10-20 14:31:24 +0200

    Update.

--- haskell-code-mike/Contract.hs
+++ haskell-code-mike/Contract.hs
@@ -7,5 +7,19 @@ Zero-Bond / Zero Coupon Bond
 
 -}
 
+data Date = MkDate String deriving (Eq, Ord, Show)
+
+type Amount = Float
+
+data Currency = EUR | GBP | USD | YEN
+  deriving Show
+
 data Contract =
-    Zero
+    ZeroCouponBond Date Amount Currency
+  | Future Date -- ...
+  | Put
+  | Call
+    deriving Show
+
+zcb1 :: Contract
+zcb1 = ZeroCouponBond (MkDate "2022-12-24") 100 EUR
