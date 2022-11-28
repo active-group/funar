7db9b4e1f118a6ef1724ff4ed9a90f2404575e73
Mike Sperber <sperber@deinprogramm.de>
2022-11-28 14:31:00 +0100

    Update.

--- vorkurs/river.rkt
+++ vorkurs/river.rkt
@@ -15,4 +15,20 @@
 
 (define eschach (make-creek "Heimliswald"))
 (define prim (make-creek "Dreifaltigkeitsberg"))
+(define schlichem (make-creek "Tieringen"))
 
+; Ein Zusammenfluss hat folgende Eigenschaften:
+; - Ort des Zusammenflusses
+; - Hauptfluss
+; - Nebenfluss
+(define-record confluence
+  make-confluence
+  confluence?
+  (confluence-location string)
+  (confluence-main-stem river) ; Selbstbezug
+  (confluence-tributary river))
+
+(define neckar1
+  (make-confluence "Rottweil" eschach prim))
+(define neckar2
+  (make-confluence "Epfendorf" neckar1 schlichem))
