b69d26c975199083d51f1112c7f0b0b48757dedd
Mike Sperber <sperber@deinprogramm.de>
2022-11-28 14:26:31 +0100

    Update.

--- vorkurs/river.rkt
+++ vorkurs/river.rkt
@@ -6,4 +6,13 @@
 (define river
   (signature (mixed creek confluence)))
 
+; Ein Bach hat folgende Eigenschaften:
+; - Ursprungsort
+(define-record creek
+  make-creek
+  creek?
+  (creek-origin string))
+
+(define eschach (make-creek "Heimliswald"))
+(define prim (make-creek "Dreifaltigkeitsberg"))
 
