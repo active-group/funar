696c185faf76f925d42edc7868919c77fecd1847
Mike Sperber <sperber@deinprogramm.de>
2022-11-28 10:13:52 +0100

    Update.

--- vorkurs/image.rkt
+++ vorkurs/image.rkt
@@ -14,6 +14,21 @@
 (define star1 (star 50 "solid" "blue"))
 (define overlay1 (overlay star1 circle1))
 
+(above
+ (beside circle1 star1)
+ (beside star1 circle1))
 
+(above
+ (beside star1 square1)
+ (beside square1 star1))
 
+; Abstraktion:
+; 1. letztes Mal kopieren
+; 2. Unterschieden Namen geben
+; 3. lambda
+
+(lambda (image1 image2)
+  (above
+   (beside image1 image2)
+   (beside image2 image1)))
   
