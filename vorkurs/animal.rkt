9e92d94c1d3791ca858d8de4b4864d17d406659b
Mike Sperber <sperber@deinprogramm.de>
2022-11-28 11:10:57 +0100

    Update.

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -2,9 +2,20 @@
 
 ; Datenanalyse
 
+; Datendefinition:
 ; Ein Haustier ist eins der folgenden:
 ; - Hund - ODER -
 ; - Katze - ODER -
 ; - Schlange
 ; Fallunterscheidung
 ; hier speziell: Aufzählung
+(define pet
+  (signature (enum "dog" "cat" "snake")))
+
+; Ist ein Haustier niedlich?
+(: cute? (pet -> boolean))
+
+(check-expect (cute? "dog") #t)
+(check-expect (cute? "cat") #t)
+(check-expect (cute? "snake") #f)
+
