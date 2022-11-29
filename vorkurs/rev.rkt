17e33e8068c4feb3821946579de9fece1779005c
Mike Sperber <sperber@deinprogramm.de>
2022-11-29 09:19:44 +0100

    Update.

--- vorkurs/rev.rkt
+++ vorkurs/rev.rkt
@@ -6,3 +6,12 @@
 (check-expect (rev (list 1 2 3 4))
               (list 4 3 2 1))
 
+(define rev
+  (lambda (list)
+    (cond
+      ((empty? list) ...)
+      ((cons? list)
+       ...
+       (first list)
+       (rev (rest list))
+       ...))))
