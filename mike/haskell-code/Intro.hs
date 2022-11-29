1fda3075d0374fb11cbc30d183b7dce20f9cc0d7
Mike Sperber <sperber@deinprogramm.de>
2022-11-29 10:39:03 +0100

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -2,6 +2,23 @@
 module Intro where
 
 import Prelude hiding (Semigroup, Monoid)
+import Data.Text.Encoding (Decoding)
+
+-- Zeilenkommentar
 
 x :: Integer
+-- >>> x+15 
+-- 57
 x = 42
+
+y :: Integer
+y = x+3
+
+-- Haustier ist eins der folgenden:
+-- - Hund
+-- - Katze
+-- - Schlange
+
+-- data: neuer Datentyp
+data Pet =
+    Dog | Cat | Snake
