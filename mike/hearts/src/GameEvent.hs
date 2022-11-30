0ddfe41a162bafead66908a9a0853cab79d38f05
Mike Sperber <sperber@deinprogramm.de>
2022-11-30 15:17:18 +0100

    Update.

--- mike/hearts/src/GameEvent.hs
+++ mike/hearts/src/GameEvent.hs
@@ -34,4 +34,24 @@ data GameCommand =
   | MakeFirstMove Player
   | StartGame [Card] -- Hand Hand Hand Hand
 -}
+data GameEvent
+  = HandDealt Player Hand
+  | PlayerTurnChanged Player
+  | LegalCardPlayed Player Card
+  | TrickTaken Player Trick
+  | IllegalCardAttempted Player Card
+  | GameEnded Player
+  deriving (Show, Eq)
 
+data GameCommand
+  = DealHands (Map Player Hand)
+  | PlayCard Player Card
+  deriving (Show, Eq)
+
+-- Steht in den Events, wer gewonnen hat?
+eventsWinner :: [GameEvent] -> Maybe Player
+eventsWinner [] = Nothing
+eventsWinner (first : rest) =
+  case first of
+    GameEnded winner -> Just winner
+    _ -> eventsWinner rest
