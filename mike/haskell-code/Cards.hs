013ddbc4b827edef1dea3a347a58f144d85c0194
Mike Sperber <sperber@deinprogramm.de>
2022-11-30 08:52:54 +0100

    Update.

---
+++ mike/haskell-code/Cards.hs
@@ -0,0 +1,10 @@
+module Cards where
+
+-- Datenanalyse/Datentypen für Spielkarten des französischen Blattes
+
+-- Yaron Minsky: "Make illegal states unrepresentable."
+
+-- Liste aller Karten, ohne alle aufzulisten
+-- (alle Wertigkeiten, Farben auflisten OK)
+
+-- Funktion, die ermittelt, ob eine Karte eine andere (gleicher Farbe) schlägt
