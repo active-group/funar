60c7e0089759197101afc27d2e5df0aef6a03cf9
Mike Sperber <sperber@deinprogramm.de>
2022-11-30 14:28:06 +0100

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -123,5 +123,5 @@ runDBSQLite conn (Get key callback) =
         <- queryNamed conn "SELECT key, value FROM entries WHERE key = :key" [":key" := key]
        runDBSQLite conn (callback value)
 runDBSQLite conn (Put key value callback) = 
-    do execute conn "REPLACE INTO entries (key, value)"
+    do execute conn "REPLACE INTO entries (key, value) VALUES (?,?)"
 runDBSQLite conn (Return result) = return result
