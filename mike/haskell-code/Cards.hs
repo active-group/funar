8b86a97af55012f9e991e06260273f1f0039ca74
Mike Sperber <sperber@deinprogramm.de>
2022-11-30 09:09:28 +0100

    Update.

--- mike/haskell-code/Cards.hs
+++ mike/haskell-code/Cards.hs
@@ -10,3 +10,16 @@ module Cards where
 -- Funktion, die ermittelt, ob eine Karte eine andere (gleicher Farbe) schlägt
 
 -- Alle Karten aufzählen: Alle Kombinationen aus Elementen zweier Listen
+
+cartesianProduct :: [a] -> [b] -> [(a, b)]
+-- >>> cartesianProduct [1,2,3] [4,5,6]
+-- [(1,4),(1,5),(1,6),(2,4),(2,5),(2,6),(3,4),(3,5),(3,6)]
+cartesianProduct [] bs = []
+cartesianProduct (a:as) bs =
+    cartesianProduct1 a bs ++ cartesianProduct as bs
+
+cartesianProduct1 :: a -> [b] -> [(a, b)]
+-- >>> cartesianProduct1 42 [1,2,3]
+-- [(42,1),(42,2),(42,3)]
+cartesianProduct1 a [] = []
+cartesianProduct1 a (b:bs) = (a, b) : cartesianProduct1 a bs
