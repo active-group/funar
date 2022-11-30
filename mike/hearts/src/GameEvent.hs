800d0fb01c0afa6f5030c1a0dd179cafb98673c0
Mike Sperber <sperber@deinprogramm.de>
2022-11-30 15:00:34 +0100

    Update.

--- mike/hearts/src/GameEvent.hs
+++ mike/hearts/src/GameEvent.hs
@@ -5,3 +5,7 @@ import Cards
 import qualified Data.Map as Map
 import Data.Map (Map)
 
+-- Event: Objekt, das ein fachliches Ereignis in der Domäne repräsentiert.
+
+-- - können gesamte Geschichte der Domäne erzählen
+-- - Redundanz ist OK
