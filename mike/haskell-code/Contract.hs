3f8055b03706c9ea805943dae1283ee12b27f6c5
Mike Sperber <sperber@deinprogramm.de>
2022-12-01 15:19:35 +0100

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -2,4 +2,32 @@ module Contract where
 
 -- 1. Einfaches Beispiel
 -- Zero-Coupon Bond / Zero-Bond
--- 
+-- "Ich bekomme 24.12.2022 100€."
+
+-- 2. In "atomare Bestandteile" / "Ideen" zerlegen
+
+-- "Währung": "Ich bekomme 1€ jetzt."
+-- "Betrag" / "Vielfaches"
+-- "Später"
+
+newtype Date = Date String
+  deriving (Show, Eq, Ord)
+
+christmas = Date "2022-12-24"
+
+type Amount = Double
+
+data Currency = EUR | USD | GBP | YEN
+  deriving Show
+
+{-
+data Contract =
+    ZeroCouponBond Date Amount Currency
+  | Call 
+  | Put
+  | FxSwap
+  | Everest
+    deriving Show
+
+zcb1 = ZeroCouponBond christmas 100 EUR
+-}
