9ebea94f8358d7ba66f617db013397feb9fa23b8
Johannes Maier <johannes.maier@active-group.de>
2023-01-31 11:05:27 +0100

    update

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -1,2 +1,40 @@
 #lang deinprogramm/sdp/beginner
 
+; Haustier _ist eins der Folgenden_:
+; - Katze -ODER-
+; - Hund -ODER-
+; - Schlange
+(define pet
+  (signature (enum "cat" "dog" "snake")))
+
+; Ist ein Haustier niedlich?
+(: cute? ; <- cute-p
+   (pet -> boolean))
+
+(check-expect (cute? "dog")
+              #t)
+(check-expect (cute? "cat")
+              #t)
+(check-expect (cute? "snake")
+              #f)
+
+; Gerüst zuerst
+(define cute?
+  (lambda (pet)
+    ...))
+
+; bei Daten, die Fallunterscheidung sind ->
+; Fälle unterschiedlich behandeln
+; -> cond
+; Schablone:
+(define cute?
+  (lambda (pet)
+    (cond ; Verzweigung: ein Paar pro Fall
+      ((string=? pet "dog") ...) ; (<Beding> <Erg>)
+      ((string=? pet "snake") ...)
+      ((string=? pet "cat") ...))))
+
+
+
+
+
