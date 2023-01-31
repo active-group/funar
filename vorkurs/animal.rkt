b0da79c36242b45c9a68e6d275d8dfeb0ad4f2e3
Johannes Maier <johannes.maier@active-group.de>
2023-01-31 15:53:22 +0100

    update

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -370,7 +370,7 @@ Siehe: Expression problem
              (list-map f (rest list)))))))
 
 ; Abstraktion über list-sum und list-product
-(: foo (%neutral (%neutral %neutral -> %neutral) (list-of %a) -> ...))
+(: foo (%neutral (%neutral %neutral -> %neutral) (list-of %a) -> %neutral))
 
 (define list-product
   (lambda (neutral op list)
