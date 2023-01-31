a1ad90e443717a72c6f965a9617afdeb849ef996
Johannes Maier <johannes.maier@active-group.de>
2023-01-31 15:24:08 +0100

    update

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -316,6 +316,7 @@ Siehe: Expression problem
                  (extract-odds (rest list))))))))
 
 ; Signaturvariable
+; Higher-Order-Funktion
 (: extract ((%a -> boolean) (list-of %a) -> (list-of %a)))
 
 ; Gehen so vor wie bei tile (Mike)
