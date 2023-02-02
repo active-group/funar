67535d0da72d700eb4112a7955ae1e172cd7973d
Johannes Maier <johannes.maier@active-group.de>
2023-02-02 15:56:01 +0100

    update

--- johannes/hearts/src/GameEvent.hs
+++ johannes/hearts/src/GameEvent.hs
@@ -24,22 +24,22 @@ import Data.Map (Map)
 
 type GameId = String
 
-data GameEvent
-    = GameStarted GameId     -- Ein Spiel besteht aus mehreren Runden
-    | RoundStarted -- Jede Runde besteht aus 13 Stichen/Tricks
-    | HandsDistributed (Map Player Hand)
-    | StartingPlayerSelected Player
-    | CardPlayed Player Card
-    | TrickCompleted
-    | TrickTaken Player Trick
-    | NextPlayerSelected Player
-    | RoundCompleted [Player] -- Gewinner der Runde
-    | GameCompleted [Player]
-    | PlayerResigned Player
-    | IllegalCardAttempted Player Card
-    deriving (Eq, Show)
+-- data GameEvent
+--     = GameStarted GameId     -- Ein Spiel besteht aus mehreren Runden
+--     | RoundStarted -- Jede Runde besteht aus 13 Stichen/Tricks
+--     | HandsDistributed (Map Player Hand)
+--     | StartingPlayerSelected Player
+--     | CardPlayed Player Card
+--     | TrickCompleted
+--     | TrickTaken Player Trick
+--     | NextPlayerSelected Player
+--     | RoundCompleted [Player] -- Gewinner der Runde
+--     | GameCompleted [Player]
+--     | PlayerResigned Player
+--     | IllegalCardAttempted Player Card
+--     deriving (Eq, Show)
 
-data GameCommand
-    = PlayCard Player Card
-    | DealHands (Map Player Hand)
-    deriving (Eq, Show)
+-- data GameCommand
+--     = PlayCard Player Card
+--     | DealHands (Map Player Hand)
+--     deriving (Eq, Show)
