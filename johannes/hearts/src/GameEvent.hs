d90133d73da108dca6de35ce5881f2ddeaf7573a
Johannes Maier <johannes.maier@active-group.de>
2023-02-02 15:30:24 +0100

    update

--- johannes/hearts/src/GameEvent.hs
+++ johannes/hearts/src/GameEvent.hs
@@ -5,3 +5,16 @@ import Cards
 import qualified Data.Map as Map
 import Data.Map (Map)
 
+-- Events: Domain-Event
+-- - Log anlegen von Dingen, die in der Anwendung/Domäne passieren
+-- - Log sollte alles enthalten, was passiert ist -> Erzählung
+-- - gehen davon aus, dass sie in der Reihenfolge des Passierens aufgezeichnet werden
+
+-- Event:
+-- - in der Vergangenheit passiert!
+-- - fachlich motiviert
+-- - Redundanz ist OK
+-- - (keinen Zustand reinschreiben)
+-- - Events sind (einzige) Quelle der Wahrheit im System
+
+-- 
