28e9e4748d9c627dea11dbe8c6adaac1abbeb2f1
Johannes Maier <johannes.maier@active-group.de>
2023-02-03 15:37:37 +0100

    update

--- johannes/haskell-code/Contract.hs
+++ johannes/haskell-code/Contract.hs
@@ -81,5 +81,5 @@ data Direction = Long | Short
 data Payment = MkPayment Direction Date Amount Currency
 
 -- Datum: "Zahlungen bis jetzt"
-semantics :: Contract -> Date -> ([Payment], Contract)
+semantics :: Contract -> Date -> ([Payment], Contract) -- Residualvertrag
 semantics = undefined
