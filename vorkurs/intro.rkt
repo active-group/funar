d9b03ded4f1a5f86026410e8f126b85d4a4a6eaf
Mike Sperber <sperber@deinprogramm.de>
2023-05-02 11:17:22 +0200

    Update.

--- vorkurs/intro.rkt
+++ vorkurs/intro.rkt
@@ -119,3 +119,23 @@
 (: dillo-alive? (dillo -> boolean))
 (: dillo-weight (dillo -> number))
 
+; Gürteltier, lebendig, 10kg
+(define dillo1 (make-dillo #t 10))
+; totes Gürteltier, 8kg
+(define dillo2 (make-dillo #f 8))
+
+; Gürteltier überfahren
+(: run-over-dillo (dillo -> dillo))
+
+(check-expect (run-over-dillo dillo1)
+              (make-dillo #f 10))
+(check-expect (run-over-dillo dillo2)
+              dillo2)
+
+(define run-over-dillo
+  (lambda (dillo)
+    (make-dillo ... ...)
+    ...
+    (dillo-alive? dillo)
+    (dillo-weight dillo)
+    ...))
