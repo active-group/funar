b9af96b1643a15c8258605e4846febac1a9d146d
Mike Sperber <sperber@deinprogramm.de>
2023-05-03 10:45:17 +0200

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -14,3 +14,18 @@ f = \ x -> x * 2
 
 f' :: Integer -> Integer
 f' x = x * 2
+
+-- Haustier ist eins der folgenden:
+-- - Hund -ODER-
+-- - Katze -ODER-
+-- - Schlange
+
+-- neuer Datentyp:
+data Pet =
+    Dog
+  | Cat
+  | Snake
+  deriving Show
+
+-- Abseitsregel: Bei mehrzeiligen Konstrukten müssen die
+-- Folgezeilen gegenüber der erste Zeile eingerückt sein.
