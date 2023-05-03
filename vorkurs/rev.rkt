2dc0d9be61c9000f1f64281a87a7e073283fb442
Mike Sperber <sperber@deinprogramm.de>
2023-05-03 09:14:20 +0200

    Update.

--- vorkurs/rev.rkt
+++ vorkurs/rev.rkt
@@ -1,2 +1,15 @@
 #lang deinprogramm/sdp
 
+; Summe aller Listenelemente
+(: list-sum (list-of-numbers -> number))
+
+(check-expect (list-sum (list 1 2 3 4 5)) 15)
+
+(define list-sum
+  (lambda (list)
+    (cond
+      ((empty? list) 0) ; neutrales Element bezüglich +
+      ((cons? list)
+       (+ (first list)
+          (list-sum (rest list)))))))
+
