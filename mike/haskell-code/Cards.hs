901e2d340f7d2f45c57c248bcf6565796b025011
Mike Sperber <sperber@deinprogramm.de>
2023-05-03 15:20:20 +0200

    Update.

---
+++ mike/haskell-code/Cards.hs
@@ -0,0 +1,11 @@
+module Cards where
+
+-- Aufgabe:
+
+-- Domänenmodell für Spielkarten
+-- "Make illegal states unrepresentable."
+
+-- Liste aller Spielkarten (52 Stück)
+
+-- Funktion, welche die höherwertige zweier Karten
+-- findet.  Nur definiert bei Karten gleicher Farbe.
