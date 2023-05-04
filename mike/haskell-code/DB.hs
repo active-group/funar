b093dcb32c2363ca32bb775263b756c2e29cd9ee
Mike Sperber <sperber@deinprogramm.de>
2023-05-04 12:18:18 +0200

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -30,3 +30,11 @@ data DB a =
     Get Key (Value -> DB a) -- Callback / Continuation
   | Put Key Value (() -> DB a)
   | Return a
+
+p1 :: DB String
+p1 =
+    Put "Mike" 100 (\() ->
+    Get "Mike" (\x ->
+    Put "Mike" (x+1) (\() ->
+    Get "Mike" (\y ->
+    Return (show (x+y))))))
