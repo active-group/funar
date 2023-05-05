49fd5033b6fde335999f57d104adbc20179acd42
Mike Sperber <sperber@deinprogramm.de>
2023-05-05 10:58:50 +0200

    Update.

--- mike/haskell-code/Free.hs
+++ mike/haskell-code/Free.hs
@@ -6,5 +6,14 @@ data DB a
   | Put Key Value (() -> DB a)
   | Return a
 
+data Game a
+  = PlayValid Player Card (Bool -> Game a)
+  | RecordEvent GameEvent (() -> Game a)
+  | TurnOverTrick (Maybe (Trick, Player) -> Game a)
+  | PlayerAfter Player (Player -> Game a)
+  | GameOver (Maybe Player -> Game a)
+  | GetCommand (GameCommand -> Game a)
+  | Done a
+
 
 -}
