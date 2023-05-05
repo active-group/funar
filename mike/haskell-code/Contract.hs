5460328fab0d7f7114523e570384125fa8ce92e6
Mike Sperber <sperber@deinprogramm.de>
2023-05-05 15:02:35 +0200

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -7,6 +7,14 @@ module Contract where
   Zero-Coupon Bond
   Zero-Bond
 
+- in "atomare Bestandteile" / "separate Ideen"
+  - "Währung"
+    "Ich bekomme 1€ jetzt."
+  - "Vielfaches"
+    "Ich bekomme 100€ jetzt."
+  - "Später"
+
+
 -}
 
 type Amount = Double
@@ -16,6 +24,22 @@ data Date = MkDate String
 
 data Currency = EUR | GBP | YEN | USD
   deriving Show
-  
+
+{- Fehlversuch:
 data Contract =
     ZeroCouponBond Date Amount Currency
+-}
+
+data Contract =
+    One Currency -- "Ich bekomme 1€ jetzt."
+  | Many Amount Contract
+  | Later Date Contract
+  deriving Show
+
+c1 = One EUR
+-- Ich bekomme 100€ jetzt.
+c2 = Many 100 (One EUR) 
+
+
+-- Ich bekomme am 24.12.2023 100€.
+zcb1 = Later (MkDate "2023-12-24") (Many 100 (One EUR))
