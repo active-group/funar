00e39c1d1c6d73523e1b7e571f0a93ab53d7eb9a
Mike Sperber <sperber@deinprogramm.de>
2023-09-12 11:24:25 +0200

    Update.

--- vorkurs/intro.rkt
+++ vorkurs/intro.rkt
@@ -46,14 +46,34 @@
 ; - Minute
 ; zusammengesetzte Daten
 ; OOP: Klasse, Java: auch "Record", Kotlin: "data class"
+
+; Eine Stunde ist eine ganze Zahl zwischen 0 und 23
+(define hour (signature (integer-from-to 0 23)))
+; Eine Minute ist eine ganze Zahl zwischen 0 und 59
+(define minute (signature (integer-from-to 0 59)))
+  
 (define-record time ; Signatur
   make-time ; Konstruktor
-  (time-hour natural) ; Selektor / "Getter-Funktion"
-  (time-minute natural))
+  (time-hour hour) ; Selektor / "Getter-Funktion"
+  (time-minute minute))
 
-(: make-time (natural natural -> time))
-(: time-hour (time -> natural))
-(: time-minute (time -> natural))
+(: make-time (hour minute -> time))
+(: time-hour (time -> hour))
+(: time-minute (time -> minute))
+
 
 ; 12 Uhr 24 Minuten
 (define time1 (make-time 12 24))
+; 14:32
+(define time2 (make-time 14 32))
+
+; Minuten seit Mitternacht
+(: msm (time -> natural))
+
+(check-expect (msm time1) 744)
+(check-expect (msm time2) 872)
+
+(define msm
+  (lambda (time)
+    ...))
+                          
