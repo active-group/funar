5c123f45594651d0c72043930df3dc249d607fc3
Mike Sperber <sperber@deinprogramm.de>
2023-09-13 10:32:55 +0200

    Update.

--- vorkurs/rev.rkt
+++ vorkurs/rev.rkt
@@ -3,6 +3,11 @@
 ; Liste umdrehen
 (: rev ((list-of %a) -> (list-of %a)))
 
+; Anzahl der Aufrufe für Liste der Länge n:
+; 1 + 2 + 3 + ... (n-1) + n
+; n/2 * (n+1) = n^2 + ... = O(n^2)
+; Gaußsche Summenformel
+
 (check-expect (rev (list 1 2 3))
               (list 3 2 1))
 
@@ -15,6 +20,20 @@
         (rev (rest list)) ; 3 2
         (first list) ; 1
         )))))
+
+; Auch Liste umdrehen
+(: rev2 ((list-of %a) (list-of %a) -> (list-of %a)))
+
+(check-expect (rev2 (list 1 2 3) empty)
+              (list 3 2 1))
+
+(define rev2
+  (lambda (list acc) ; acc: Akkumulator / Zwischenergebebnis
+    (cond
+      ((empty? list) acc)
+      ((cons? list)
+       (rev2 (rest list)
+             (cons (first list) acc))))))
 
 (: append-element ((list-of %a) %a -> (list-of %a)))
 
