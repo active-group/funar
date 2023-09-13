8c50ba4e5365bf46806d7d99dd0d735134e55394
Mike Sperber <sperber@deinprogramm.de>
2023-09-13 16:23:39 +0200

    Update.

---
+++ mike/haskell-code/Cards.hs
@@ -0,0 +1,12 @@
+module Cards where
+-- Das französische Blatt
+
+-- 1. Datenanalyse für Spielkarten
+-- "Make illegal states unrepresentable."
+
+-- 2. Liste aller 52 Spielkarten
+-- berechnet aus einer Liste der Farben und einer
+-- Liste der Wertigkeiten
+
+-- 3. Funktion, die zwei Karten vergleicht mit gleicher Farbe
+-- 2 ... 10 Bube Dame König As
