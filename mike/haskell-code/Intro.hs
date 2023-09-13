e2b7972c440a618a6078ed1af0d761c53f289f2c
Mike Sperber <sperber@deinprogramm.de>
2023-09-13 13:36:09 +0200

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -127,10 +127,11 @@ runOverAnimal (MkDillo _ weight) = MkDillo Dead weight
 
 -- gibt nur einstellige Funktionen
 feedAnimal :: Animal -> (Weight -> Animal)
--- feedAnimal dillo@(MkDillo liveness weight) amount = -- Alias-Pattern
---     case liveness of
---         Alive -> MkDillo liveness (weight + amount)
---         Dead -> dillo -- MkDillo liveness weight
-feedAnimal dillo@(MkDillo Alive weight) amount = MkDillo Alive (weight + amount)
-
+feedAnimal dillo@(MkDillo liveness weight) amount = -- Alias-Pattern
+    case liveness of
+        Alive -> MkDillo liveness (weight + amount)
+        Dead -> dillo -- MkDillo liveness weight
+-- feedAnimal dillo@(MkDillo Alive weight) amount = MkDillo Alive (weight + amount)
+-- feedAnimal dillo@(MkDillo Dead weight) amount = -- Alias-Pattern
+--   dillo -- MkDillo liveness weight
 feedAnimal (MkParrot sentence weight) amount = MkParrot sentence (weight + amount)
