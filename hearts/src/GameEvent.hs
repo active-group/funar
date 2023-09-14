dc9fffbfddd83b30d11b33784b334428600929f0
Mike Sperber <sperber@deinprogramm.de>
2023-09-14 17:09:15 +0200

    Mike's events and commands

--- hearts/src/GameEvent.hs
+++ hearts/src/GameEvent.hs
@@ -5,3 +5,24 @@ import Cards
 import qualified Data.Map as Map
 import Data.Map (Map)
 
+data GameEvent
+  = HandDealt Player Hand
+  | PlayerTurnChanged Player
+  | LegalCardPlayed Player Card
+  | TrickTaken Player Trick
+  | IllegalCardAttempted Player Card
+  | GameEnded Player
+  deriving (Show, Eq)
+
+data GameCommand
+  = DealHands (Map Player Hand)
+  | PlayCard Player Card
+  deriving (Show, Eq)
+
+-- Steht in den Events, wer gewonnen hat?
+eventsWinner :: [GameEvent] -> Maybe Player
+eventsWinner [] = Nothing
+eventsWinner (first : rest) =
+  case first of
+    GameEnded winner -> Just winner
+    _ -> eventsWinner rest
