d82c4c0d2f40cad43d90025447dc1df035b1d58d
Mike Sperber <sperber@deinprogramm.de>
2023-09-14 14:30:49 +0200

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -1,5 +1,6 @@
 module DB where
 
+
 type Key = String
 type Value = Integer
 
@@ -11,3 +12,19 @@ type Value = Integer
    return (show (x+y))
 -}
 
+{-
+data DBCommand a =
+      Put Key Value
+    | Get Key
+    | Return a
+
+type DBProgram a = [DBCommand a]
+
+p1 = [Put "Mike" 100,
+      Get "Mike"]
+-}
+
+data DB a =
+    Put Key Value (() -> DB a)
+  | Get Key (Value -> DB a)
+  | Return a
