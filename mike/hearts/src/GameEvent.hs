28e106df6cb598b5356cea7cdce1eebc46803ecf
Mike Sperber <sperber@deinprogramm.de>
2023-09-15 09:20:17 +0200

    Update.

--- mike/hearts/src/GameEvent.hs
+++ mike/hearts/src/GameEvent.hs
@@ -72,5 +72,5 @@ instance Monad Game where
 -- Spielregeln / "Tisch"
 -- Commands rein, Events raus, Spiel vorbei?
 tableProcessCommandM :: GameCommand -> Game (Maybe Player)
-tableProcessCommandM (DealHands hands)= undefined
+tableProcessCommandM (DealHands hands) = undefined
 tableProcessCommandM (PlayCard player card) = undefined
