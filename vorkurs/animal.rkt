61e12faf46a224ea1ab9d665c89b05c6ccb0c1e5
Mike Sperber <sperber@deinprogramm.de>
2023-11-06 12:08:33 +0100

    Update.

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -8,3 +8,8 @@
   make-dillo
   (dillo-alive? boolean)
   (dillo-weight number))
+
+; lebendiges Gürteltier, 10kg
+(define dillo1 (make-dillo #t 10))
+; totes Gürteltier, 8kg
+(define dillo2 (make-dillo #f 8))
