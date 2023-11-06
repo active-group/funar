6e2295b301f6aaf9d6573124cd0c72c76e18f492
Mike Sperber <sperber@deinprogramm.de>
2023-11-06 13:44:31 +0100

    Update.

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -33,3 +33,33 @@
     (make-dillo #f (dillo-weight dillo))))
 
 ; Gürteltier füttern
+(: feed-dillo (dillo number -> dillo))
+
+(check-expect (feed-dillo dillo1 5)
+              (make-dillo #t 15))
+(check-expect (feed-dillo dillo2 5)
+              dillo2)
+
+(define feed-dillo
+  (lambda (dillo amount)
+    ; Fallunterscheidung
+    ; zusammengesetzte Daten
+    (define alive? (dillo-alive? dillo))
+    (define weight (dillo-weight dillo))
+    (make-dillo
+     alive?
+     (if alive?
+         (+ weight amount)
+         weight)
+     #;(cond
+       ((dillo-alive? dillo) (+ (dillo-weight dillo) amount))
+       (else (dillo-weight dillo))))))
+
+#;(define feed-dillo
+  (lambda (dillo amount)
+    (if (dillo-alive? dillo)
+        (make-dillo #t (+ (dillo-weight dillo) amount))
+        dillo)))
+
+; lexikalische Bindung:
+; vom Vorkommen aus von innen nach außen suchen, nach lambda oder define
