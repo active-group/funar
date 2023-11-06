3b17f935cef9e079856b8a97a28d25cddf663dab
Mike Sperber <sperber@deinprogramm.de>
2023-11-06 11:12:38 +0100

    Update.

--- vorkurs/intro.rkt
+++ vorkurs/intro.rkt
@@ -1,2 +1,15 @@
 #lang deinprogramm/sdp/beginner
 
+; Ist ein Haustier niedlich?
+
+; Datendefinition:
+
+; Ein Haustier ist eins der folgenden:
+; - Hund -ODER-
+; - Katze -ODER-
+; - Schlange
+; Fallunterscheidung
+; hier speziell: Aufzählung
+(define pet
+  (signature (enum "dog" "cat" "snake")))
+
