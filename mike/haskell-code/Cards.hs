aa84710817111c1d8d38985f031dabab6499d00f
Mike Sperber <sperber@deinprogramm.de>
2023-11-07 15:47:58 +0100

    Update.

---
+++ mike/haskell-code/Cards.hs
@@ -0,0 +1,12 @@
+module Cards where
+
+-- Datenanalyse für das französische Blatt:
+-- => 1. Datentyp für Karten
+-- Yaron Minsky: "Make illegal states unrepresentable."
+
+-- 2. Liste aller Karten (Länge: 52)
+--    aus Liste aller Farben / Wertigkeiten
+
+-- 3. Funktion, die bei 2 Karten gleicher Farbe testet,
+--    welche höherwertig ist
+
