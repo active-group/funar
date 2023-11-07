6805cdc3955f0b5dd4404930faab310567455f45
Mike Sperber <sperber@deinprogramm.de>
2023-11-07 09:38:26 +0100

    Update.

--- vorkurs/rev.rkt
+++ vorkurs/rev.rkt
@@ -16,3 +16,16 @@
         (first list)))))) ; 1
 
 ; Element ans Ende einer Liste anhängen
+(: append-element ((list-of %a) %a -> (list-of %a)))
+
+(check-expect (append-element (list 1 2 3) 4)
+              (list 1 2 3 4))
+
+(define append-element
+  (lambda (list element)
+    (cond
+      ((empty? list) (cons element empty))
+      ((cons? list)
+       (cons
+        (first list)
+        (append-element (rest list) element))))))
