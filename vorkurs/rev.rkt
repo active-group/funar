61562a02bc84bdca02e0851f7b8405215f5bd31c
Mike Sperber <sperber@deinprogramm.de>
2023-11-07 09:18:21 +0100

    Update.

--- vorkurs/rev.rkt
+++ vorkurs/rev.rkt
@@ -1,1 +1,16 @@
 #lang deinprogramm/sdp
+
+; Liste umdrehen
+(: rev ((list-of %a) -> (list-of %a)))
+
+(check-expect (rev (list 1 2 3 4))
+              (list 4 3 2 1))
+
+(define rev
+  (lambda (list)
+    (cond
+      ((empty? list) empty)
+      ((cons? list)
+       (append-element
+        (rev (rest list)) ; 4 3 2
+        (first list)))))) ; 1
