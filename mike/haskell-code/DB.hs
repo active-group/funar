595bd8f978d84cd15b01ace52b88179f4a5ab016
Mike Sperber <sperber@deinprogramm.de>
2023-11-08 11:34:20 +0100

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -50,4 +50,36 @@ splice (Put key value callback) next =
     Put key value (\() -> splice (callback ()) next)
 splice (Return result) next = next result
 
+p1' :: DB String
+p1' = splice (put "Mike" 100) (\() ->
+      splice (get "Mike") (\x ->
+      splice (put "Mike" (x+1)) (\() ->
+      splice (get "Mike") (\y ->
+      Return (show (x+y))))))
 
+-- >>> :info Monad
+-- type Monad :: (* -> *) -> Constraint
+-- class Applicative m => Monad m where
+--   (>>=) :: m a -> (a -> m b) -> m b
+--   return :: a -> m a
+
+instance Functor DB where
+
+instance Applicative DB where
+
+instance Monad DB where
+    (>>=) = splice
+    return = Return
+
+p1'' :: DB String
+p1'' = do put "Mike" 100
+          x <- get "Mike"
+          put "Mike" (x+1)
+          y <- get "Mike"
+          return (show(x+y))
+
+{-
+in Java:
+in Stream<A>:
+<B> Stream<B> 	flatMap(Function<A, Stream<B>> mapper)
+-}
