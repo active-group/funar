764b8fde7bd12c8a04e9779215dd6dd7404a9cc7
Mike Sperber <sperber@deinprogramm.de>
2023-11-08 12:27:57 +0100

    Update.

--- mike/hearts/src/GameEvent.hs
+++ mike/hearts/src/GameEvent.hs
@@ -55,11 +55,19 @@ eventsWinner (first : rest) =
 -- Spielregel-Monade
 data Game a =
     RecordEvent GameEvent (() -> Game a)
+  | PlayValid Player Card (Bool -> Game a)
+  | TurnOverTrick (Maybe (Trick, Player) -> Game a)
   | Done a
 
 recordEventM :: GameEvent -> Game ()
 recordEventM event = RecordEvent event Done
 
+playValidM :: Player -> Card -> Game Bool
+playValidM player card = PlayValid player card Done
+
+turnOverTrickM :: Game (Maybe (Trick, Player))
+turnOverTrickM = TurnOverTrick Done
+ 
 instance Functor Game where
 
 instance Applicative Game where
@@ -67,8 +75,30 @@ instance Applicative Game where
 instance Monad Game where
     return = Done
     (>>=) (Done result) next = next result
+    (>>=) (RecordEvent event callback) next =
+        RecordEvent event (\() -> (callback () >>= next))
+    (>>=) (PlayValid player card callback) next =
+        PlayValid player card (\isValid -> callback isValid >>= next)
+    (>>=) (TurnOverTrick callback) next =
+        TurnOverTrick (\over -> callback over >>= next)
 
 -- Rückgabe: Gewinner:in, falls Spiel vorbei
 tableProcessCommandM :: GameCommand -> Game (Maybe Player)
-tableProcessCommandM (DealHands hands) = undefined
-tableProcessCommandM (PlayCard player card) = undefined
+tableProcessCommandM (DealHands hands) =
+    let events = map (\(player, hand) -> HandDealt player hand) 
+                     (Map.toList hands)
+    in do mapM_ recordEventM events
+          return Nothing
+
+tableProcessCommandM (PlayCard player card) =
+    do isValid <- playValidM player card
+       if isValid
+       then do recordEventM (LegalCardPlayed player card)
+               over <- turnOverTrickM
+               case over of
+                  Nothing -> undefined
+                  Just (trick, trickTaker) ->
+                    do recordEventM (TrickTaken trickTaker trick)
+                       undefined
+       else do recordEventM (IllegalCardAttempted player card)
+               return Nothing
