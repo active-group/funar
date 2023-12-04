5e6a34fff2f8239cae58af351f26d7026928dbbd
Mike Sperber <sperber@deinprogramm.de>
2023-12-04 16:13:17 +0100

    Update.

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -258,6 +258,32 @@
        (+ (first list) 
           (list-sum (rest list)))))))
 
-; Aus einer Liste die geraden Elemente extrahieren
+; Aus einer Liste die ungeraden Elemente extrahieren
+(: extract-odds (list-of-numbers -> list-of-numbers))
 
-  
+(check-expect (extract-odds list4)
+              (cons 7 (cons 5 empty)))
+
+(define extract-odds
+  (lambda (list)
+    (cond
+      ((empty? list) empty)
+      ((cons? list)
+       (if (odd? (first list))
+           (cons (first list) (extract-odds (rest list)))
+           (extract-odds (rest list)))))))
+
+; abstrahieren:
+; - kopieren
+; - ggf. umbenennen (rekursive Aufrufe nicht vergessen!)
+; - Unterschiede -> abstrakte Namen
+; - abstrakte Namen -> Parameter in einem lambda
+;   (rekursive Aufrufe nicht vergessen!)
+(define extract
+  (lambda (p? list)
+    (cond
+      ((empty? list) empty)
+      ((cons? list)
+       (if (p? (first list))
+           (cons (first list) (extract p? (rest list)))
+           (extract p? (rest list)))))))
