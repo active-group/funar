9a1dd20df60e3e6837c03bf055e0fc34fdc6ccb6
Mike Sperber <sperber@deinprogramm.de>
2023-12-04 14:05:37 +0100

    Update.

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -130,3 +130,27 @@
     (make-dillo #f (dillo-weight dillo))))
 
 ; Gürteltier füttern (Futtermenge variabel)
+(: feed-dillo (dillo number -> dillo))
+
+(check-expect (feed-dillo dillo1 5)
+              (make-dillo #t 15))
+(check-expect (feed-dillo dillo2 5)
+              dillo2)
+
+#;(define feed-dillo
+  (lambda (dillo amount)
+    (make-dillo
+     (dillo-alive? dillo)
+     (cond
+       ((dillo-alive? dillo)
+        (+ (dillo-weight dillo) amount))
+       ((not (dillo-alive? dillo))
+        (dillo-weight dillo))))))
+
+(define feed-dillo
+  (lambda (dillo amount)
+    (make-dillo
+     (dillo-alive? dillo)
+     (if (dillo-alive? dillo)
+         (+ (dillo-weight dillo) amount)
+         (dillo-weight dillo)))))
