0db3b1b3dd40384c9f2ea7fdefb02b699671ecb6
Mike Sperber <sperber@deinprogramm.de>
2023-12-04 12:23:58 +0100

    Update.

--- vorkurs/animal.rkt
+++ vorkurs/animal.rkt
@@ -87,3 +87,35 @@
      hours
      (- minutes (* 60 hours))
      #;(remainder minutes 60))))
+
+
+; Tiere auf dem texanischen Highway
+
+; Gürteltiere hat folgende Eigenschaften: 
+; - lebendig oder tot? -UND-
+; - Gewicht
+; zusammengesetzte Daten
+(define-record dillo
+  make-dillo
+  (dillo-alive? boolean)
+  (dillo-weight number))
+
+(: make-dillo (boolean number -> dillo))
+(: dillo-alive? (dillo -> boolean))
+(: dillo-weight (dillo -> number))
+
+; lebendiges Gürteltier, 10kg
+(define dillo1 (make-dillo #t 10))
+; totes Gürteltier, 8kg
+(define dillo2 (make-dillo #f 8))
+
+; Gürteltier überfahren
+(: run-over-dillo (dillo -> dillo))
+
+(check-expect (run-over-dillo dillo1)
+              (make-dillo #f 10))
+(check-expect (run-over-dillo dillo2)
+              dillo2)
+
+
+
