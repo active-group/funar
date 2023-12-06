963fc2f1dff21807d182edff7e448ae3d80dc0fe
Mike Sperber <sperber@deinprogramm.de>
2023-12-06 09:06:25 +0100

    Update.

--- mike/haskell-code/Cards.hs
+++ mike/haskell-code/Cards.hs
@@ -3,3 +3,11 @@ module Cards where
 -- Datenmodell für Karten des französischen Blattes
 
 -- Yaron Minsky: "Make illegal states unrepresentable."
+
+-- Liste aller Karten des französischen Blatts
+-- ... aus jeweils den Listen der Farben und Wertigkeiten
+-- ... dafür gibt's Library-Funktionen
+-- (Allgemeinverwendbare Abstraktion?)
+
+-- Funktion, die feststellt, ob eine andere Karte
+-- höherwertig ist - aber nur bei gleicher Farbe.
