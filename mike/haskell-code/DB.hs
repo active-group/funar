f694374892a020e4f7b82ba2e693f14ddb006505
Mike Sperber <sperber@deinprogramm.de>
2023-12-06 12:07:06 +0100

    Update.

---
+++ mike/haskell-code/DB.hs
@@ -0,0 +1,42 @@
+module DB where
+
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map)
+
+type Key = String
+type Value = Integer
+
+{-
+put "Mike" 100
+x = get "Mike" 
+put "Mike" (x+1)
+y = get "Mike"
+return (show (x+y))
+-}
+
+{-
+data DBCommand result =
+    Put Key Value
+  | Get Key
+  | Return result
+
+type DBProgram result = [DBCommand result]
+
+p1 = [Put "Mike" 100,
+      Get "Mike",
+      Put "Mike"] -- wie dem Resultat vom Get einen Namen geben?
+-}
+
+data DB a =
+    Get Key       (Value -> DB a)
+  | Put Key Value (()    -> DB a)
+  | Return a
+
+p1 :: DB String
+p1 =
+    Put "Mike" 100 (\() ->
+    Get "Mike" (\x ->
+    Put "Mike" (x+1) (\() ->
+    Get "Mike" (\y -> 
+    Return (show (x+y))))))
+
