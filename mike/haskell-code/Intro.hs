1c93a5b56504fd1591fd38d1a1df61a0d4fbc82e
Mike Sperber <sperber@deinprogramm.de>
2023-12-06 11:42:40 +0100

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -537,7 +537,7 @@ listIndex a (first:rest) =
     then Result 0 
     else 
 --      optionalMap (\index -> index + 1) (listIndex a rest)
-      optionalMap (+1) (listIndex a rest)
+      fmap (+1) (listIndex a rest)
 {-
         case listIndex a rest of
           Empty -> Empty
@@ -548,6 +548,25 @@ optionalMap :: (a -> b) -> Optional a -> Optional b
 optionalMap f Empty = Empty
 optionalMap f (Result a) = Result (f a)
 
+instance Functor Optional where
+  fmap = optionalMap
+
+-- >>> :info Functor
+-- type Functor :: (* -> *) -> Constraint
+-- class Functor f where
+--   fmap :: (a -> b) -> f a -> f b
+--   (<$) :: a -> f b -> f a
+--   {-# MINIMAL fmap #-}
+--         -- Defined in ‘GHC.Base’
+-- instance Functor (Either a) -- Defined in ‘Data.Either’
+-- instance Functor [] -- Defined in ‘GHC.Base’
+-- instance Functor Solo -- Defined in ‘GHC.Base’
+-- instance Functor Maybe -- Defined in ‘GHC.Base’
+-- instance Functor IO -- Defined in ‘GHC.Base’
+-- instance Functor ((->) r) -- Defined in ‘GHC.Base’
+-- instance Functor ((,,,) a b c) -- Defined in ‘GHC.Base’
+-- instance Functor ((,,) a b) -- Defined in ‘GHC.Base’
+-- instance Functor ((,) a) -- Defined in ‘GHC.Base’
 -- >>> :info Num
 -- type Num :: * -> Constraint
 -- class Num a where
