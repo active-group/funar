59060d772cdf9113bfe21f02d08249eca31ceeab
Mike Sperber <sperber@deinprogramm.de>
2023-12-06 10:14:12 +0100

    Update.

--- mike/haskell-code/Cards.hs
+++ mike/haskell-code/Cards.hs
@@ -1,4 +1,17 @@
-module Cards where
+module Cards(Suit(..), Rank(..), Card(..),
+             deck, cardOrder,
+             Player(..), 
+             Hand, makeHand, emptyHand, isHandEmpty, containsCard, removeCard, handCards,
+             Trick(..), emptyTrick,  trickEmpty, addToTrick, cardsOfTrick, leadingCardOfTrick,
+             Pile, emptyPile, pileEmpty, pileAddTrick, pileCards,
+             allSuits, allRanks)
+where
+
+import qualified Data.Set as Set
+import Data.Set (Set)
+
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map, (!))
 
 -- Datenmodell für Karten des französischen Blattes
 
@@ -82,3 +95,78 @@ deck :: [Card]
 -- >>> length deck
 -- 52
 deck = map Card allSuits <*> allRanks
+
+-- | Karten, die jemand auf der Hand hält
+newtype Hand = Hand {unHand :: Set Card}
+  deriving (Eq, Show)
+
+makeHand :: [Card] -> Hand
+makeHand cards = Hand (Set.fromList cards)
+
+handCards :: Hand -> [Card]
+handCards (Hand set) = Set.toList set
+
+-- | Ist die Hand leer?
+isHandEmpty :: Hand -> Bool
+isHandEmpty hand = Set.null (unHand hand)
+
+-- | Enthält die Hand eine spezifische Karte?
+containsCard :: Card -> Hand -> Bool
+containsCard card hand = Set.member card (unHand hand)
+
+-- | Karte aus der Hand entfernen
+removeCard :: Card -> Hand -> Hand
+removeCard card hand = Hand (Set.delete card (unHand hand))
+
+-- | Leere Hand
+emptyHand :: Hand
+emptyHand = Hand Set.empty
+
+-- Spieler
+
+data Player = Player {playerName :: String}
+  deriving (Show, Eq, Ord)
+
+-- * Stich
+
+-- Zuletzt gespielte Karte zuerst
+newtype Trick = Trick {trickToList :: [(Player, Card)]}
+  deriving (Show, Eq)
+
+-- leeren Stich herstellen
+emptyTrick :: Trick
+emptyTrick = Trick []
+
+--- ist Stich leer
+trickEmpty :: Trick -> Bool
+trickEmpty (Trick list) = null list
+
+-- alle Karten desStich
+cardsOfTrick :: Trick -> [Card]
+cardsOfTrick (Trick list) = map snd list
+
+-- Karte auf den Stich legen
+addToTrick :: Player -> Card -> Trick -> Trick
+addToTrick player card (Trick list) = Trick ((player, card) : list)
+
+-- die Karte des Stich, die bedient werden muß
+leadingCardOfTrick :: Trick -> Card
+leadingCardOfTrick (Trick list) = snd (last list)
+
+-- Haufen aufgenommener Karten
+newtype Pile = Pile {unPile :: Set Card}
+  deriving (Show)
+
+emptyPile :: Pile
+emptyPile = Pile Set.empty
+
+pileEmpty :: Pile -> Bool
+pileEmpty pile = Set.null (unPile pile)
+
+-- Stich auf den Haufen legen
+pileAddTrick :: Pile -> Trick -> Pile
+pileAddTrick pile trick =
+  Pile (Set.union (unPile pile) (Set.fromList (cardsOfTrick trick)))
+
+pileCards :: Pile -> [Card]
+pileCards pile = Set.toList (unPile pile)
