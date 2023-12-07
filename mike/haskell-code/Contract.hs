66d21340f68dd6bbcdba39bd5cf1c8624d6abdf7
Mike Sperber <sperber@deinprogramm.de>
2023-12-07 15:48:12 +0100

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -85,9 +85,14 @@ swap1 :: Contract
 swap1 = Combine (zeroCouponBond (MkDate "2023-12-24") 100 EUR)
                 (Negative (zeroCouponBond (MkDate "2023-12-24") 100 USD))
 
-c6 = Multiple 100 (Combine (zeroCouponBond (MkDate "2023-1-24") 100 EUR)
-                           (zeroCouponBond (MkDate "2024-1-24") 100 EUR))
--- >>> meaning c6 (MkDate "2024-31-01")
+c6 = Multiple 100 (Combine (zeroCouponBond (MkDate "2023-1-24") 200 EUR)
+                           (zeroCouponBond (MkDate "2024-1-24") 300 EUR))
+-- >>> meaning c6 (MkDate "2023-31-01")
+-- ([MkPayment (MkDate "2023-31-01") Long 20000.0 EUR],Multiple 100.0 (Combine (Multiple 200.0 Zero) (At (MkDate "2024-1-24") (Multiple 300.0 (One EUR)))))
+
+multiple :: Amount -> Contract -> Contract
+multiple _ Zero = Zero
+multiple factor contract = Multiple factor contract
 
 data Direction = Long | Short 
   deriving Show
@@ -113,3 +118,19 @@ invertPayment (MkPayment date direction amount currency) =
 
 -- alle Zahlungen bis zu einem bestimmten Zeitpunkt, "jetzt"
 meaning :: Contract -> Date -> ([Payment], Contract)
+meaning (One currency) now = ([MkPayment now Long 1 currency], Zero)
+meaning (Multiple amount contract) now =
+    let (payments, residualContract) = meaning contract now
+    in (map (scalePayment amount) payments, Multiple amount residualContract)
+meaning c@(At date contract) now =
+    if now >= date
+    then meaning contract now
+    else ([], c)
+meaning (Negative contract) now =
+    let (payments, residualContract) = meaning contract now
+    in (map invertPayment payments, Negative residualContract)
+meaning (Combine contract1 contract2) now =
+    let (payments1, residualContract1) = meaning contract1 now
+        (payments2, residualContract2) = meaning contract2 now
+    in (payments1 ++ payments2, Combine residualContract1 residualContract2)
+meaning Zero now = ([], Zero)
