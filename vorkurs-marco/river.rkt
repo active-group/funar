b7f5c0ae6877ec3a2b42749d006a8d9e5d219ac3
Marco Schneider <marco.schneider@active-group.de>
2024-05-13 13:37:43 +0200

    update

--- vorkurs-marco/river.rkt
+++ vorkurs-marco/river.rkt
@@ -27,3 +27,27 @@
   (confluence-location string)
   (confluence-hauptfluss river)
   (confluence-nebenfluss river))
+
+(define neckar1
+  (make-confluence "Rottweil" eschach prim))
+
+(define neckar2
+  (make-confluence "Epfendorf" neckar1 schlichem))
+
+; Fließt Wasser aus Ort in Fluß?
+(: flows-from? (string river -> boolean))
+
+(check-expect (flows-from? "Epfendorf" neckar2)
+              #t)
+(check-expect (flows-from? "Rottweil" neckar2)
+              #t)
+(check-expect (flows-from? "Dreifaltigkeitsberg" neckar2)
+              #t)
+(check-expect (flows-from? "Heimliswald" neckar2)
+              #t)
+(check-expect (flows-from? "Tübingen" neckar2)
+              #f)
+
+(define flows-from?
+  (lambda (location river)
+    
