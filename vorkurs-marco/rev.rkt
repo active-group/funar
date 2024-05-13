984354023ca4a57f2f95beb37c6210fdce837f6a
Marco Schneider <marco.schneider@active-group.de>
2024-05-13 16:16:08 +0200

    update

--- vorkurs-marco/rev.rkt
+++ vorkurs-marco/rev.rkt
@@ -6,3 +6,9 @@
 (check-expect (rev (list 1 2))
               (list 2 1))
 (check-expect (rev empty) empty)
+
+(define rev
+  (lambda (list)
+    (cond
+      ((empty? list) ...)
+      ((cons? list) ...))))
