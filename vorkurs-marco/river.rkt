7d419b07c1f7b21ce9e480bc885247b6d37dd423
Marco Schneider <marco.schneider@active-group.de>
2024-05-13 13:30:08 +0200

    update

--- vorkurs-marco/river.rkt
+++ vorkurs-marco/river.rkt
@@ -1,1 +1,27 @@
 #lang deinprogramm/sdp/beginner
+
+; Ein Fluss ist eins der folgenden:
+; - ein Bach
+; - ein Zusammenfluss
+
+; Ein Bach hat folgende Eigenschaften
+; - Ursprungsort
+(define-record creek
+  make-creek
+  creek?
+  (creen-origin string))
+
+(define eschach (make-creek "Heimliswald"))
+(define prim (make-creek "Dreifaltigkeitsberg"))
+(define schlichem (make-creek "Tieringen"))
+
+; Ein Zusammenfluss halt folgende Eigenschaften
+; - ein Ort des Zusammenflusses
+; - Hauptfluss
+; - Nebenfluss
+(define-record confluence
+  make-confluence
+  confluence?
+  (confluence-location string)
+  (confluence-hauptfluss creek)
+  (confluence-nebenfluss creek))
