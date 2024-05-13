6b1b3232075bc6ff428474ac88542e259df0c2b8
Marco Schneider <marco.schneider@active-group.de>
2024-05-13 10:12:30 +0200

    update

--- vorkurs-marco/Image.rkt
+++ vorkurs-marco/Image.rkt
@@ -6,4 +6,39 @@
 
 (* 12 (/ 3 2))
 
-(circle 50 "solid" "gold")
+(define circle1 (circle 50 "solid" "gold"))
+(define square1 (square 100 "outline" "green"))
+(define star1 (star 50 "solid" "blue"))
+(define overlay1 (overlay star1 circle1))
+
+(above
+ (beside circle1 star1)
+ (beside star1 circle1))
+
+(above
+ (beside star1 square1)
+ (beside square1 star1))
+
+; Abstraktion
+; 1. noch ein mal kopieren
+; 2. den Unterschieden namen geben
+; 3. lambda
+
+; Konstruktionsanleitung
+; 1. Kurzbeschreibung schreiben
+; 2. Signatur
+; 3. Test
+
+; Quadratisches Kachelmuster erzeugen
+(: tile (image image -> image))
+
+(check-expect (tile star1 square1)
+              (above
+               (beside star1 square1)
+               (beside square1 star1)))
+
+(define tile
+  (lambda (image1 image2)
+    (above
+     (beside image1 image2)
+     (beside image2 image1))))
