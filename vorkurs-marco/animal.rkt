c8a6ebe8a253fc3f6531c7b89c67b2593f132b79
Marco Schneider <marco.schneider@active-group.de>
2024-05-13 10:33:44 +0200

    update

--- vorkurs-marco/animal.rkt
+++ vorkurs-marco/animal.rkt
@@ -6,17 +6,27 @@
 ; Ein Haustier ist eines der folgenden
 ; - Hund - ODER -
 ; - Katze - ODER -
-; - Schlange
+; - Schlange - ODER -
+; - Fruchtfliege
 ; Fallunterscheidung
 ; speziell hier: Aufzählung
 (define pet
-  (signature (enum "dog" "cat" "snake")))
+  (signature (enum "dog" "cat" "snake" "fruit fly")))
 
 ; Konstruktionsanleitung
 ; 1. Kurzbeschreibung schreiben
 ; 2. Signatur
 ; 3. Test
+; 4. Gerüst
 
 ; Ist ein Haustier niedlich?
 (: cute? (pet -> boolean))
 
+(check-expect (cute? "dog") #t)
+(check-expect (cute? "cat") #t)
+(check-expect (cute? "snake") #t)
+(check-expect (cute? "fruit fly") #f)
+
+(define cute?
+  (lambda (animal)
+    ...))
