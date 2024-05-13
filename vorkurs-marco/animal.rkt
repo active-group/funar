cd6f0294e1feb2f61665d4af0b2f6452f7553492
Marco Schneider <marco.schneider@active-group.de>
2024-05-13 10:29:30 +0200

    update

--- vorkurs-marco/animal.rkt
+++ vorkurs-marco/animal.rkt
@@ -9,3 +9,13 @@
 ; - Schlange
 ; Fallunterscheidung
 ; speziell hier: Aufzählung
+(define pet
+  (signature (enum "dog" "cat" "snake")))
+
+; Konstruktionsanleitung
+; 1. Kurzbeschreibung schreiben
+; 2. Signatur
+; 3. Test
+
+; Ist ein Haustier niedlich?
+#;(define check...)
