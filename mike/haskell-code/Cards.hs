bd5f2b45e8985337cf3f490444a6cd1e4ce1b55d
Mike Sperber <sperber@deinprogramm.de>
2024-05-14 15:54:32 +0200

    Update.

---
+++ mike/haskell-code/Cards.hs
@@ -0,0 +1,8 @@
+module Cards where
+
+-- 1. eine Datenanalyse für Karten des französischen Blattes => Datentypen
+--    Yaron Minsky: "Make illegal states unrepresentable."
+-- 2. eine Liste aller Karten (52 Stück), ohne alle einzeln aufzulisten
+--    (Liste aller Wertigkeiten bzw. aller Farben ist OK)
+-- 3. Funktion, die bei zwei Karten gleicher Farbe entscheidet, welche
+--    höherwertig ist.
