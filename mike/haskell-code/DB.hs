42c0d43ffd07cdec44c985cc32fd4ed0c96ce832
Mike Sperber <sperber@deinprogramm.de>
2024-05-15 15:08:54 +0200

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -12,7 +12,26 @@ return (show (x+y))
 type Key = String
 type Value = Integer
 
+{-
 data DBCommand a =
     Put Key Value
   | Get Key
   | Return a
+
+type DBProgram a = [DBCommand a]
+
+p1 = [Put "Mike" 10,
+      x = Get "Mike",
+      Put "Mike" (x+ * 2)]
+-}
+
+data DB a =
+    Get Key       (Integer -> DB a)
+  | Put Key Value (()      -> DB a)
+  | Return a
+
+p1 = Put "Mike" 10 (\() ->
+     Get "Mike" (\x -> 
+     Put "Mike" (x * 2) (\() ->
+     Get "Mike" (\y ->
+     Return (show (x+y))))))
