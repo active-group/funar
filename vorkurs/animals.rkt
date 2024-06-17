a6d87177fda5b6cee9bd6bf700c1b444b3b8ee54
Mike Sperber <sperber@deinprogramm.de>
2024-06-17 10:03:34 +0200

    Update.

--- vorkurs/animals.rkt
+++ vorkurs/animals.rkt
@@ -1,9 +1,14 @@
 #lang deinprogramm/sdp
 
+; Datenanalyse
+
 ; Haustier ist eins der folgenden:
 ; - Hund -ODER-
 ; - Katze -ODER-
 ; - Schlange
 ; Fallunterscheidung, hier speziell: Aufzählung
+(define pet
+  (signature (enum "dog" "cat" "snake")))
 
 ; Ist ein Haustier niedlich?
+(: cute? (pet -> boolean))
