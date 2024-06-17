222fb16c1e776b757616f4ab2107e4323ac83690
Mike Sperber <sperber@deinprogramm.de>
2024-06-17 12:24:52 +0200

    Update.

--- vorkurs/animals.rkt
+++ vorkurs/animals.rkt
@@ -181,7 +181,24 @@
        (run-over-dillo dillo))
       ((parrot? animal) (run-over-parrot animal)))))
 
-; - Seife
-; - Shampoo
-; - Duschgel - bestehend 50% Seife, 50% Shampoo
+; - Seife hat folgende Eigenschaft: pH-Wert
+(define-record soap
+  make-soap
+  soap?
+  (soap-ph number))
 
+; - Shampoo hat folgende Eigenschaft: Haartyp
+(define hairtype
+  (signature (enum "normal" "oily" "dandruff" "none")))
+
+(define-record shampoo
+  make-shampoo
+  shampoo?
+  (shampoo-hairtype hairtype))
+
+; - Duschgel - bestehend aus 50% Seife, 50% Shampoo
+(define-record showergel
+  make-showergel
+  showergel?
+  (showergel-soap soap)
+  (showergel-shampoo shampoo))
