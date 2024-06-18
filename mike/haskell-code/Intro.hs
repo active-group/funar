fc137b08cafc219b845d54417f7e34125692739f
Mike Sperber <sperber@deinprogramm.de>
2024-06-18 09:30:54 +0200

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -1,3 +1,48 @@
 {-# LANGUAGE InstanceSigs #-}
 module Intro where
 
+x :: Integer
+x = 5
+
+y :: Integer
+y = x * 2
+
+-- Zeilenkommentar
+-- >>> x + y
+-- 15
+
+double :: Integer -> Integer
+-- double = \ x -> x * 2
+double x = x * 2
+
+-- >>> double 7
+-- 14
+
+foo x y =
+ let z = x + y 
+    in z * 4
+
+-- Ein Haustier ist eins der folgenden:
+-- - Hund -ODER-
+-- - Katze -ODER-
+-- - Schlange
+
+-- algebraischer Datentyp / Aufzählung
+data Pet =
+    Dog 
+  | Cat
+  | Snake
+  deriving Show
+
+-- Ist Haustier niedlich?
+isCute :: Pet -> Bool
+-- eine Gleichung pro Fall
+isCute Dog = True
+isCute Cat = True
+isCute Snake = False
+
+-- >>> isCute Dog
+-- True
+
+-- >>> isCute Snake
+-- False
