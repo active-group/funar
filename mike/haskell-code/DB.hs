35ebfc6c7dc6736e7ea3ccc98732b62f1b43091b
Mike Sperber <sperber@deinprogramm.de>
2024-06-19 09:18:56 +0200

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -33,5 +33,12 @@ p1 = [
 -- Namen vergeben: let, =, \
 
 data DB a =
-    Get Key (Value -> DB a) -- callback / continuation
-  | Put Key Value (() -> DB a)
+    Get Key       (Value -> DB a) -- callback / continuation
+  | Put Key Value (()    -> DB a)
+  | Return a
+
+p1 = Put "Mike" 100 (\() ->
+     Get "Mike" (\x -> 
+     Put "Mike" (x+1) (\() ->
+     Get "Mike" (\y ->
+     Return (show (x+y))))))
