51fb9107e3d83f1d21a59e45325bc2556af8e59b
Mike Sperber <sperber@deinprogramm.de>
2024-06-19 09:12:07 +0200

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -1,2 +1,33 @@
 module DB where
 
+{-
+put "Mike" 100
+x = get "Mike"
+put "Mike" (x+1)
+y = get "Mike"
+return (show (x + y))
+-}
+
+type Key = String
+
+type Value = Integer
+
+{- 
+-- gescheitert:
+
+data DBCommand a = -- a: Typ des Ergebnisses, von return
+    Put Key Value
+  | Get Key 
+  | Return a
+  deriving Show
+
+type DBProgram a = [DBCommand a]
+
+p1 = [
+    Put "Mike" 100,
+    Get "Mike",
+    Put "Mike" ???
+]
+
+-}
+
