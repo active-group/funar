f680705611fb8aad48870e7f4280e2c7d6110e08
Mike Sperber <sperber@deinprogramm.de>
2024-09-23 09:50:13 +0200

    Update.

--- vorkurs/images.rkt
+++ vorkurs/images.rkt
@@ -10,10 +10,22 @@
 (define square1 (square 100 "outline" "blue"))
 (define star1 (star 50 "solid" "gold"))
 
-(above
+#;(above
  (beside star1 circle1)
  (beside circle1 star1))
 
-(above
+#;(above
  (beside circle1 square1)
  (beside square1 circle1))
+
+; abstrahieren
+; 2 ähnliche Code-Passagen
+; - (einmal noch) kopieren
+; - Unterschiede durch (abstrakte) Namen
+; - Namen in Lambda-Ausdruck aufgenommen
+
+(define tile
+  (lambda (image1 image2)
+    (above
+     (beside image1 image2)
+     (beside image2 image1))))
