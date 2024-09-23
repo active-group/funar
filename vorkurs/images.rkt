f39dbc96e03fb66b345acdb03bdeead4b0ac3557
Mike Sperber <sperber@deinprogramm.de>
2024-09-23 09:41:08 +0200

    Update.

--- vorkurs/images.rkt
+++ vorkurs/images.rkt
@@ -4,4 +4,8 @@
   (+ 12
      (* 23
         14)))
-(define y (* x 2)) 
+(define y (* x 2))
+
+(define circle1 (circle 50 "solid" "red"))
+(define square1 (square 100 "outline" "blue"))
+(define star1 (star 50 "solid" "gold"))
