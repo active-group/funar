c2ca98bbc7e4be974296580915573c8b231630a8
Mike Sperber <sperber@deinprogramm.de>
2024-09-24 09:42:01 +0200

    mike

---
+++ mike/hearts/src/Cards.hs
@@ -0,0 +1,8 @@
+-- Das französische Blatt
+module Cards where
+
+import qualified Data.Set as Set
+import Data.Set (Set)
+
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map, (!))
