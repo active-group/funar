55a448be0d3f0102d683c24a93d2065a01b5d991
Mike Sperber <sperber@deinprogramm.de>
2024-09-24 09:51:45 +0200

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -2,3 +2,26 @@
 module Intro where
 
 import Prelude hiding (Semigroup, Monoid)
+
+x :: Integer
+x = 23
+
+double :: Integer -> Integer
+double x =  -- Abseitsregel
+ x * 2
+
+foo x y =
+    let z = x + y
+        a = z + (2*y)
+    in (x + y + z) * a
+
+-- Gürteltier hat folgende Eigenschaften:
+-- - Lebendigkeit -UND-
+-- - Gewicht
+
+-- Lebendigkeit ist eins der folgenden:
+-- - Lebendig
+-- - Tot
+
+data Liveness = Alive | Dead
+  deriving Show
