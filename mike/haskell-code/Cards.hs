dba783f41ab691cf7b7f82e2e3198f65dd0e141e
Mike Sperber <sperber@deinprogramm.de>
2024-09-24 14:35:02 +0200

    Update.

---
+++ mike/haskell-code/Cards.hs
@@ -0,0 +1,13 @@
+-- das französische Blatt
+module Cards where
+
+-- 1. Datentyp für Karten des französischen Blatts
+
+-- Yaron Minsky:
+-- "Make illegal states unrepresentable."
+
+-- 2. Liste aller Karten (52 Stück)
+-- Input: alle Farben, alle Wertigkeiten
+
+-- 3. Funktion, die bei zwei Karten ermittelt, welche höherwertig ist
+-- ("höherwertig" nur definiert bei gleicher Farbe)
