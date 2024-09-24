36bb9a3742897c86b5d12a61a690c152645a59db
Mike Sperber <sperber@deinprogramm.de>
2024-09-24 12:03:53 +0200

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -105,8 +105,11 @@ feedAnimal dillo@(MkDillo liveness weight) amount = -- Alias-Pattern
 feedAnimal (MkSnake length thickness) amount =
     MkSnake length (thickness+amount)
 
-swap :: (Animal -> Weight -> Animal) -> (Weight -> Animal -> Animal)
-swap f = \ weight -> \ animal -> f animal weight
+-- swap :: (Animal -> Weight -> Animal) -> (Weight -> Animal -> Animal)
+-- Typvariablen: Kleinbuchstaben
+swap :: (a -> b -> c) -> (b -> a -> c)
+--swap f = \ b -> \ a -> f a b
+swap f b a = f a b
 
 feedAnimal' :: (Animal, Weight) -> Animal
 feedAnimal'(dillo@(MkDillo liveness weight), amount) =
