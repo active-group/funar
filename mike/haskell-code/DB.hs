669c0d3958b11d1b8c466011203a5aebe023726a
Mike Sperber <sperber@deinprogramm.de>
2024-09-25 10:59:49 +0200

    Update.

---
+++ mike/haskell-code/DB.hs
@@ -0,0 +1,81 @@
+module DB where
+
+import qualified Data.Map.Strict as Map
+import Data.Map (Map, (!))
+
+{-
+put "Mike" 100
+x = get "Mike"
+put "Mike" (x+1)
+y = get "Mike"
+return (show (x+y))
+-}
+
+type Key = String
+type Value = Integer
+
+{- gescheitert:
+type DBProgram a = [DBCommand a]
+
+data DBCommand a =
+    Get Key
+  | Put Key Value
+  | Return a
+
+p1 = [
+    Put "Mike" 100,
+    Get "Mike",
+    Put "Mike" ... x+1 
+]
+-}
+
+data DB a =
+    Get Key       (Value -> DB a)
+  | Put Key Value (()    -> DB a)
+  | Return a
+
+get :: Key -> DB Value
+get key = Get key (\value -> Return value)
+
+put :: Key -> Value -> DB ()
+put key value = Put key value Return
+
+splice :: DB a -> (a -> DB b) -> DB b
+splice (Get key callback) next =
+    Get key (\value -> splice (callback value) next)
+splice (Put key value callback) next =
+    Put key value (\() -> splice (callback ()) next)
+splice (Return result) next = next result
+
+
+p1 :: DB String
+p1 = Put "Mike" 100 (\() ->
+     Get "Mike" (\x ->
+     Put "Mike" (x+1) (\() ->
+     Get "Mike" (\y ->
+     Return (show (x+y))))))
+
+p1' :: DB String
+p1' = splice (put "Mike" 100) (\() ->
+      splice (get "Mike") (\x ->
+      splice (put "Mike" (x+1)) (\() ->
+      splice (get "Mike") (\y ->
+      Return (show (x+y))))))
+
+
+
+-- >>> runDB p1 Map.empty
+-- ("201",fromList [("Mike",101)])
+
+-- >>> runDB p1' Map.empty
+-- ("201",fromList [("Mike",101)])
+
+runDB :: DB a -> Map Key Value -> (a, Map Key Value)
+runDB (Get key callback) mp =
+    let value = mp ! key
+    in runDB (callback value) mp
+runDB (Put key value callback) mp =
+    let mp' = Map.insert key value mp
+    in runDB (callback ()) mp'
+runDB (Return result) mp =
+    (result, mp)
