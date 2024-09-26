088dc294f14bb22daedcecee3ec2d40c1f4c9350
Mike Sperber <sperber@deinprogramm.de>
2024-09-26 14:14:52 +0200

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -7,6 +7,12 @@ module Contract where
   - Datum
   - Betrag
   - Währung
+- Kombinatoren formulieren
+- wiederholen
+
+"Am 24.12.2024:
+- Ich bekomme 100€.
+- Ich bezahle 100CHF."
 -}
 data Date = Date String
   deriving (Eq, Ord, Show)
@@ -25,11 +31,16 @@ data Contract =
   | Butterblume
 -}
 
+data Direction = Long | Short
+  deriving Show
+
 data Contract =
     -- Ich bekomme einen Euro. Jetzt.
     One Currency
   | Amount Amount Contract
   | At Date Contract
+  | Reverse Contract
+  | Mix Contract Contract
   deriving Show
 
 c1 :: Contract
@@ -39,4 +50,26 @@ c1 = One EUR
 -- Ich bekomme jetzt 100€.
 c100 = Amount 100 (One EUR)
 
+zcb1 :: Contract
 zcb1 = At (Date "2024-12-24") (Amount 100 (One EUR))
+
+zeroCouponBond :: Date -> Amount -> Currency -> Contract
+zeroCouponBond date amount currency =
+    At date (Amount amount (One currency))
+
+zcb1' :: Contract
+zcb1' = zeroCouponBond (Date "2024-12-24") 100 EUR
+
+-- "Ich bezahle Weihnachten 100CHF."
+c2 :: Contract
+c2 = Reverse (zeroCouponBond (Date "2024-12-24") 100 CHF)
+
+swap1 :: Contract
+swap1 = Mix zcb1 c2
+
+fxSwap :: Date -> Amount -> Currency -> Amount -> Currency -> Contract
+fxSwap date amount1 currency1 amount2 currency2 =
+--    Mix (zeroCouponBond date amount1 currency1)
+--        (Reverse (zeroCouponBond date amount2 currency2))
+  At date (Mix (Amount amount1 (One currency1))
+               (Reverse (Amount amount2 (One currency2))))
