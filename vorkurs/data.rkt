13fc3cdc19dba5d57a99bbf2257d28cf0f672884
Mike Sperber <sperber@deinprogramm.de>
2024-10-07 12:01:19 +0200

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -97,3 +97,36 @@
       ((equal? pet "dog") #t)
       ((equal? pet "cat") #t)
       ((equal? pet "snake") #f))))
+
+; Tiere auf dem texanischen Highway
+
+; Gürteltier hat folgende Eigenschaften:
+; - lebendig? -UND-
+; - Gewicht
+; eigentlich: (Repräsentation des) Zustand des Gürteltiers zu einem bestimmten Zeitpunkt
+(define-record dillo
+  make-dillo
+  (dillo-alive? boolean)
+  (dillo-weight number))
+
+; lebendiges Gürteltier 10kg
+(define dillo1 (make-dillo #t 10))
+; totes Gürteltier 8kg
+(define dillo2 (make-dillo #f 8))
+
+; Gürteltier überfahren
+(: run-over-dillo (dillo -> dillo))
+
+(check-expect (run-over-dillo dillo1)
+              (make-dillo #f 10))
+(check-expect (run-over-dillo dillo2)
+              dillo2)
+
+(define run-over-dillo
+  (lambda (dillo)
+    (make-dillo #f (dillo-weight dillo))))
+  
+
+
+
+
