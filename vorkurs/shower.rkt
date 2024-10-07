6ec971f1b108f3eb2debb035b849b2ed7f2796f5
Mike Sperber <sperber@deinprogramm.de>
2024-10-07 14:29:19 +0200

    Update.

--- vorkurs/shower.rkt
+++ vorkurs/shower.rkt
@@ -1,2 +1,9 @@
 #lang deinprogramm/sdp/beginner
 
+; Duschprodukt ist eins der folgenden:
+; - Seife -ODER-
+; - Shampoo -ODER-
+; - Duschgel: besteht aus gleichen Teilen aus Seife und Shampoo
+; gemischte Daten
+
+
