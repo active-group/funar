6f5b088b89feba81a76445d13e9c54872d1bdf58
Mike Sperber <sperber@deinprogramm.de>
2024-10-07 14:39:16 +0200

    Update.

--- vorkurs/shower.rkt
+++ vorkurs/shower.rkt
@@ -48,3 +48,28 @@
 
 (define gel1 (make-showergel soap1 shampoo1))
 (define gel2 (make-showergel soap2 shampoo2))
+
+; Mixtur aus Duschgel und Seife
+(define-record mixture-a
+  make-mixture-a
+  mixture-a?
+  (mixture-a-showergel showergel)
+  (mixture-a-soap soap))
+
+(define-record mixture-z
+  make-mixture-z
+  mixture-z?
+  (mixture-a-showergel1 showergel)
+  (mixture-a-showergel2 showergel))
+
+; Eine Mixture besteht aus:
+; - Duschprodukt
+; - noch'n Duschprodukt
+(define-record mixture
+  make-mixture
+  mixture?
+  (mixture-product1 shower-product)
+  (mixture-product2 shower-product))
+
+(define mix1 (make-mixture gel1 soap2))
+(define mix2 (make-mixture gel1 gel2))
