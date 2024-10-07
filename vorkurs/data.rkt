6f0986cf083f4115573c58572243e9b0420d7a5e
Mike Sperber <sperber@deinprogramm.de>
2024-10-07 11:05:43 +0200

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -18,3 +18,29 @@
 ; 10 Uhr 54 Minuten
 (define time1 (make-time 10 54))
 (define time2 (make-time 14 32))
+
+; Minuten seit Mitternacht
+(: minutes-since-midnight (time -> natural))
+
+(check-expect (minutes-since-midnight time1)
+              654)
+(check-expect (minutes-since-midnight time2)
+              872)
+
+; Gerüst
+#;(define minutes-since-midnight
+  (lambda (time)
+    ...))
+
+; Schablone, entsteht aus der Signatur
+#;(define minutes-since-midnight
+  (lambda (time)
+    ; zusammengesetzte Daten als Input
+    ... (time-hour time) ...
+    ... (time-minute time) ...))
+
+(define minutes-since-midnight
+  (lambda (time)
+    ; zusammengesetzte Daten als Input
+    (+ (* 60 (time-hour time))
+       (time-minute time))))
