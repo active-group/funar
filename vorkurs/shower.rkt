dbfdc652a8d3a8af9d5ff71c6914ebc065d3e891
Mike Sperber <sperber@deinprogramm.de>
2024-10-07 14:31:40 +0200

    Update.

--- vorkurs/shower.rkt
+++ vorkurs/shower.rkt
@@ -6,4 +6,23 @@
 ; - Duschgel: besteht aus gleichen Teilen aus Seife und Shampoo
 ; gemischte Daten
 
+; Seife hat folgende Eigenschaften:
+; - pH-Wert
+(define-record soap
+  make-soap
+  soap?
+  (soap-ph number))
 
+; Shampoo hat folgende Eigenschaften:
+; - Haartyp
+(define-record shampoo
+  make-shampoo
+  shampoo?
+  (shampoo-hairtype hairtype))
+
+; Haartyp:
+; - Schuppen -ODER-
+; - trocken -ODER-
+; - fettig
+(define hairtype
+  (signature (enum "dandruff" "dry" "oily")))
