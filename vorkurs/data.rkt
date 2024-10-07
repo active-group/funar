07f377c5915515ad4a6bd48603b943168af199df
Mike Sperber <sperber@deinprogramm.de>
2024-10-07 10:58:14 +0200

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -8,10 +8,12 @@
 ; zusammengesetzte Daten
 (define-record time ; Signatur
   make-time ; Konstruktor
-  (time-hour   natural)
+  (time-hour   natural) ; Selektor ("Getter-Funktion")
   (time-minute natural))
 
 (: make-time (natural natural -> time))
+(: time-hour (time -> natural))
+(: time-minute (time -> natural))
 
 ; 10 Uhr 54 Minuten
 (define time1 (make-time 10 54))
