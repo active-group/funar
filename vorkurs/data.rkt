4d7148831b2bba5eabfd69d0056a68167e0ffc7a
Mike Sperber <sperber@deinprogramm.de>
2024-10-07 10:53:42 +0200

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -1,1 +1,12 @@
 #lang deinprogramm/sdp/beginner
+
+; Datenanalyse
+
+; Uhrzeit besteht aus/hat folgende Eigenschaften:
+; - Stunde -UND-
+; - Minute
+; zusammengesetzte Daten
+(define-record time
+  make-time ; Konstruktor
+  (time-hour natural)
+  (time-minute natural))
