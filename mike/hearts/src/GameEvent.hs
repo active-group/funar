9a028780e36049b2a9ee5e7e08598d014c6535eb
Mike Sperber <sperber@deinprogramm.de>
2024-10-08 16:50:52 +0200

    Update.

--- mike/hearts/src/GameEvent.hs
+++ mike/hearts/src/GameEvent.hs
@@ -4,3 +4,56 @@ import Cards
 
 import qualified Data.Map as Map
 import Data.Map (Map)
+
+-- Event: Datenrepräsentation von einem Ereignis
+
+-- - ist passiert, in der Vergangenheit
+-- - fachlich
+-- - vollständig: gesamte Geschichte der Domäne
+-- - Redundanz OK
+-- - *nicht* Zustand mit ablegen
+{-
+data GameEvent =
+    CardsDealt (Map Player Hand)
+  | GameStarted Player Player Player Player
+  | CardLaidAtTrickStart Player Card
+  | CardLaidIntoTrick Player Card
+  | CardsTaken Player [Card]
+  | GameWon Player
+  deriving Show
+
+-- Command: Repräsentation eines Wunschs, daß etwas passieren soll - in der Zukunft
+-- != Event
+data GameCommand =
+    DealCards Player [Card]
+  | StartGame
+  | PlayCard Player Card
+-}
+data GameEvent
+  = HandDealt Player Hand
+  | PlayerTurnChanged Player
+  | LegalCardPlayed Player Card
+  | TrickTaken Player Trick
+  | IllegalCardAttempted Player Card
+  | GameEnded Player
+  deriving (Show, Eq)
+
+data GameCommand
+  = DealHands (Map Player Hand)
+  | PlayCard Player Card
+  deriving (Show, Eq)
+
+
+
+{-
+playCard :: Player -> Card -> ... -> ...
+
+--> 
+
+data PlayCardCommand = MkPlayCardCommand Player Card
+
+playCard :: PlayCardCommand -> ... -> ...
+
+playCard p c ... -> playCard (MkPlayerCommand p c) ...
+
+-}
