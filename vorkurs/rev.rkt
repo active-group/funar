ed70e2951cce760a9521f62adde44de7f890aceb
Mike Sperber <sperber@deinprogramm.de>
2024-10-08 09:33:09 +0200

    Update.

--- vorkurs/rev.rkt
+++ vorkurs/rev.rkt
@@ -1,1 +1,34 @@
 #lang deinprogramm/sdp
+
+; Liste umdrehen
+(: rev ((list-of %a) -> (list-of %a)))
+
+(check-expect (rev (list 1 2 3 4))
+              (list 4 3 2 1))
+
+(define rev
+  (lambda (list)
+    (cond
+      ((empty? list) empty)
+      ((cons? list)
+       (append-element
+        (rev (rest list)) ; 4 3 2
+        (first list) ; 1
+      )))))
+
+
+; Element an eine Liste anhängen
+(: append-element ((list-of %a) %a -> (list-of %a)))
+
+(check-expect (append-element (list 1 2 3) 4)
+              (list 1 2 3 4))
+
+(define append-element
+  (lambda (list element)
+    (cond
+      ((empty? list) (cons element empty))
+      ((cons? list)
+       (cons
+        (first list)
+        (append-element (rest list) element))))))
+                   
