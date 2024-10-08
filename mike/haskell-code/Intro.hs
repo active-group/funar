4a5b3e67457ba536b12c06fdc6db745c2b494ce0
Mike Sperber <sperber@deinprogramm.de>
2024-10-08 10:15:41 +0200

    Update

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -8,7 +8,29 @@ x = 42
 
 double :: Integer -> Integer
 -- double x = x * 2
-double = \ x -> x * 2
+double =
+ \ x -> x * 2 -- \ lambda
 
 -- >>> double 42
 -- 84
+
+foo :: Integer -> Integer
+foo x =
+    let y = x * 2
+        z = y + 1
+    in x + y + z
+
+-- >>> foo 3
+-- 16
+
+-- Haustier ist eins der folgenden:
+-- - Hund -ODER-
+-- - Katze -ODER-
+-- - Schlange
+-- Fallunterscheidung/Aufzählung
+
+data Pet =
+    Dog 
+  | Cat
+  | Snake
+  deriving Show
