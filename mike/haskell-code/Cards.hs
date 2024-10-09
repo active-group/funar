913075cad1b9d3d0846c02f85db25adb9335cc29
Mike Sperber <sperber@deinprogramm.de>
2024-10-09 08:53:22 +0200

    Update.

---
+++ mike/haskell-code/Cards.hs
@@ -0,0 +1,7 @@
+module Cards where
+
+-- 1. Datenmodell für das französische Canastablatt ohne Joker
+-- Yaron Minsky: "Make illegal states unrepresentable."
+
+-- 2. Liste aller 52 Karten (aus den Farben und Wertigkeiten)
+-- 3. Funktion, um zwei Karten gleicher Farbe zu vergleichen
