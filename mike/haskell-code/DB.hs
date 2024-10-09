fa838d2eeb1982af15024f9f6c853a745db40619
Mike Sperber <sperber@deinprogramm.de>
2024-10-09 13:50:39 +0200

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -108,7 +108,8 @@ runDB (Return result) mp = (result, mp)
 -- >>> runDB p1 Map.empty
 -- ("201",fromList [("Mike",101)])
 
+io1 :: IO String
 io1 = do putStrLn "Mike"
          x <- getLine
-         putStrLn ("you typed" ++ x)
+         putStrLn ("you typed: " ++ x)
          return x
