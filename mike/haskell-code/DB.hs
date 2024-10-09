575f5eabd5553db8aa37c7344b6951a4181b2ffc
Mike Sperber <sperber@deinprogramm.de>
2024-10-09 11:59:49 +0200

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -1,1 +1,44 @@
 module DB where
+
+{-
+put "Mike" 100
+x = get "Mike"
+put "Mike" (x+1)
+y = get "Mike"
+return (show (x+y))
+-}
+
+-- Programm als Objekt
+
+type Key = String
+type Value = Integer
+
+-- 1. Versuch:
+
+{-
+data DBCommand a =
+    Put Key Value
+  | Get Key
+  | Return a
+
+type DBProgram a = [DBCommand a]
+
+p1 = [
+    Put "Mike" 100,
+    Get "Mike",
+    Put "Mike" (x+1) -- kein x ... blöd
+ ]
+-}
+
+-- mit Callbacks
+data DB a =
+    Get Key       (Value -> DB a)
+  | Put Key Value (()    -> DB a)
+  | Return a
+
+p1 :: DB String
+p1 = Put "Mike" 100 (\() ->
+     Get "Mike" (\x -> 
+     Put "Mike" (x+1) (\() ->
+     Get "Mike" (\y ->
+     Return (show (x+y))))))
