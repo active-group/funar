047c9595cae19f26904bbf7ad23e8562811e1f8d
Mike Sperber <sperber@deinprogramm.de>
2024-10-09 14:43:23 +0200

    Update.

--- mike/hearts/src/GameEvent.hs
+++ mike/hearts/src/GameEvent.hs
@@ -65,27 +65,45 @@ playCard p c ... -> playCard (MkPlayerCommand p c) ...
 
 data Game a =
     RecordEvent GameEvent (() -> Game a)
+  | IsPlayCardAllowed Player Card (Bool -> Game a)
   | Return a
 
 recordEventM :: GameEvent -> Game ()
 recordEventM event = RecordEvent event Return
+
+isPlayCardAllowedM :: Player -> Card -> Game Bool
+isPlayCardAllowedM player card = IsPlayCardAllowed player card Return
 
 instance Functor Game
 
 instance Applicative Game
 
 instance Monad Game where
+  (>>=) (RecordEvent event callback) next =
+    RecordEvent event (\() -> callback () >>= next) -- (\x -> callback x >>= next)
+  (>>=) (IsPlayCardAllowed player card callback) next =
+    IsPlayCardAllowed player card (\allowed -> 
+                                    callback allowed >>= next)
   (>>=) (Return result) next = next result
   return = Return
 
 -- data Maybe a = Nothing | Just a
 
+-- liefert Gewinner:in
 tableProcessCommandM :: GameCommand -> Game (Maybe Player) -- sagt uns, ob das Spiel vorbei ist und wer gewonnen hat
 tableProcessCommandM (DealHands hands) =
   let list = Map.toList hands
       events = map (uncurry HandDealt) list
       games = map recordEventM events
   in do sequence_ games
-        return Nothing
+        return Nothing -- Spiel geht weiter
 
-tableProcessCommandM (PlayCard player card) = undefined
+tableProcessCommandM (PlayCard player card) =
+  -- darf der das?
+  do allowed <- isPlayCardAllowedM player card
+     if allowed
+     then do recordEventM (LegalCardPlayed player card)
+             -- your program goes here
+             return undefined
+     else do recordEventM (IllegalCardAttempted player card)
+             return Nothing
