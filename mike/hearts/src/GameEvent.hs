56a39d6f0b0826a6837fd798342fd72edb4c3a71
Mike Sperber <sperber@deinprogramm.de>
2024-10-09 14:28:23 +0200

    Update.

--- mike/hearts/src/GameEvent.hs
+++ mike/hearts/src/GameEvent.hs
@@ -60,3 +60,32 @@ playCard p c ... -> playCard (MkPlayerCommand p c) ...
 
 -- als nächstes: Ablauf des Spiels / Anordnung der Events
 -- sequentielle Abläufe: Monade
+
+-- Wie werden aus Commands Events?
+
+data Game a =
+    RecordEvent GameEvent (() -> Game a)
+  | Return a
+
+recordEventM :: GameEvent -> Game ()
+recordEventM event = RecordEvent event Return
+
+instance Functor Game
+
+instance Applicative Game
+
+instance Monad Game where
+  (>>=) (Return result) next = next result
+  return = Return
+
+-- data Maybe a = Nothing | Just a
+
+tableProcessCommandM :: GameCommand -> Game (Maybe Player) -- sagt uns, ob das Spiel vorbei ist und wer gewonnen hat
+tableProcessCommandM (DealHands hands) =
+  let list = Map.toList hands
+      events = map (uncurry HandDealt) list
+      games = map recordEventM events
+  in do sequence_ games
+        return Nothing
+
+tableProcessCommandM (PlayCard player card) = undefined
