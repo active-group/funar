be5f8fc9193345c85c8749e78fcdb131cd709c08
Mike Sperber <sperber@deinprogramm.de>
2024-11-25 15:14:22 +0100

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -273,7 +273,14 @@ class Dillo {
   (first number) ; vorläufig
   (rest list-of-numbers))
 
-
+; 1elementige Liste: 5
+(define list1 (cons 5 empty))
+; 2elementige Liste: 2 5
+(define list2 (cons 2 (cons 5 empty)))
+; 3elementige Liste: 2 5 8
+(define list3 (cons 2 (cons 5 (cons 8 empty))))
+; 4elementige Liste: 7 2 5 8
+(define list4 (cons 7 list3))
 
 
 
