600a3433297be3317e8ca5140a73e1aecabef870
Mike Sperber <sperber@deinprogramm.de>
2024-11-26 15:57:15 +0100

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -5,9 +5,13 @@ module Contract where
   Zero-Bond / zero-coupon bond
   "Ich bekomme am 24.12.2024 100€."
 - Beispiel in "atomare Bestandteile" zerlegen
-  1. Währung
-  2. Betrag
+  1. Währung: "Ich bekomme 1€ jetzt."
+  2. Betrag: "Ich bekomme 100€ jetzt."
   3. Später
+- ... dabei Selbstbezüge eingeführt
+
+- Currency-Swap:
+  "Ich bekomme Weihnachten 100€ und ich zahle Weihnachten $100."
 -}
 
 data Date = MkDate String
@@ -30,5 +34,33 @@ zcb1 :: Contract
 zcb1 = ZeroCouponBond christmas 100 EUR
 -}
 
+data Direction = Long | Short
+  deriving Show
+
 data Contract =
-    
+    One Currency
+  | Value Amount Contract
+  | Later Date Contract
+  deriving Show
+
+
+-- "Ich bekomme 1€ jetzt."
+c1 :: Contract
+c1 = One EUR
+
+-- "Ich bekomme 100€ jetzt."
+c2 :: Contract
+c2 = Value 100 (One EUR)
+
+-- "Ich bekomme 2000€ jetzt."
+c3 :: Contract
+c3 = Value 20 c2
+
+zcb1 :: Contract
+zcb1 = Later christmas (Value 100 (One EUR))
+
+zeroCouponBond :: Date -> Amount -> Currency -> Contract
+zeroCouponBond date amount currency = Later date (Value amount (One currency))
+
+zcb1' :: Contract
+zcb1' = zeroCouponBond christmas 100 EUR
