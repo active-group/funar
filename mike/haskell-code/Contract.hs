a8d85c22bd3e56469f7cdcab0ed825de921d13a3
Mike Sperber <sperber@deinprogramm.de>
2024-11-26 16:32:09 +0100

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -75,3 +75,20 @@ c4 = Reverse (Value 100 (One GBP))
 
 -- "Ich bekomme 100GBP."
 c6 = Reverse c4
+
+-- "Ich bekomme Weihnachten 100€ und ich zahle Weihnachten $100."
+c7 = And zcb1 (Later christmas (Reverse (Value 100 (One USD))))
+c7' = Later christmas (And (Value 100 (One EUR)) (Reverse (Value 100 (One USD))))
+
+fxSwap :: Date -> Amount -> Currency -> Amount -> Currency -> Contract
+fxSwap date amount1 currency1 amount2 currency2 =
+    And (zeroCouponBond date amount1 currency1)
+        (Reverse (zeroCouponBond date amount2 currency2))
+
+data Payment = MkPayment Direction Date Amount Currency
+  deriving Show
+
+-- Bedeutung / Denotation
+
+-- Zahlungen bis Datum + Residualvertrag
+denotation :: Contract -> Date -> ([Payment], Contract)
