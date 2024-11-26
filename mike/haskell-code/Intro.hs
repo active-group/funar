5cc66df35b0d5e88a84a1d873cb5abc56c2e1ec3
Mike Sperber <sperber@deinprogramm.de>
2024-11-26 10:40:16 +0100

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -2,3 +2,39 @@
 module Intro where
 
 import Prelude hiding (Semigroup, Monoid)
+
+x :: Integer
+x = 42
+
+y :: Integer
+y = x * 2
+
+double :: Integer -> Integer
+-- double = \ x -> x * 2
+double x = x * 2
+
+-- >>> double 7
+-- 14
+
+-- Haustier ist eins der folgenden:
+-- - Hund -ODER-
+-- - Katze -ODER-
+-- - Schlange
+data Pet
+  = Dog
+  | Cat 
+  | Snake 
+ deriving Show
+
+-- Ist Haustier niedlich?
+isCute :: Pet -> Bool
+
+-- >>> isCute Dog
+-- True
+-- >>> isCute Snake
+-- False
+
+-- eine Gleichung pro Fall
+isCute Dog = True
+isCute Cat = True
+isCute Snake = False
