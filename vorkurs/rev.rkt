7a9a343868ff4e05113da0b337faedc64d8736df
Mike Sperber <sperber@deinprogramm.de>
2024-11-26 09:54:32 +0100

    Update.

--- vorkurs/rev.rkt
+++ vorkurs/rev.rkt
@@ -9,9 +9,12 @@
     (cond
       ((empty? list) empty)
       ((cons? list)
-       (adjoin
+       (adjoin ; Kontext von rev-Aufruf
         (rev (rest list))
         (first list))))))
+
+; O(n^2) = 1 + 2 + 3 + ... + (n-1) + n
+; (n+1)*n/2 = n^2 + ...
 
 ; Element hinten an Liste anhängen
 (: adjoin ((list-of %a) %a -> (list-of %a)))
@@ -27,3 +30,19 @@
        (cons
         (first list)
         (adjoin (rest list) element))))))
+
+(: rev2 ((list-of %a) (list-of %a) -> (list-of %a)))
+
+(check-expect (rev2 (list 1 2 3 4) empty)
+              (list 4 3 2 1))
+
+(define rev2
+  (lambda (list acc) ; Akkumulator
+    (cond
+      ((empty? list) acc)
+      ((cons? list)
+       (rev2 (rest list)
+             (cons (first list) acc))))))
+    
+    
+    
