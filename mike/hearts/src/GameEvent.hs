78bdd53927217d1ea7e4dd849c305120535b2ee1
Mike Sperber <sperber@deinprogramm.de>
2024-11-27 17:16:07 +0100

    Events for hearts

--- mike/hearts/src/GameEvent.hs
+++ mike/hearts/src/GameEvent.hs
@@ -5,3 +5,23 @@ import Cards
 import qualified Data.Map as Map
 import Data.Map (Map)
 
+-- Event:
+-- - Beschreibung von Ereignis, das passiert ist
+-- - Reihenfolge (wahrscheinlich) wichtig
+-- - fachlich
+-- - (unverändlich)
+-- - "vollständig"
+-- - Redundanz OK
+
+data GameEvent =
+    GameStarted [Player]
+  | CardsDealt [(Player, Hand)]
+  | PlayerPlayedCard Player Card
+  | RoundPlayed Player [Card]
+  | GameEnded Player
+  | PlayerTurnChanged Player
+  deriving Show
+
+-- Command:
+-- Beschreibung eines Wunsches, daß etwas passieren möge
+-- ... noch nicht in der Vergangenheit
