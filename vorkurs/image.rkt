3d4e6c9ce2ed3f7808a037ceeccb263542442aeb
Mike Sperber <sperber@deinprogramm.de>
2025-03-24 09:44:26 +0100

    Update.

--- vorkurs/image.rkt
+++ vorkurs/image.rkt
@@ -11,3 +11,13 @@
 (define circle1 (circle 50 "solid" "red"))
 (define square1 (square 100 "outline" "magenta"))
 (define star1 (star 50 "solid" "gold"))
+
+(define overlay1 (overlay star1 circle1))
+
+(above
+ (beside circle1 star1)
+ (beside star1 circle1))
+
+(above
+ (beside square1 circle1)
+ (beside circle1 square1))
