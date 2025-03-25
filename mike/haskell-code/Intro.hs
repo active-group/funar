c97b3cf52dfa12e81fb04363e3f7897cc88568cc
Mike Sperber <sperber@deinprogramm.de>
2025-03-25 10:30:49 +0100

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -1,3 +1,60 @@
 {-# LANGUAGE InstanceSigs #-}
 module Intro where
 
+x :: Integer
+x = 12 + 23 * 2
+
+-- Intuition: "Konstanten" groß, "Variablen" klein
+double :: Integer -> Integer
+-- double = \ x -> x * 2
+double x = x * 2
+
+-- Kommentar
+-- >>> double 18 
+-- 36
+
+quadruple :: Integer -> Integer
+quadruple x =
+    let d = double x
+    in d * d
+
+-- >>> quadruple 4 
+-- 64
+
+data Pet
+  = Dog
+  | Cat
+  | Snake
+  deriving Show
+
+-- Ist Haustier niedlich?
+isCute :: Pet -> Bool
+-- eine Gleichung pro Fall
+-- Schablone:
+-- isCute Dog = undefined
+-- isCute Cat = undefined
+-- isCute Snake = undefined
+
+-- isCute pet =
+--     case pet of
+--         Dog -> True
+--         Cat -> True
+--         Snake -> False
+
+isCute Dog = True
+isCute Cat = True
+isCute Snake = False
+
+-- >>> isCute Dog
+-- True
+-- >>> isCute Snake
+-- False
+
+-- Gürteltier hat folgende Eigenschaften:
+-- -- lebendig oder tot
+-- -- Gewicht
+
+data Liveness = Alive | Dead
+  deriving Show
+
+data Dillo = MkDillo { }
