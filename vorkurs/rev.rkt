429c6b4fbcd2ebe8bb703163ba23b605f0759b36
Mike Sperber <sperber@deinprogramm.de>
2025-03-25 09:12:25 +0100

    Update.

--- vorkurs/rev.rkt
+++ vorkurs/rev.rkt
@@ -9,10 +9,25 @@
 (define rev
   (lambda (list)
     (cond
+      ((empty? list) empty)
+      ((cons? list)
+       (add-element
+        (rev (rest list))
+        (first list))))))
+
+; Element an eine Liste anhängen
+(: add-element ((list-of %a) %a -> (list-of %a)))
+
+(check-expect (add-element (list 1 2 3) 4)
+              (list 1 2 3 4))
+
+(define add-element
+  (lambda (list element)
+    (cond
       ((empty? list) ...)
       ((cons? list)
        ...
        (first list)
-       (rev (rest list))
+       (add-element (rest list) element)
        ...))))
        
