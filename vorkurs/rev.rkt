3e9ff3a841108e2f2c0af184ead3772e79eae1b8
Mike Sperber <sperber@deinprogramm.de>
2025-03-25 09:26:24 +0100

    Update.

--- vorkurs/rev.rkt
+++ vorkurs/rev.rkt
@@ -15,6 +15,11 @@
         (rev (rest list))
         (first list))))))
 
+; Liste Länge n
+; 1 + 2 + 3 + ... + (n - 1) + n = n * (n + 1) / 2
+; = O(n^2)
+
+
 ; Element an eine Liste anhängen
 (: add-element ((list-of %a) %a -> (list-of %a)))
 
@@ -29,6 +34,34 @@
        (cons
         (first list)
         (add-element (rest list) element))))))
+
+
+; mit Zwischenergebnis/Akkumulator
+(: rev-2 ((list-of %a) (list-of %a) -> (list-of %a)))
+
+(check-expect (rev-2 (list 1 2 3 4) empty)
+              (list 4 3 2 1))
+
+(define rev-2
+  (lambda (list acc)
+    (cond
+      ((empty? list) acc)
+      ((cons? list)
+       (rev-2 (rest list)
+              (cons (first list) acc))))))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
