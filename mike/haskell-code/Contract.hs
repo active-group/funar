690b5cf34d128542c831d79e5ff602bc9a1d4a8f
Mike Sperber <sperber@deinprogramm.de>
2025-03-26 09:24:13 +0100

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -1,2 +1,16 @@
 module Contract where
 
+{-
+Implizit: Vertrag zwischen zwei Parteien, eine davon "ich"
+
+zero-coupon bond / Zero-Bond
+"Ich bekomme am 24.12.2025 100€."
+-}
+
+newtype Date = MkDate String -- wie data
+  deriving (Eq, Ord)
+
+christmas = MkDate "2025-12-24"
+
+data Contract 
+    = ZeroCouponBond Date Amount Currency
