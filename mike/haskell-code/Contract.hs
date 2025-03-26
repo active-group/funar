b41c3e581d503177fdfb4054f6c4c0c5b26def43
Mike Sperber <sperber@deinprogramm.de>
2025-03-26 09:32:30 +0100

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -3,8 +3,16 @@ module Contract where
 {-
 Implizit: Vertrag zwischen zwei Parteien, eine davon "ich"
 
+1. einfaches Beispiel
 zero-coupon bond / Zero-Bond
 "Ich bekomme am 24.12.2025 100€."
+
+2. Beispiel zerlegen in "atomare Bestandteile" / "Ideen"
+   z.B. entlang der Attribute
+   - Währung - "Ich bekomme jetzt 1EUR."
+   - Betrag
+   - "Später"
+
 -}
 
 newtype Date = MkDate String -- wie data
@@ -17,9 +25,17 @@ type Amount = Double
 data Currency = EUR | GBP | USD | YEN
   deriving Show
 
+{-
 data Contract 
     = ZeroCouponBond Date Amount Currency
   deriving Show
 
 zcb1 :: Contract
 zcb1 = ZeroCouponBond christmas 100 EUR
+-}
+
+data Contract
+    = One Currency
+    deriving Show
+
+c1 = One EUR -- "Ich bekomme 1€ jetzt."
