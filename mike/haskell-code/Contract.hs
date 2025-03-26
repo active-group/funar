e86a0a17e4ca8e267fd57b3dd6fe284907ace0df
Mike Sperber <sperber@deinprogramm.de>
2025-03-26 09:59:47 +0100

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -17,7 +17,11 @@ zero-coupon bond / Zero-Bond
 
 ... und von vorne.
 
+Currency-Swap / FxSwap:
 
+Am 24.12.2025:
+- Ich bekomme 100EUR und
+  ich bezahle 100GBP.
 
 -}
 
@@ -40,10 +44,15 @@ zcb1 :: Contract
 zcb1 = ZeroCouponBond christmas 100 EUR
 -}
 
+data Direction = Long | Short
+  deriving Show
+
 data Contract
     = One Currency
     | Many Amount Contract
     | Later Date Contract
+    | Inverse Contract
+    | Combine Contract Contract
     deriving Show
 
 c1 = One EUR -- "Ich bekomme 1€ jetzt."
@@ -52,6 +61,8 @@ c1 = One EUR -- "Ich bekomme 1€ jetzt."
 c2 = Many 12 (One EUR) -- "Ich bekomme 12€ jetzt."
 
 c3 = Many 10 c2 -- "Ich bekomme 120€ jetzt."
+
+c4 = Inverse (Inverse c3)
 
 -- "Ich bekomme am 24.12.2025 100€."
 zcb1' = Later christmas (Many 100 (One EUR))
@@ -62,3 +73,8 @@ zeroCouponBond date amount currency =
 
 zcb1 :: Contract
 zcb1 = zeroCouponBond christmas 100 EUR
+
+fxSwap1 :: Contract
+fxSwap1 = Later christmas (Combine (Many 100 (One EUR))
+                                   (Inverse (Many 100 (One GBP))))
+
