fb4052345942c3586b110ec803641932ec251870
Mike Sperber <sperber@deinprogramm.de>
2025-03-26 14:26:21 +0100

    Update.

--- mike/hearts/src/GameEvent.hs
+++ mike/hearts/src/GameEvent.hs
@@ -5,3 +5,14 @@ import Cards
 import qualified Data.Map as Map
 import Data.Map (Map)
 
+{-
+Event: Datenwert, der ein Ereignis beschreibt
+- ist passiert - in der Vergangenheit
+- alle Informationen über das Ereignis
+- Redundanz OK
+- Zustand nicht OK
+- fachlich, nicht technisch
+
+Commands: Datenwert, der einen Wunsch beschreibt, daß etwas passiert
+- in der Zukunft, noch nicht passiert
+-}
