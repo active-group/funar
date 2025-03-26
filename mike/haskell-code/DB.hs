ea598148e0b2b0d928524befa635a07135dae569
Mike Sperber <sperber@deinprogramm.de>
2025-03-26 15:12:55 +0100

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -1,1 +1,39 @@
 module DB where
+
+{-
+put "Mike" 50
+x = get "Mike"
+put "Mike" (x+1)
+y = get "Mike"
+return (show (x + y))
+-}
+
+type Key = String
+type Value = Integer
+
+{-
+data DBCommand result =
+    Put Key Value
+  | Get Key
+  | Return result
+
+type DBProgram result = [DBCommand result]
+
+p1 = [Put "Mike" 50,
+      Get "Mike"] -- ups
+-}
+data DB a =
+    Get Key (Value -> DB a)
+  | Put Key Value (() -> DB a)
+  | Return a
+
+p1 :: DB String
+p1 = Put "Mike" 50 (\() ->
+     Get "Mike" (\x -> 
+     Put "Mike" (x+1) (\() ->
+     Get "Mike" (\y ->
+     Return (show (x+y))))))
+
+-- fehlt noch:
+-- 1. laufenlassen
+-- 2. schick aussehen (ohne viele Klammern)
