23f741cf0b43cb439b401c051b8c52e4936835c0
Mike Sperber <sperber@deinprogramm.de>
2025-05-13 16:16:36 +0200

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -1,4 +1,20 @@
 module Contract where
 
 -- Finanzderivat:
--- (Bedingungen des) Vertrag zwischen zwei Parteien
+-- (Bedingungen des) Vertrag zwischen zwei Parteien, "egozentrisch" aus Sicht der Bank
+
+-- Domänenanalyse:
+
+-- - einfaches Beispiel
+--   Zero-Bond / zero-coupon bond
+--   "Ich bekomme Weihnachten 100€."
+
+data Date = MkDate String
+  deriving (Show, Eq, Ord)
+
+-- >>> :info Ord
+
+data Contract =
+    ZeroCouponBond Date Amount Currency
+    deriving Show
+
