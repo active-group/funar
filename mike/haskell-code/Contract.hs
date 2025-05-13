3046a82f36c12c973d7c9420308a02325c0c5e11
Mike Sperber <sperber@deinprogramm.de>
2025-05-13 16:20:40 +0200

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -9,12 +9,29 @@ module Contract where
 --   Zero-Bond / zero-coupon bond
 --   "Ich bekomme Weihnachten 100€."
 
-data Date = MkDate String
+newtype Date = MkDate String -- newtype: 1 Fall, 1 Attribut, wie data
   deriving (Show, Eq, Ord)
 
 -- >>> :info Ord
+-- type Ord :: * -> Constraint
+-- class Eq a => Ord a where
+--   compare :: a -> a -> Ordering
+--   (<) :: a -> a -> Bool
+--   (<=) :: a -> a -> Bool
+--   (>) :: a -> a -> Bool
+--   (>=) :: a -> a -> Bool
+--   max :: a -> a -> a
+--   min :: a -> a -> a
 
-data Contract =
-    ZeroCouponBond Date Amount Currency
+type Amount = Double
+
+data Currency =
+    EUR | GBP | USD | YEN | CHF
     deriving Show
 
+data Contract =
+   ZeroCouponBond Date Amount Currency
+   deriving Show
+
+zcb1 :: Contract
+zcb1 = ZeroCouponBond (MkDate "2025-12-24") 100 EUR
