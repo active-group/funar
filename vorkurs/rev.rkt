6fd9b59ad6eee7058be24ac59439128e032d7ecb
Mike Sperber <sperber@deinprogramm.de>
2025-05-13 09:33:21 +0200

    Update.

--- vorkurs/rev.rkt
+++ vorkurs/rev.rkt
@@ -3,7 +3,7 @@
 ; Liste umdrehen
 (: rev ((list-of %a) -> (list-of %a)))
 
-(check-expect (rev (list 1 2 3))
+#;(check-expect (rev (list 1 2 3))
               (list 3 2 1))
 
 (define rev
@@ -25,7 +25,7 @@
 
 (: add-element ((list-of %a) %a -> (list-of %a)))
 
-(check-expect (add-element (list 3 2) 1)
+#;(check-expect (add-element (list 3 2) 1)
               (list 3 2 1))
 
 (define add-element
