62bc3fb1541d8db4d720fbf7cf4ca29d7b0e8c21
Mike Sperber <sperber@deinprogramm.de>
2025-05-14 14:30:30 +0200

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -1,1 +1,33 @@
 module DB where
+
+{-
+put "Mike" 100
+x = get "Mike"
+put "Mike" (x+1)
+y = get Mike
+return (show (x+y))
+-}
+
+type Key = String
+type Value = Integer
+
+{-
+-- Ablauf als Daten
+data DBCommand result =
+    Put Key Value
+  | Get Key
+  | Return result
+
+type DBProgram result = [DBCommand result]
+
+p1 = [
+    Put "Mike" 100,
+    Get "Mike",
+    Put "Mike" -- abhängig vom Ergebnis vom Get davor
+]
+-}
+
+data DB a = -- a statt result
+    Get Key (Value -> DB a) -- Callback/Continuation
+  | Put Key Value (() -> DB a)
+
