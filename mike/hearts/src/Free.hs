b53dbe00c133c985bb5a58625b6b33bec3680841
Mike Sperber <sperber@deinprogramm.de>
2025-05-15 14:22:31 +0200

    Update.

--- mike/hearts/src/Free.hs
+++ mike/hearts/src/Free.hs
@@ -1,24 +1,24 @@
 module Free where 
 
 data Free f a =
-     Pure a
+     Return a
    | Impure (f (Free f a))
 
 instance Functor f => Functor (Free f) where
-  fmap f (Pure a) = Pure (f a)
+  fmap f (Return a) = Return (f a)
   fmap f (Impure r) = Impure (fmap (fmap f) r)
 
 instance Functor f => Applicative (Free f) where
-  pure = Pure
-  (Pure f) <*> aa = fmap f aa
+  pure = Return
+  (Return f) <*> aa = fmap f aa
   (Impure fa) <*> aa = Impure (fmap (<*> aa) fa)
 
 instance Functor f => Monad (Free f) where
-    return = Pure
+    return = Return
     
-    (Pure a) >>= k = k a
+    (Return a) >>= k = k a
     (Impure r) >>= k = Impure (fmap (>>= k) r)
 
 runFree :: Monad m => (f (Free f a) -> (Free f a -> m a) -> m a) -> Free f a -> m a
-runFree _ (Pure result) = return result
+runFree _ (Return result) = return result
 runFree rrr (Impure command) = rrr command (runFree rrr)
