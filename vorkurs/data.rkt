f2d0f69e5cf586809500de442df7793abf8ff05a
Mike Sperber <sperber@deinprogramm.de>
2025-09-15 10:30:43 +0200

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -1,1 +1,11 @@
 #lang deinprogramm/sdp/beginner
+; Datendefinition
+; Haustier ist eins der folgenden:
+; - Hund ODER
+; - Katze ODER
+; - Schlange
+; Fallunterscheidung / Summe
+; Aufzählung
+
+; -> für jede Datendefinition eine Signatur
+(define pet (signature (enum "dog" "cat" "snake")))
