92430de5717d45179f75e3948400e1f07d901850
Mike Sperber <sperber@deinprogramm.de>
2025-09-16 11:03:51 +0200

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -77,7 +77,14 @@ dillo1 = MkDillo { dilloLiveness = Alive, dilloWeight = 10 }
 
 dillo2 :: Dillo
 dillo2 = MkDillo Dead 8
+-- dillo2 = MkDillo Dead 8
 
 runOverDillo :: Dillo -> Dillo
-runOverDillo dillo =
-    MkDillo { dilloLiveness = Dead, dilloWeight = dilloWeight dillo }
+
+-- >>> runOverDillo dillo1
+-- MkDillo {dilloLiveness = Dead, dilloWeight = 10}
+
+-- runOverDillo dillo = MkDillo { dilloLiveness = Dead, dilloWeight = dilloWeight dillo }
+-- runOverDillo dillo = MkDillo Dead (dilloWeight dillo)
+runOverDillo (MkDillo { dilloLiveness = l, dilloWeight = w }) =
+    MkDillo Dead w
