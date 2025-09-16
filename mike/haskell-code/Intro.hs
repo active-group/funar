ed1901d56a46a552c5187b5103aeee4d403a59a5
Mike Sperber <sperber@deinprogramm.de>
2025-09-16 10:35:06 +0200

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -8,9 +8,14 @@ y :: Integer
 y = x + 2
 
 double :: Integer -> Integer
-double = \x -> x * 2
+-- double = \x -> x * 2
+double x = x * 2
 
 -- >>> double 21
 -- 42
 
-
+quadruple :: Integer -> Integer
+quadruple x =
+    let d = double x
+    in double d 
+ 
