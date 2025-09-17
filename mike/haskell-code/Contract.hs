98bae83f0678dfe3d8fb45d99677b61dc86e6e9d
Mike Sperber <sperber@deinprogramm.de>
2025-09-17 10:02:27 +0200

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -79,7 +79,32 @@ data Payment = MkPayment Date Direction Amount Currency
 -- operationelle Semantik
 -- Zahlungen bis zu einem bestimmten Zeitpunkt + Residualvertrag
 meaning :: Contract -> Date -> ([Payment], Contract)
+meaning Zero today = ([], Zero)
+meaning (One currency) today = ([MkPayment today Long 1 currency], Zero)
+meaning (WithAmount amount contract) today =
+  let (payments, residualContract) = meaning contract today
+   in (map (scalePayment amount) payments, WithAmount amount residualContract)
+meaning (WithChangedDirection contract) today =
+  let (payments, residualContract) = meaning contract today
+   in (map invertPayment payments, WithChangedDirection residualContract)
+meaning c@(WithDate date contract) today =
+  if today >= date
+    then meaning contract today
+    else ([], c) -- WithDate date contract
+meaning (Two contract1 contract2) today =
+  let (payments1, residualContract1) = meaning contract1 today
+      (payments2, residualContract2) = meaning contract2 today
+  in (payments1 ++ payments2, Two residualContract1 residualContract2)
 
+scalePayment :: Amount -> Payment -> Payment
+scalePayment factor (MkPayment direction date amount currency) =
+  MkPayment direction date (factor * amount) currency
+
+invertPayment :: Payment -> Payment
+invertPayment (MkPayment date Long amount currency) =
+  MkPayment date Short amount currency
+invertPayment (MkPayment date Short amount currency) =
+  MkPayment date Long amount currency
 
 -- >>> meaning c6 (MkDate "2025-10-01")
 c6 = WithAmount 100 (Two (One EUR) (WithDate xmas (One EUR)))
