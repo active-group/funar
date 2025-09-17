71810a6b76e72de31ca6494f86b43faa7bdb3060
Mike Sperber <sperber@deinprogramm.de>
2025-09-17 16:04:28 +0200

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 module DB where
 
 {-
@@ -58,3 +59,26 @@ splice (Put key value callback) next =
       ( \()    -> splice (callback ()) next)
 splice (Return result) next = next result
 
+-- >>> :info Monad
+-- type Monad :: (* -> *) -> Constraint
+-- class Applicative m => Monad m where
+--   -- "bind"
+--   (>>=) :: m a -> (a -> m b) -> m b
+--   return :: a -> m a
+
+instance Functor DB where
+
+instance Applicative DB where
+    
+instance Monad DB where
+    (>>=) :: DB a -> (a -> DB b) -> DB b
+    (>>=) = splice
+    return = Return
+
+-- syntaktischer Zucker für p1'
+p1''' :: DB String
+p1''' = do put "Mike" 100
+           x <- get "Mike"
+           put "Mike" (x+1)
+           y <- get "Mike"
+           return (show (x+y))
