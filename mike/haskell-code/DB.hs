d7ea983d86f8a14a15d6b5fe2129ceef5ad90ab3
Mike Sperber <sperber@deinprogramm.de>
2025-09-17 15:33:58 +0200

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -7,3 +7,37 @@ put "Mike" (x+1)
 y = get "Mike"
 return (show (x+y))
 -}
+
+type Key = String
+type Value = Integer
+
+{-
+data DBCommand result =
+    Put Key Value
+  | Get Key
+  | Return result
+
+type DBProgram result = [DBCommand result]
+
+-- tut nicht:
+p1 = [Put "Mike" 100,
+      Put "Mike" (Get "Mike")]
+-}
+data DB result =
+    Get Key       (Value -> DB result)
+  | Put Key Value (()    -> DB result)
+  | Return result
+
+p1 :: DB String
+p1 = Put "Mike" 100 (\() ->
+     Get "Mike" (\x ->
+     Put "Mike" (x+1) (\() ->
+     Get "Mike" (\y ->
+     Return (show (x+y))))))
+
+get :: Key -> DB Value
+-- get key = Get key (\value -> Return value)
+get key = Get key Return
+
+put :: Key -> Value -> DB ()
+put key value = Put key value Return
