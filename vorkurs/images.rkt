0bbf0c1f20a3305f7d8bfaed1f310d0c79a5c71d
Mike Sperber <sperber@deinprogramm.de>
2025-10-06 09:57:07 +0200

    Update.

--- vorkurs/images.rkt
+++ vorkurs/images.rkt
@@ -10,3 +10,23 @@
 (define circle1 (circle 50 "solid" "red"))
 (define star1 (star 50 "solid" "green"))
 (define square1 (square 100 "outline" "blue"))
+(define overlay1 (overlay star1 circle1))
+
+
+(above
+ (beside star1 circle1)
+ (beside circle1 star1))
+
+(above
+ (beside circle1 square1)
+ (beside square1 circle1))
+
+; Abstraktion - 2 ähnliche Code-Stellen
+; - noch einmal kopieren
+; - Unterschiede durch abstrakte Namen ersetzen
+; - Namen in lambda
+
+(lambda (image1 image2)
+  (above
+   (beside image1 image2)
+   (beside image2 image1)))
