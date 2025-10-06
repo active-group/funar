7ff79a69c94fd8c03a2d1621210445896cff3e0b
Mike Sperber <sperber@deinprogramm.de>
2025-10-06 12:00:44 +0200

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -118,3 +118,45 @@
   (lambda (minutes)
     (make-time (quotient minutes 60)
                (remainder minutes 60))))
+
+; Tiere auf dem texanischen Highway
+
+; Gürteltier hat folgende Eigenschaften:
+; - lebendig? -UND-
+; - Gewicht
+(define-record dillo
+  make-dillo
+  (dillo-alive? boolean)
+  (dillo-weight number))
+; "Zustand des Gürteltiers zu einem bestimmten Zeitpunkt"
+
+(: make-dillo (boolean number -> dillo))
+(: dillo-alive? (dillo -> boolean))
+(: dillo-weight (dillo -> number))
+
+; lebendiges Gürteltier, 10kg
+(define dillo1 (make-dillo #t 10))
+
+; totes Gürteltier, 8kg
+(define dillo2 (make-dillo #f 8))
+
+; Gürteltier überfahren
+(: run-over-dillo (dillo -> dillo))
+
+(check-expect (run-over-dillo dillo1)
+              (make-dillo #f 10))
+(check-expect (run-over-dillo dillo2)
+              dillo2)
+
+; Schablone
+#;(define run-over-dillo
+  (lambda (dillo)
+    (make-dillo ... ...)
+    ...
+    (dillo-alive? dillo)
+    (dillo-weight dillo)
+    ...))
+
+(define run-over-dillo
+  (lambda (dillo)
+    (make-dillo #f (dillo-weight dillo))))
