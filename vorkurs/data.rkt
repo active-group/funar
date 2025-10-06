c02bf1399ea3eb96296bb981a796fb1b2ee9a253
Mike Sperber <sperber@deinprogramm.de>
2025-10-06 10:44:06 +0200

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -1,1 +1,10 @@
 #lang deinprogramm/sdp/beginner
+
+; Haustier ist eins der folgenden:
+; - Hund -ODER-
+; - Katze -ODER-
+; - Schlange
+; Fallunterscheidung
+; hier speziell: Aufzählung / "Enumeration"
+
+; Ist Haustier niedlich?
