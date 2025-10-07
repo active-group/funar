76b3dd69cd4d209ee0545020ac01e766704ffac0
Mike Sperber <sperber@deinprogramm.de>
2025-10-07 10:57:50 +0200

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -1,2 +1,22 @@
 {-# LANGUAGE InstanceSigs #-}
 module Intro where
+
+x :: Integer
+x = 42
+
+y :: Integer
+y = x + 5
+
+-- Zahl verdoppeln
+double :: Integer -> Integer
+-- double = \ n -> n * 2
+double n = n * 2 -- syntaktischer Zucker
+
+
+-- >>> double 21
+-- 42
+
+quadruple :: Integer -> Integer
+quadruple n =
+    let d = double n
+    in double d
