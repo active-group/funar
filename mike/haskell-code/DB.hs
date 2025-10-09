2442655c762041398ec0aa4783e44dc8dc5c38a5
Mike Sperber <sperber@deinprogramm.de>
2025-10-09 10:12:03 +0200

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -1,5 +1,8 @@
 module DB where
 
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map, (!))
+
 {-
    put "Mike" 100
    x = get "Mike"
@@ -38,3 +41,23 @@ p1 = Put "Mike" 100 (\() ->
      Put "Mike" (x+1) (\() ->
      Get "Mike" (\y -> 
      Return (show (x+y))))))
+
+-- notationelle Annäherung
+get :: Key -> DB Value
+get key = Get key Return
+
+put :: Key -> Value -> DB ()
+put key value = Put key value Return -- (\() -> Return ())
+
+-- >>> runDB p1 Map.empty
+-- ("201",fromList [("Mike",101)])
+
+-- "dependency injection"
+runDB :: DB a -> Map Key Value -> (a, Map Key Value)
+runDB (Get key callback) mp =
+    let value = mp ! key
+    in runDB (callback value) mp
+runDB (Put key value callback) mp = 
+    let mp' = Map.insert key value mp
+    in runDB (callback ()) mp'
+runDB (Return result) mp = (result, mp)
