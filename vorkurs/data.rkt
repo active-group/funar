58bf4a9afb6aceb8f0b05f2b1189e6a77d5ee840
Mike Sperber <sperber@deinprogramm.de>
2025-11-17 11:17:56 +0100

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -1,1 +1,52 @@
 #lang deinprogramm/sdp/beginner
+
+; Datenanalyse
+
+; Haustier ist eins der folgenden:
+; - Hund -ODER-
+; - Katze -ODER-
+; - Schlange
+; -> Fallunterscheidung, hier: Aufzählung
+
+; "dog", "cat", "snake"
+
+; pro Datensorte eine Signatur
+(define pet
+  (signature (enum "dog" "cat" "snake")))
+
+; Haustier niedlich?
+
+(: cute? (pet -> boolean)) ; ? wg. boolean
+
+(check-expect (cute? "dog")
+              #t)
+(check-expect (cute? "cat")
+              #t)
+(check-expect (cute? "snake")
+              #f)
+
+; Gerüst
+#;(define cute?
+  (lambda (pet)
+    ...))
+
+; Schablone
+#;(define cute?
+  (lambda (pet)
+    ; Verzweigung (<- Fallunterscheidung)
+    (cond
+      ; 1 Zweig pro Fall
+      ; (<Bedingung> <Ergebnis>)
+      ((equal? pet "dog") ...)
+      ((equal? pet "cat") ...)
+      ((equal? pet "snake") ...))))
+
+(define cute?
+  (lambda (pet)
+    ; Verzweigung (<- Fallunterscheidung)
+    (cond
+      ; 1 Zweig pro Fall
+      ; (<Bedingung> <Ergebnis>)
+      ((equal? pet "dog") #t)
+      ((equal? pet "cat") #t)
+      ((equal? pet "snake") #f))))
