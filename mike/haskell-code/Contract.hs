c527c4e72b17adfa5ed781edfdbea7217928da8f
Mike Sperber <sperber@deinprogramm.de>
2025-11-18 16:17:54 +0100

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -62,6 +62,12 @@ data Contract =
   | And Contract Contract
   deriving Show
 
+-- "smart constructor"
+and :: Contract -> Contract -> Contract
+and Zero c = c
+and c Zero = c
+and c1 c2 = And c1 c2
+
 -- "Ich bekomme 1€ jetzt."
 c1 :: Contract
 c1 = One EUR
@@ -108,11 +114,38 @@ fxSwap1' = And (zeroCouponBond xmas 100 EUR)
 data Payment = MkPayment Date Direction Amount Currency
   deriving Show
 
+scalePayment :: Amount -> Payment -> Payment
+scalePayment factor (MkPayment direction date amount currency) =
+  MkPayment direction date (factor * amount) currency
+
+invertPayment :: Payment -> Payment
+invertPayment (MkPayment date Long amount currency) =
+  MkPayment date Short amount currency
+invertPayment (MkPayment date Short amount currency) =
+  MkPayment date Long amount currency
+
 -- Bedeutung eines Vertrags
 -- "Zahlungen bis zum Datum (heute)" + Residualvertrag
--- meaning :: Contract -> Date -> ([Payment], Contract)
+meaning :: Contract -> Date -> ([Payment], Contract)
+meaning Zero today = ([], Zero)
+meaning (One currency) today = ([MkPayment today Long 1 currency], Zero)
+meaning (Multiple amount contract) today =
+  let (payments, residualContract) = meaning contract today
+   in (map (scalePayment amount) payments, Multiple amount residualContract)
+meaning (Shorten contract) today =
+  let (payments, residualContract) = meaning contract today
+   in (map invertPayment payments, Shorten residualContract)
+meaning (Later date contract) today =
+  if today >= date
+    then meaning contract today
+    else ([], Later date contract)
+meaning (And contract1 contract2) today =
+  let (payments1, residualContract1) = meaning contract1 today
+      (payments2, residualContract2) = meaning contract2 today
+   in (payments1 ++ payments2, And residualContract1 residualContract2)
 
 -- >>> meaning c9 (MkDate "2025-12-01")
+-- ([MkPayment (MkDate "2025-12-01") Long 100.0 EUR],Multiple 100.0 (And Zero (Later (MkDate "2025-12-24") (One EUR))))
 
 c9 :: Contract
 c9 = Multiple 100 (And (One EUR) (Later xmas (One EUR)))
