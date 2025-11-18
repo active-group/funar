3866308f853b7da735d4b1b120039e4b7d250846
Mike Sperber <sperber@deinprogramm.de>
2025-11-18 14:12:00 +0100

    Update.

--- mike/haskell-code/Contract.hs
+++ mike/haskell-code/Contract.hs
@@ -6,6 +6,17 @@ Identifizieren, worum es geht - besondere "Dinge".
 1. einfaches Beispiel
    Zero-Coupon Bond / Zero-Bond
    "Ich bekomme Weihnachten 100€."
+
+2. Beispiel in "atomare" Bestandteile zerlegen
+   - "später"
+   - "Vielfaches"
+   - "Währung"
+
+   Einfachster Vertrag, in dem nur die Währung eine Rolle spielt.
+   "Ich bekomme jetzt 1€."
+
+   + "Vielfaches"
+   "Ich bekomme jetzt 100€."
 -}
 
 data Date = MkDate String -- YYYY-MM-DD
@@ -15,3 +26,34 @@ xmas :: Date
 xmas = MkDate "2025-12-24"
 easter :: Date
 easter = MkDate "2025-04-20"
+
+type Amount = Double
+
+data Currency = EUR | GBP | YEN | USD
+  deriving Show
+
+{-
+data Contract =
+    ZeroCouponBond Date Amount Currency
+  | Call
+  | Put
+  | Future
+
+zcb1 :: Contract
+zcb1 = ZeroCouponBond xmas 100 EUR
+-}
+
+data Contract =
+    One Currency
+  | Multiple Amount Contract -- Selbstbezug => Kombinator
+  deriving Show
+
+-- "Ich bekomme 1€ jetzt."
+c1 :: Contract
+c1 = One EUR
+
+-- "Ich bekomme 100€ jetzt."
+c2 :: Contract
+c2 = Multiple 100 (One EUR)
+
+c3 = Multiple 50 c2
