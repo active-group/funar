8945a42288a9600d5c4aa8d3811668050c87d258
Mike Sperber <sperber@deinprogramm.de>
2025-11-18 09:49:53 +0100

    Update.

--- vorkurs/rev.rkt
+++ vorkurs/rev.rkt
@@ -3,7 +3,7 @@
 ; Liste umdrehen
 (: rev ((list-of %a) -> (list-of %a)))
 
-(check-expect (rev (list 1 2 3 4))
+#;(check-expect (rev (list 1 2 3 4))
               (list 4 3 2 1))
 
 (define rev
@@ -24,7 +24,7 @@
 ; Element an eine Liste anhängen
 (: append-element ((list-of %a) %a -> (list-of %a)))
 
-(check-expect (append-element (list 1 2 3) 4)
+#;(check-expect (append-element (list 1 2 3) 4)
               (list 1 2 3 4))
 
 (define append-element
@@ -46,5 +46,9 @@
     (cond
       ((empty? list) acc)
       ((cons? list)
+       ; rekursiver Aufruf: kein Kontext
+       ; tail call, "endrekursiver Aufruf"
        (rev2 (rest list)
              (cons (first list) acc))))))
+
+; JVM-Bug: Auch tail calls verbrauchen Platz auf dem Stack
