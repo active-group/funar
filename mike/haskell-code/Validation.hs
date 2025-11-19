4559e0562d0e3bd3b27a05b539e01a2ba77a5611
Mike Sperber <sperber@deinprogramm.de>
2025-11-19 12:15:50 +0100

    Update.

--- mike/haskell-code/Validation.hs
+++ mike/haskell-code/Validation.hs
@@ -1,1 +1,23 @@
 module Validation where
+
+-- https://docs.hibernate.org/validator/9.1/reference/en-US/html_single/#_validating_constraints
+
+data Car = MkCar { licensePlate :: LicensePlate, 
+                   seatCount :: SeatCount }
+ deriving Show
+
+data LicensePlate = MkLicensePlate String
+  deriving Show
+
+data SeatCount = MkSeatCount Integer
+  deriving Show
+
+data Validated a =
+    Valid a
+  | Invalid [String]
+  deriving Show
+
+instance Functor Validated where
+    fmap :: (a -> b) -> Validated a -> Validated b
+    fmap f (Valid a) = undefined
+    fmap f (Invalid errors) = undefined
