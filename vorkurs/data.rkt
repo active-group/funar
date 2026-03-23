32381db727ba9c5f9cb2fed2329aa8358e063a31
Mike Sperber <sperber@deinprogramm.de>
2026-03-23 11:15:17 +0100

    Update.

---
+++ vorkurs/data.rkt
@@ -0,0 +1,105 @@
+#lang deinprogramm/sdp/beginner
+
+; Datendefinition:
+; Haustier ist eins der folgenden:
+; - Hund  -ODER-
+; - Katze -ODER-
+; - Schlange
+; Fallunterscheidung
+; Aufzählung
+(define pet
+  (signature (enum "dog"
+                   "cat"
+                   "snake")))
+
+; Ist Haustier niedlich?
+(: cute? (pet -> boolean))
+
+(check-expect (cute? "dog") #t)
+(check-expect (cute? "cat") #t)
+(check-expect (cute? "snake") #f)
+
+; Gerüst
+#;(define cute?
+  (lambda (pet)
+    ...))
+
+; Schablone
+#;(define cute?
+  (lambda (pet)
+    ; Verzweigung, 1 Zweig pro Fall
+    ; (<Bedingung> <Ergebnis>)
+    (cond
+      ((equal? pet "dog") ...)
+      ((equal? pet "cat") ...)
+      ((equal? pet "snake") ...))))
+
+(define cute?
+  (lambda (pet)
+    ; Verzweigung, 1 Zweig pro Fall
+    ; (<Bedingung> <Ergebnis>)
+    #;(match pet
+      ("dog" #t)
+      ("cat" #t)
+      ("snake" #f))
+        
+    (cond
+      ((equal? pet "dog") #t)
+      ((equal? pet "cat") #t)
+      ((equal? pet "snake") #f))))
+
+; Uhrzeit besteht aus / hat folgende Eigenschaften:
+; - Stunde -UND-
+; - Minute
+; zusammengesetzte Daten / Produkte
+(define-record time ; Signatur
+  make-time ; Konstruktor
+  (time-hour natural) ; Selektor / "Getter"
+  (time-minute natural))
+
+(: make-time (natural natural -> time))
+(: time-hour (time -> natural))
+(: time-minute (time -> natural))
+
+; 11 Uhr 1 Minute
+(define time1 (make-time 11 1))
+
+; 14:07
+(define time2 (make-time 14 07))
+
+; Minuten seit Mitternacht
+(: msm (time -> natural))
+
+(check-expect (msm time1)
+              661)
+(check-expect (msm time2)
+              847)
+
+; Schablone
+#;(define msm
+  (lambda (time)
+    ...
+    (time-hour time)
+    (time-minute time)
+    ...))
+
+
+#;(define msm
+  (lambda (time)
+    (+ (* 60 (time-hour time))
+       (time-minute time))))
+
+(define msm
+  (lambda (time)
+    (define hour (time-hour time))
+    (define minute (time-minute time))
+    (+ (* 60 hour)
+       minute)))
+
+; lexikalische Bindung:
+; von einem Vorkommen einer Variable
+; von INNEN nach AUSSEN
+; suchen nach: dem ersten lambda, define
+
+; Aus den Minuten-seit-Mitternacht eine Uhrzeit machen
+
