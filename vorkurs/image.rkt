9e5bfa052e05cda1f32582d2a053253f3a332283
Mike Sperber <sperber@deinprogramm.de>
2026-03-23 10:08:16 +0100

    Update.

--- vorkurs/image.rkt
+++ vorkurs/image.rkt
@@ -1,1 +1,37 @@
 #lang deinprogramm/sdp/beginner
+(require deinprogramm/sdp/image)
+  
+(define x (+ 23 42))
+(define y (* x 2))
+(define z
+  (+ 23
+     (* 2
+        42)))
+
+(define circle1
+  (circle 50 "solid" "red"))
+(define square1
+  (square 100 "outline" "blue"))
+(define star1
+  (star 50 "solid" "green"))
+(define overlay1
+  (overlay star1 circle1))
+
+(above
+ (beside circle1 star1)
+ (beside star1 circle1))
+
+(above
+ (beside star1 square1)
+ (beside square1 star1))
+
+; Abstrahieren
+; - 2 ähnliche Code-Passagen
+; - (das letzte Mal) kopieren
+; - Unterschiede durch ("abstrakte") Namen
+; - lambda-Ausdruck mit Namen als Parameter
+(define tile
+  (lambda (image1 image2)
+    (above
+     (beside image1 image2)
+     (beside image2 image1))))
