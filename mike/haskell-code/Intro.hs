1910900d74f887b6a41c1e5fc3d4f87c3bae04c2
Mike Sperber <sperber@deinprogramm.de>
2026-03-24 09:39:04 +0100

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -13,4 +13,23 @@ double :: Integer -> Integer
 -- >>> double 21
 -- 42
 
-double = \ x -> x * 2
+-- double = \ x -> x * 2
+double x = x * 2 -- syntaktischer Zucker
+
+-- Zahl vervierfachen
+quadruple :: Integer -> Integer
+-- >>> quadruple 7
+-- 28
+quadruple x =
+    let d = double x
+    in double d
+
+-- Ein Haustier ist eins der folgenden:
+-- - Hund -ODER-
+-- - Katze -ODER-
+-- - Schlange
+data Pet =
+    Dog
+  | Cat
+  | Snake
+  deriving Show
