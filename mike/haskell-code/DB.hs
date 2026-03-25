4542ad39d3110538a17acf4da0e3e69d6ad6f2d8
Mike Sperber <sperber@deinprogramm.de>
2026-03-25 14:56:28 +0100

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 module DB where
 
 import qualified Data.Map.Strict as Map
@@ -43,6 +44,22 @@ splice (Put key value callback) next =
     Put key value (\() -> splice (callback ()) next)
 splice (Return result) next = next result
 
+-- >>> :info Monad
+-- type Monad :: (* -> *) -> Constraint
+-- class Applicative m => Monad m where
+--   (>>=) :: m a -> (a -> m b) -> m b
+--   return :: a -> m a
+
+instance Functor DB where
+
+instance Applicative DB where
+
+instance Monad DB where
+    return :: a -> DB a
+    return = Return
+    (>>=) :: DB a -> (a -> DB b) -> DB b
+    (>>=) = splice
+
 p1 :: DB String
 p1 = Put "Mike" 100 (\() ->
      Get "Mike" (\x ->
@@ -50,10 +67,25 @@ p1 = Put "Mike" 100 (\() ->
      Get "Mike" (\y ->
      Return (show (x+y))))))
 
+p1' :: DB String
+p1' = splice (put "Mike" 100) (\() ->
+      splice (get "Mike") (\ x ->
+      splice (put "Mike" (x+1)) (\() ->
+      splice (get "Mike") (\ y -> 
+      Return (show (x+y))))))
+      
+
+p1'' :: DB String
+p1'' = do put "Mike" 100
+          x <- get "Mike"
+          put "Mike" (x+1)
+          y <- get "Mike"
+          return (show (x+y))
+
 -- DB-Programm ausführen
 executeDB :: DB a -> Map Key Value -> (a, Map Key Value)
 
--- >>> executeDB p1 Map.empty
+-- >>> executeDB p1'' Map.empty
 -- ("201",fromList [("Mike",101)])
 
 executeDB (Get key callback) mp = 
