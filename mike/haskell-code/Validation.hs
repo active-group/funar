146b7ffadedc80d8e499c92373f0f61132acb9ca
Mike Sperber <sperber@deinprogramm.de>
2026-03-25 10:59:50 +0100

    Update.

--- mike/haskell-code/Validation.hs
+++ mike/haskell-code/Validation.hs
@@ -1,2 +1,18 @@
 module Validation where
 
+-- "Make illegal states unrepresentable." -- Yaron Minsky
+
+-- 1. möglichst viele Einschränkungen in den Typen festhalten
+
+-- 2 bis 14 Buchstaben
+data LicensePlate = MkLicensePlate String
+  deriving Show
+
+-- mindestens 2
+data SeatCount = MkSeatCount Integer
+  deriving Show
+
+data Car = MkCar { licensePlate :: LicensePlate,
+                   seatCount :: Integer }
+    deriving Show
+
