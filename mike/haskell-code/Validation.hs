06533d061fb47f942b665384254b345ae083ff56
Mike Sperber <sperber@deinprogramm.de>
2026-03-25 11:30:21 +0100

    Update.

--- mike/haskell-code/Validation.hs
+++ mike/haskell-code/Validation.hs
@@ -13,6 +13,34 @@ data SeatCount = MkSeatCount Integer
   deriving Show
 
 data Car = MkCar { licensePlate :: LicensePlate,
-                   seatCount :: Integer }
+                   seatCount :: SeatCount }
     deriving Show
 
+mkSeatCount :: Integer -> Maybe SeatCount
+mkSeatCount n =
+    if n >= 2
+    then Just (MkSeatCount n)
+    else Nothing
+
+mkLicensePlate :: String -> Maybe LicensePlate
+mkLicensePlate s =
+    let l = length s
+    in if l >= 2 && l <= 14
+       then Just (MkLicensePlate s)
+       else Nothing
+
+mkCar :: String -> Integer -> Maybe Car
+mkCar s n =
+    {-
+    case mkLicensePlate s of
+        Just licensePlate ->
+            case mkSeatCount n of
+                Just seatCount ->
+                    Just (MkCar licensePlate seatCount)
+                Nothing -> Nothing
+        Nothing -> Nothing
+-}
+    case (mkLicensePlate s, mkSeatCount n) of
+        (Just licensePlate, Just seatCount) ->
+            Just (MkCar licensePlate seatCount)
+        _ -> Nothing
