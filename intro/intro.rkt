b27741e7c431043fea141a292743d3c53efe1fd4
Mike Sperber <sperber@deinprogramm.de>
2026-09-22 10:40:08 +0200

    Update.

--- intro/intro.rkt
+++ intro/intro.rkt
@@ -7,3 +7,29 @@
         23)))
 (define y
   (* x 2))
+
+(define circle1 (circle 50 "solid" "red"))
+(define square1 (square 100 "outline" "blue"))
+(define star1 (star 50 "solid" "green"))
+  
+(define overlay1 (overlay star1 circle1))
+
+(above
+ (beside star1 circle1)
+ (beside circle1 star1))
+
+(above
+ (beside square1 star1)
+ (beside star1 square1))
+
+; Abstraction
+; 2 similar/related pieces of code
+; - copy (one last time)
+; - replace differences by (abstract) names
+; - put the names in a lambda -> function
+
+(define tile
+  (lambda (image1 image2)
+    (above
+     (beside image1 image2)
+     (beside image2 image1))))
