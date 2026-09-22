cbf335e6f5b1f154c1e60b51bec10ef827a7dcf5
Mike Sperber <sperber@deinprogramm.de>
2026-09-22 15:21:45 +0200

    Animals in F#

--- FSharpIntro/src/Test/UnitTest1.fs
+++ FSharpIntro/src/Test/UnitTest1.fs
@@ -11,6 +11,34 @@ let isCute (pet: Pet): bool =
    | Cat -> true
    | Snake -> false
 
+type Liveness = Alive | Dead
+
+type Weight = double
+
+type Dillo = { liveness : Liveness 
+               weight : Weight }
+
+let runOverDillo (dillo: Dillo): Dillo =
+    // { liveness = Dead
+    //  weight = dillo.weight }
+    { dillo with liveness = Dead } // functional update
+
+type Parrot = { sentence : string
+                weight : Weight }
+
+let runOverParrot (parrot: Parrot): Parrot =
+    { sentence = ""
+      weight = parrot.weight }
+
+type Animal =
+| MkDillo of Dillo
+| MkParrot of Parrot 
+
+let runOverAnimal (animal: Animal): Animal =
+    match animal with
+    | MkDillo dillo -> MkDillo (runOverDillo dillo)
+    | MkParrot parrot -> MkParrot (runOverParrot parrot)
+
 [<SetUp>]
 let Setup () =
     ()
