c414a469191ea29950e977a71fa2ac066ce510df
Mike Sperber <sperber@deinprogramm.de>
2026-09-23 09:54:21 +0200

    Update.

--- intro/rev.rkt
+++ intro/rev.rkt
@@ -1,1 +1,23 @@
 #lang deinprogramm/sdp
+
+; reverse a list
+(: rev ((list-of %a) -> (list-of %a<)))
+
+(check-expect (rev (list 1 2 3 4))
+              (list 4 3 2 1))
+
+(define rev
+  (lambda (list)
+    (cond
+      ((empty? list) empty)
+      ((cons? list)
+       (add-to-list
+        (rev (rest list))
+        (first list))))))
+
+; add an element to the end of the list
+(: add-to-list ((list-of %a) %a -> (list-of %a)))
+
+(check-expect (add-to-list (list 1 2 3) 4)
+              (list 1 2 3 4))
+                   
