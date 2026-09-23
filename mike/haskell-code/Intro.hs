326daba4fa4c8ef1f545e11a437400597b91bbc4
Mike Sperber <sperber@deinprogramm.de>
2026-09-23 10:56:59 +0200

    Update.

--- mike/haskell-code/Intro.hs
+++ mike/haskell-code/Intro.hs
@@ -1,3 +1,39 @@
 {-# LANGUAGE InstanceSigs #-}
 module Intro where
 
+x :: Integer
+x = 23 + 42
+
+-- >>> x
+-- 65
+
+y :: Integer
+y = x * 2
+
+-- Pet is one of the following:
+-- - dog OR
+-- - cat OR
+-- - snake
+data Pet = Dog | Cat | Snake
+  deriving Show
+
+-- Is a pet cute?
+isCute :: Pet -> Bool
+
+-- >>> isCute Dog
+-- True
+-- >>> isCute Snake
+-- False
+
+-- one equation per case
+isCute Dog = True
+isCute Cat = True
+isCute Snake = False
+
+{-
+isCute pet = 
+    case pet of
+        Dog -> True
+        Cat -> True
+        Snake -> False
+-}
