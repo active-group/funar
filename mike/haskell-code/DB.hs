0b7436e9ad53298318eb5b0eed49db16958a8e9f
Mike Sperber <sperber@deinprogramm.de>
2026-09-24 14:58:54 +0200

    Update.

--- mike/haskell-code/DB.hs
+++ mike/haskell-code/DB.hs
@@ -52,6 +52,12 @@ splice (Put key value cont) next =
     Put key value (\() -> splice (cont ()) next)
 splice (Return result) next = next result
 
+p1' = splice (put "Mike" 100) (\() ->
+      splice (get "Mike") (\x ->
+      splice (put "Mike" (x+1)) (\() ->
+      splice (get "Mike") (\y ->
+      Return (show (x+y))))))
+
 runDB :: DB a -> Map Key Value -> (a, Map Key Value)
 
 -- >>> runDB p1 Map.empty
