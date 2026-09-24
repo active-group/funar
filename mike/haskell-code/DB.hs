decbed114f1525acf4036f4cefd3178f2f8bde3c
Mike Sperber <sperber@deinprogramm.de>
2026-09-24 14:30:30 +0200

    Update.

---
+++ mike/haskell-code/DB.hs
@@ -0,0 +1,42 @@
+module DB where
+
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map, (!))
+
+{-
+put "Mike" 100
+x = get "Mike"
+put "Mike" (x+1)
+y = get "Mike"
+return (show (x+y))
+-}
+
+type Key = String
+type Value = Integer
+
+{-
+data DBCommand a =
+    Put Key Value
+  | Get Key
+  | Return a
+
+type DBProgram a = [DBCommand a]
+
+p1 = [Put "Mike" 100,
+      Get "Mike",
+      Put "Mike" (x+1)  -- failure!
+-}
+
+data DB a =
+    Get Key       (Value -> DB a) -- trick, continuation
+  | Put Key Value (()    -> DB a)
+  | Return a
+
+p1 :: DB String
+p1 = Put "Mike" 100 (\() ->
+     Get "Mike" (\x ->
+     Put "Mike" (x+1) (\() ->
+     Get "Mike" (\y ->
+     Return (show (x+y))))))
+
+runDB :: DB a -> a
