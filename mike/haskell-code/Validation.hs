ad3f660fbedfb967d3cd93848236b0083ab80aca
Mike Sperber <sperber@deinprogramm.de>
2026-09-24 12:02:59 +0200

    Update.

--- mike/haskell-code/Validation.hs
+++ mike/haskell-code/Validation.hs
@@ -2,10 +2,33 @@ module Validation where
 
 -- "Make illegal states unpresentable." -- Yaron Minsky
 
+data SimpleMovie = MkSimpleMovie { simpleTitle :: String }
+  deriving Show
+
+checkTitle :: String -> Maybe String
+checkTitle s = if length s == 0
+               then Nothing
+               else Just s
+
+mkSimpleMovie :: String -> Maybe SimpleMovie
+-- mkSimpleMovie s = MkSimpleMovie (checkTitle s)
+mkSimpleMovie s = fmap MkSimpleMovie (checkTitle s)
+
 data Movie = MkMovie { title :: String,
                        price :: Integer,
                        rating :: String }
   deriving Show
+
+maybeMap2 :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+maybeMap2 f Nothing _ = Nothing
+maybeMap2 f _ Nothing = Nothing
+maybeMap2 f (Just a) (Just b) = Just (f a b)
+
+-- >>> :info Applicative
+-- type Applicative :: (* -> *) -> Constraint
+-- class Functor f => Applicative f where
+--   pure :: a -> f a
+--   (<*>) :: f (a -> b) -> f a -> f b
 
 -- values of this type should always be valid
 
