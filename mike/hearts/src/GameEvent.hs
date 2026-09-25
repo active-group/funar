0c937902c7b5ffdc384f39bc072cbd937de9abf0
Mike Sperber <sperber@deinprogramm.de>
2026-09-25 09:21:51 +0200

    GameEvent

--- mike/hearts/src/GameEvent.hs
+++ mike/hearts/src/GameEvent.hs
@@ -5,3 +5,16 @@ import Cards
 import qualified Data.Map as Map
 import Data.Map (Map)
 
+data GameEvent
+  = HandDealt Player Hand
+  | PlayerTurnChanged Player
+  | LegalCardPlayed Player Card
+  | TrickTaken Player Trick
+  | IllegalCardAttempted Player Card
+  | GameEnded Player
+  deriving (Show, Eq)
+
+data GameCommand
+  = DealHands (Map Player Hand)
+  | PlayCard Player Card
+  deriving (Show, Eq)
